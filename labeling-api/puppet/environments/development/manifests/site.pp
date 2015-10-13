9dc8880e18ef22a1db4457f6ae7836a5a647ceda
Hans-Christian Otto <cho@crosscan.com>
2015-10-13 17:37:19 +0200

    Add Nginx & Configure it to use PHP-FPM

--- labeling-api/puppet/environments/development/manifests/site.pp
+++ labeling-api/puppet/environments/development/manifests/site.pp
@@ -1,8 +1,28 @@
 class { 'apt':
   update => {
-    frequency => 'always',
+    frequency => 'daily',
   },
 }
 
+class { 'nginx': }
+
 include php
 ensure_packages(["software-properties-common"])
+
+nginx::resource::vhost { "_":
+  ensure              => present,
+  www_root            => "/vagrant/web",
+  index_files     => ['app_dev.php'],
+  fastcgi     => '127.0.0.1:9000',
+  try_files   => ['$uri', '/app.php$is_args$args'],
+}
+
+exec { 'composer-install-labelstation':
+  command => '/usr/local/bin/composer install',
+  cwd     => '/vagrant',
+  creates => '/vagrant/vendor',
+  require => Class['php::composer'],
+}
+
+
+
