0fa1ec9e28a78ee3c125058c6ef58cb8b3fa6511
Jakob Westhoff <jakob@westhoffswelt.de>
2015-10-21 18:23:19 +0200

    Implemented registration of Frame module

--- labeling-ui/Application/Frame/Frame.js
+++ labeling-ui/Application/Frame/Frame.js
@@ -1,7 +1,12 @@
 import Module from '../Module';
+import FrameService from 'Services/FrameService';
+import TaskFrameLocationService from 'Services/TaskFrameLocationService';
 
 export default class Frame extends Module {
   registerWithAngular(angular) {
     this.module = angular.module('AnnoStation.Frame', []);
+
+    this.module.service('FrameService', FrameService);
+    this.module.service('TaskFrameLocationService', TaskFrameLocationService);
   }
 }
