157f21066cd778114a0eb3a888ca265f8d92215f
Jan Dworschak <jad@crosscan.com>
2015-10-21 14:51:24 +0200

    Removed dummy tests

--- labeling-ui/Tests/unit/dummy.spec.js
+++
@@ -1,9 +0,0 @@
-import angularMocks from 'angular-mocks';
-import $ from 'jquery';
-import angular from 'angular';
-
-describe('UnitTestDummy', function() {
-  it('should pass', function() {
-    expect(1).toBe(1);
-  });
-});
