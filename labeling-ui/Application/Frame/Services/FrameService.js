2a1473631cec7c3ca2464d11e481ff89eca69b2d
Jakob Westhoff <jakob@westhoffswelt.de>
2015-10-21 17:55:35 +0200

    Implemented and Tested FrameService
    This first version is able to create Images from given FrameLocations. Most likely we will integrate other "Output-Formats" in the future as well.

---
+++ labeling-ui/Application/Frame/Services/FrameService.js
@@ -0,0 +1,21 @@
+/**
+ * Service to interact with {@link FrameLocation}s
+ *
+ * This service is capable of preloading creating and interacting with {@link FrameLocation} models.
+ */
+export default class FrameService {
+  /**
+   * Create and return an {@link Image} for the given {@link FrameLocation}
+   *
+   * @param location
+   * @returns {Promise<HTMLImageElement>}
+   */
+  getImage(location) {
+    return new Promise((resolve, reject) => {
+      const image = new Image();
+      image.addEventListener('load', () => resolve(image));
+      image.addEventListener('error', (error) => reject(error));
+      image.src = location.url;
+    });
+  }
+}
