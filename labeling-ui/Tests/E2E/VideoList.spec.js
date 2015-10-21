7d2306b09088229ab3a22e527116864b17bd0060
Jan Dworschak <jad@crosscan.com>
2015-10-21 16:15:05 +0200

    Added example on how to use protractor mocks

--- labeling-ui/Tests/E2E/VideoList.spec.js
+++ labeling-ui/Tests/E2E/VideoList.spec.js
@@ -1,5 +1,13 @@
+import mock from 'protractor-http-mock';
+
 describe('VideoList', () => {
+  beforeEach(() => {
+    mock(['VideoList/Dummy'])
+  });
+
   it('should pass', () => {
+    browser.get('/');
+
     expect(1).toBe(1);
   });
 });
---
+++ labeling-ui/Tests/ProtractorMocks/VideoList/Dummy.js
@@ -0,0 +1,53 @@
+module.exports = {
+  request: {
+    path: '/api/video/list',
+    method: 'GET'
+  },
+  response: {
+    data: {
+      "success": true,
+      "data": {
+        "id": "2297",
+        "serial": "000B9101331F",
+        "store_id": "2050",
+        "client_type_id": "1",
+        "client_type_name": "crosscan 3d",
+        "version": "2.4.1",
+        "system_version": "2.4.1",
+        "hagl_system_version": "1.11.3.4",
+        "internal_ip": "10.38.91.241",
+        "buffer_size": "0",
+        "last_contact": "1415784175",
+        "last_updater_connect": "1415782867",
+        "last_ip": "164.61.4.5",
+        "log_connections": "0",
+        "store_title": "010 K\u00f6ln",
+        "store_cssid": "E1U7WH91FA",
+        "store_town": "K\u00f6ln",
+        "customer_name": "Kaufhof",
+        "customer_id": "1054",
+        "object_names": "Zugang_EG_3_22_Rolltreppe",
+        "assembling_height": "",
+        "angel_size": "",
+        "use_polygon_count": false,
+        "vpn_enabled": false,
+        "vpn_ip": "",
+        "is_not_responding": true,
+        "router_mac_address": "",
+        "router_version": "",
+        "router_model_name": "",
+        "live_view_enabled": false,
+        "live_view_starttime": 0,
+        "live_view_endtime": 0,
+        "live_view_reason": "",
+        "live_view_username": "",
+        "live_view_by_employee": "",
+        "update_version": "unknown",
+        "schedule_user_id": "4302",
+        "schedule_update_id": "218",
+        "schedule_start_timestamp": "1428761040",
+        "schedule_update_version": "2.11.5"
+      }
+    }
+  }
+};
