1bf17a5d24be85f8f6927b6ea77c4e1704ef2f72
Jakob Westhoff <jakob@westhoffswelt.de>
2015-10-21 18:22:45 +0200

    Implemented registration of Task Module

--- labeling-ui/Application/Task/Task.js
+++ labeling-ui/Application/Task/Task.js
@@ -1,6 +1,10 @@
 import Module from '../Module';
+import TaskService from 'Services/TaskService';
 
 export default class Task extends Module {
   registerWithAngular(angular) {
+    this.module = angular.module('AnnoStation.Task', []);
+
+    this.module.service('TaskService', TaskService);
   }
 }
