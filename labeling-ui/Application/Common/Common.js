c1fa832e72845ca8ddf0f38a8b672b9ec0636306
Jakob Westhoff <jakob@westhoffswelt.de>
2015-10-22 13:27:50 +0200

    Registered ApiService

--- labeling-ui/Application/Common/Common.js
+++ labeling-ui/Application/Common/Common.js
@@ -1,7 +1,10 @@
 import Module from '../Module';
+import ApiService from 'Services/ApiService';
 
 export default class Common extends Module {
   registerWithAngular(angular) {
     this.module = angular.module('AnnoStation.Common', []);
+
+    this.module.service('ApiService', ApiService);
   }
 }
