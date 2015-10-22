7df0bc110a3d9fb948035857a78c2bd21d9a6680
Jakob Westhoff <jakob@westhoffswelt.de>
2015-10-22 13:57:18 +0200

    Fix importing of ApiService

--- labeling-ui/Application/Common/Common.js
+++ labeling-ui/Application/Common/Common.js
@@ -1,10 +1,9 @@
 import Module from '../Module';
-import ApiService from 'Services/ApiService';
+import ApiService from './Services/ApiService';
 
 export default class Common extends Module {
   registerWithAngular(angular) {
     this.module = angular.module('AnnoStation.Common', []);
-
     this.module.service('ApiService', ApiService);
   }
 }
