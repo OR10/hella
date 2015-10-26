96c23a5d170a016368d17b816cd4e4e0682d41f1
Felix Knischewski <fek@crosscan.com>
2015-10-26 15:29:14 +0100

    labeling_api: Add video upload form

--- labeling-api/src/AppBundle/Controller/Index.php
+++ labeling-api/src/AppBundle/Controller/Index.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Service\ImporterService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration;
 use Symfony\Bridge\Twig;
 use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -16,10 +18,15 @@ class Index extends Base
      * @var TwigEngine
      */
     private $twigEngine;
+    /**
+     * @var ImporterService
+     */
+    private $importerService;
 
-    function __construct(Twig\TwigEngine $twigEngine)
+    function __construct(Twig\TwigEngine $twigEngine, ImporterService $importerService)
     {
-        $this->twigEngine = $twigEngine;
+        $this->twigEngine      = $twigEngine;
+        $this->importerService = $importerService;
     }
 
     /**
@@ -30,6 +37,41 @@ class Index extends Base
         return new Response(
             $this->twigEngine->render(
                 'AppBundle:default:index.html.twig'
+            )
+        );
+    }
+
+    /**
+     * @Configuration\Route("/upload")
+     * @Configuration\Method({"GET"})
+     */
+    public function uploadGetAction(HttpFoundation\Request $request)
+    {
+        return new Response(
+            $this->twigEngine->render(
+                'AppBundle:default:uploadForm.html.twig'
+            )
+        );
+    }
+
+    /**
+     * @Configuration\Route("/upload")
+     * @Configuration\Method({"POST"})
+     */
+    public function uploadPostAction(HttpFoundation\Request $request)
+    {
+        /**
+         * @var UploadedFile $file
+         */
+        $filename = $request->files->get('file');
+        $stream   = fopen($filename, 'r+');
+
+        $task = $this->importerService->import($filename, $stream);
+
+        return new Response(
+            $this->twigEngine->render(
+                'AppBundle:default:uploadForm.html.twig',
+                ['taskId' => $task->getId()]
             )
         );
     }
