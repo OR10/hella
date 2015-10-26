336952b123358241d3242840288edefa19041645
Christian Hammerl <chh@crosscan.com>
2015-10-26 15:13:03 +0100

    puppet: fix nginx config
    Stupid me ...

--- labeling-api/puppet/modules/labeling_api/manifests/init.pp
+++ labeling-api/puppet/modules/labeling_api/manifests/init.pp
@@ -119,9 +119,11 @@ class labeling_api(
       vhost                => '_',
       index_files          => [$app_main_script],
       fastcgi              => '127.0.0.1:9000',
-      fastcgi_read_timeout => '900',
       fastcgi_param        => {
           'SCRIPT_FILENAME' => '$document_root$fastcgi_script_name',
+      },
+      location_cfg_append => {
+          fastcgi_read_timeout => '900',
       }
     }
   }
