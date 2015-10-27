d2aeef8feb98214c955ba63268d24324e3cd8fd4
Felix Knischewski <fek@crosscan.com>
2015-10-27 09:56:56 +0100

    labeling_ui: Add labeling data sevice

---
+++ labeling-ui/Application/LabelingData/Interfaces/LabelingData.js
@@ -0,0 +1,25 @@
+/**
+ * One specific LabelingData object
+ *
+ * @interface LabelingData
+ */
+
+/**
+ * Unique id identifying this specific labeling data object
+ *
+ * @property {string} id
+ */
+
+/**
+ * Unique string that represents the revision of the current object
+ *
+ * @property {string} rev
+ */
+
+/**
+ * Array of shapes the labeling data object can consist of
+ *
+ * Different shapes exist, like rectangle, circle etc.
+ *
+ * @propery {Shapes[]} shapes
+ */
