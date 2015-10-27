bdb8ae3c4968be2d5829446cc5e38d9b3b11d2f0
Felix Knischewski <fek@crosscan.com>
2015-10-27 10:08:05 +0100

    Eslint fixes

--- labeling-ui/Tests/Unit/LabelingData/LabelingDataService.spec.js
+++ labeling-ui/Tests/Unit/LabelingData/LabelingDataService.spec.js
@@ -6,140 +6,139 @@ import Common from 'Application/Common/Common';
 import LabelingDataService from 'Application/LabelingData/Services/LabelingDataService';
 
 describe('LabelingDataService', () => {
-    let $httpBackend;
-    let $httpParamSerializer;
-    let service;
+  let $httpBackend;
+  let $httpParamSerializer;
+  let service;
 
-    beforeEach(() => {
-        const commonModule = new Common();
-        commonModule.registerWithAngular(angular);
-        module('AnnoStation.Common');
+  beforeEach(() => {
+    const commonModule = new Common();
+    commonModule.registerWithAngular(angular);
+    module('AnnoStation.Common');
 
-        module($provide => {
-            $provide.value('applicationConfig', {
-                Common: {
-                    apiPrefix: '/api',
-                    backendPrefix: '/backend',
-                },
-            });
-        });
-
-        inject($injector => {
-            $httpBackend = $injector.get('$httpBackend');
-            $httpParamSerializer = $injector.get('$httpParamSerializer');
-            service = $injector.instantiate(LabelingDataService);
-        });
+    module($provide => {
+      $provide.value('applicationConfig', {
+        Common: {
+          apiPrefix: '/api',
+          backendPrefix: '/backend',
+        },
+      });
     });
 
-    it('should be able to instantiate without non injected arguments', () => {
-        expect(service instanceof LabelingDataService).toEqual(true);
+    inject($injector => {
+      $httpBackend = $injector.get('$httpBackend');
+      $httpParamSerializer = $injector.get('$httpParamSerializer');
+      service = $injector.instantiate(LabelingDataService);
     });
+  });
 
-    it('should receive the extracted response', done => {
-        const task = {id: 'someTaskId234'};
-        const frameNumber = 2;
-        const expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
-        const expectedResult = [
-            {id: 'abc', rev: 'bcd', shapes: [{type: 'rectangle'}]},
-            {id: 'cde', rev: 'def', shapes: [{type: 'circle'}]},
-        ];
+  it('should be able to instantiate without non injected arguments', () => {
+    expect(service instanceof LabelingDataService).toEqual(true);
+  });
 
-        $httpBackend
-            .expect('GET', expectedUrl)
-            .respond(200, {result: expectedResult});
+  it('should receive the extracted response', done => {
+    const task = {id: 'someTaskId234'};
+    const frameNumber = 2;
+    const expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
+    const expectedResult = [
+      {id: 'abc', rev: 'bcd', shapes: [{type: 'rectangle'}]},
+      {id: 'cde', rev: 'def', shapes: [{type: 'circle'}]},
+    ];
 
-        service.getLabeledThingsInFrame(task, frameNumber)
-            .then(result => {
-                expect(result).toEqual(expectedResult);
-                done();
-            });
+    $httpBackend
+      .expect('GET', expectedUrl)
+      .respond(200, {result: expectedResult});
 
-        $httpBackend.flush();
+    service.getLabeledThingsInFrame(task, frameNumber)
+      .then(result => {
+        expect(result).toEqual(expectedResult);
+        done();
+      });
+
+    $httpBackend.flush();
+  });
+
+  it('should save a labeling data object', done => {
+    const task = {id: 'someTaskId234'};
+    const frameNumber = 2;
+    const expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
+    const labelingDataObject = {id: 'abc', rev: 'bcd', shapes: [{type: 'rectangle'}]};
+    const expectedResult = {success: true};
+
+    $httpBackend
+      .expect('POST', expectedUrl)
+      .respond(200, expectedResult);
+
+    service.createLabeledThingsInFrame(task, frameNumber, labelingDataObject)
+      .then(result => {
+        expect(result).toEqual(true);
+        done();
+      });
+
+    $httpBackend.flush();
+  });
+
+  it('should update a labeling data objects', done => {
+    const task = {id: 'someTaskId234'};
+    const frameNumber = 2;
+    const expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
+    const labelingDataObjects = [
+      {id: 'abc', rev: 'bcd', shapes: [{type: 'rectangle'}]},
+      {id: 'cde', rev: 'def', shapes: [{type: 'circle'}]},
+    ];
+    const expectedResult = {success: true};
+
+    $httpBackend
+      .expect('PUT', expectedUrl)
+      .respond(200, expectedResult);
+
+    service.updateLabeledThingsInFrame(task, frameNumber, labelingDataObjects)
+      .then(result => {
+        expect(result).toEqual(true);
+        done();
+      });
+
+    $httpBackend.flush();
+  });
+
+  using([
+    [{id: 'task1'}],
+    [{id: 'task2'}],
+    [{id: 'task3'}],
+    [{id: 'task4'}],
+  ], (task) => {
+    const frameNumber = 1;
+    const expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
+
+    it('should request the task as specified', done => {
+      $httpBackend
+        .expect('GET', expectedUrl)
+        .respond(200, {result: []});
+
+      service.getLabeledThingsInFrame(task, frameNumber)
+        .then(done);
+
+      $httpBackend.flush();
     });
+  });
 
-    it('should save a labeling data object', done => {
-        const task = {id: 'someTaskId234'};
-        const frameNumber = 2;
-        const expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
-        const labelingDataObject = {id: 'abc', rev: 'bcd', shapes: [{type: 'rectangle'}]};
-        const expectedResult = {success: true};
+  using([
+    [1],
+    [2],
+    [3],
+    [4],
+  ], (frameNumber) => {
+    const task = {id: 'abc'};
+    const expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
 
-        $httpBackend
-            .expect('POST', expectedUrl)
-            .respond(200, expectedResult);
+    it('should request the frame as specified', done => {
+      $httpBackend
+        .expect('GET', expectedUrl)
+        .respond(200, {result: []});
 
-        service.createLabeledThingsInFrame(task, frameNumber, labelingDataObject)
-            .then(result => {
-                expect(result).toEqual(true);
-                done();
-            });
+      service.getLabeledThingsInFrame(task, frameNumber)
+        .then(done);
 
-        $httpBackend.flush();
+      $httpBackend.flush();
     });
-
-    it('should update a labeling data objects', done => {
-
-        const task = {id: 'someTaskId234'};
-        const frameNumber = 2;
-        const expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
-        const labelingDataObjects = [
-            {id: 'abc', rev: 'bcd', shapes: [{type: 'rectangle'}]},
-            {id: 'cde', rev: 'def', shapes: [{type: 'circle'}]}
-        ];
-        const expectedResult = {success: true};
-
-        $httpBackend
-            .expect('PUT', expectedUrl)
-            .respond(200, expectedResult);
-
-        service.updateLabeledThingsInFrame(task, frameNumber, labelingDataObjects)
-            .then(result => {
-                expect(result).toEqual(true);
-                done();
-            });
-
-        $httpBackend.flush();
-    });
-
-    using([
-        [{id: 'task1'}],
-        [{id: 'task2'}],
-        [{id: 'task3'}],
-        [{id: 'task4'}],
-    ], (task) => {
-        let frameNumber = 1;
-        let expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
-
-        it('should request the task as specified', done => {
-            $httpBackend
-                .expect('GET', expectedUrl)
-                .respond(200, {result: []});
-
-            service.getLabeledThingsInFrame(task, frameNumber)
-                .then(done);
-
-            $httpBackend.flush();
-        });
-    });
-
-    using([
-        [1],
-        [2],
-        [3],
-        [4],
-    ], (frameNumber) => {
-        let task = {id: 'abc'};
-        let expectedUrl = `/backend/api/task/${task.id}/labeledThingInFrame/${frameNumber}`;
-
-        it('should request the frame as specified', done => {
-            $httpBackend
-                .expect('GET', expectedUrl)
-                .respond(200, {result: []});
-
-            service.getLabeledThingsInFrame(task, frameNumber)
-                .then(done);
-
-            $httpBackend.flush();
-        });
-    });
+  });
 });
