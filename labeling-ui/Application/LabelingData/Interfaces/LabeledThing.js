de206e1985168e71f0c42708fde4e1186c6b0e93
Jan Dworschak <jad@crosscan.com>
2015-10-28 10:24:30 +0100

    Extended LabeledThing* interfaces

---
+++ labeling-ui/Application/LabelingData/Interfaces/LabeledThing.js
@@ -0,0 +1,21 @@
+/**
+ * @interface LabeledThing
+ */
+
+/**
+ * Id of the task this LabeledThing belongs to.
+ *
+ * @property {String} taskId
+ */
+
+/**
+ * The range of frames in which this LabeledThing has been marked.
+ *
+ * @property {FrameRange} frameRange
+ */
+
+/**
+ * Array of annotated labeling properties for this LabeledThing.
+ *
+ * @property {Object[]} classes
+ */
--- labeling-ui/Application/LabelingData/Interfaces/LabeledThingInFrame.js
+++ labeling-ui/Application/LabelingData/Interfaces/LabeledThingInFrame.js
@@ -17,9 +17,27 @@
  */
 
 /**
+ * The frame this entity belongs to
+ *
+ * @property {int} frameNumber
+ */
+
+/**
+ * A reference to the LabeledThing this entity belongs to
+ *
+ * @property {String} labeledThingId
+ */
+
+/**
  * Array of shapes the labeled thing in frame can consist of.
  *
  * Different shapes exist, like rectangle, circle etc.
  *
  * @property {Shape[]} shapes
+ */
+
+/**
+ * Array of annotated labeling properties for this LabeledThingInFrame.
+ *
+ * @property {Object[]} classes
  */
