842ada3a5fb1769656607ced53232472d8dca933
Jan Dworschak <jad@crosscan.com>
2015-11-05 17:38:15 +0100

    Made selected step persistent across selection changes

--- labeling-ui/Application/LabelStructure/Directives/LabelWizardController.js
+++ labeling-ui/Application/LabelStructure/Directives/LabelWizardController.js
@@ -7,13 +7,33 @@ export default class LabelWizardController {
       this.offset = 0;
     }
 
-    $scope.$watchCollection('vm.labelState', () => {
+    $scope.$watchCollection('vm.labelState', (newState, oldState) => {
+      if (oldState) {
+        this.selectedStepIndex = this._findPreviousStepIndex(oldState) || 0;
+      } else {
+        this.selectedStepIndex = 0;
+      }
+
       if (this.limit) {
         this.steps = this.labelState.children.slice(this.offset, this.offset + this.limit);
       } else {
         this.steps = this.labelState.children;
       }
+
+      if (this.steps.length > this.selectedStepIndex) {
+        this.steps[this.selectedStepIndex].active = true;
+      }
     });
+  }
+
+  _findPreviousStepIndex(state) {
+    return state.children.reduce((foundIndex, step, index) => {
+      if (foundIndex) {
+        return foundIndex;
+      }
+
+      return step.active ? index : null;
+    }, null);
   }
 }
 
