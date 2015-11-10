da880e2cff36e7c31f0adc7ff53fe69d225437f0
Hans-Christian Otto <cho@crosscan.com>
2015-11-10 17:14:45 +0100

    [Labeling-UI] Make deployment target configurable

--- labeling-ui/Gulpfile.babel.js
+++ labeling-ui/Gulpfile.babel.js
@@ -290,12 +290,17 @@ gulp.task('build-fonts', (next) => {
 });
 
 gulp.task('deploy', () => {
+  const deploymentIp = process.env.LABELING_UI_DEPLOY_IP;
+  if(deploymentIp === undefined || deploymentIp === '') {
+    throw "Please set the environment variable LABELING_UI_DEPLOY_IP to the ip address that you want to deploy to.";
+  }
+
   return gulp.src('Distribution/**')
     .pipe($$.rsync({
       recurse: true,
       exclude: ['index-protractor.html'],
       root: 'Distribution/',
-      hostname: '192.168.217.212',
+      hostname: deploymentIp,
       destination: '/var/www/labeling-ui'
     }));
 });
