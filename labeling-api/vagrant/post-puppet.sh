1bde5f4dbe29713a0c7de2a54e9f9a3859569965
Christian Hammerl <chh@crosscan.com>
2015-11-13 01:19:33 +0100

    puppet: run post-puppet-provisioner only where it makes sense

--- labeling-api/vagrant/post-puppet.sh
+++ labeling-api/vagrant/post-puppet.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-sudo -u vagrant /vagrant/app/console doctrine:schema:update --force
-sudo -u vagrant /vagrant/app/console doctrine:couchdb:update-design-doc
+NAME="${1}"
 
-echo " "
-echo "************************************************************"
-echo " "
-echo "Please consider to run"
-echo " "
-echo "    app/console annostation:init"
-echo " "
-echo "to initialize your database with a clean state."
-echo " "
-echo "************************************************************"
-echo " "
+if [ "${NAME}" = "vagrant" -o "${NAME}" = "app.vagrant" ]; then
+    sudo -u vagrant /vagrant/app/console doctrine:schema:update --force
+    sudo -u vagrant /vagrant/app/console doctrine:couchdb:update-design-doc
+
+    echo " "
+    echo "************************************************************"
+    echo " "
+    echo "Please consider to run"
+    echo " "
+    echo "    app/console annostation:init"
+    echo " "
+    echo "to initialize your database with a clean state."
+    echo " "
+    echo "************************************************************"
+    echo " "
+fi
