7c09eaa5676af217559a2a4963d7116bbb2f998a
Christian Hammerl <chh@crosscan.com>
2015-11-16 14:22:52 +0100

    vagrant: rename vagrant files for multiple machines
    'distributed' was just the wrong term.

--- labeling-api/distributed-vagrant-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export VAGRANT_VAGRANTFILE=Vagrantfile.distributed
-export VAGRANT_DOTFILE_PATH=.vagrant.distributed
---
+++ labeling-api/vagrant-multi-env.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export VAGRANT_VAGRANTFILE=Vagrantfile.multi
+export VAGRANT_DOTFILE_PATH=.vagrant.multi
