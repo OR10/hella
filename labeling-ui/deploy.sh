271283ef5fd7b69f40fe7fa12f1fff9aff9a6a2b
Hans-Christian Otto <cho@crosscan.com>
2015-11-18 09:52:47 +0100

    ui: added deploy script

---
+++ labeling-ui/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This script is currently being used to deploy the software to a new staging-system.
+npm install
+jspm config registries.github.auth aGNvOmh5cGVyMGtpa2k=
+jspm install
+gulp test-unit
+gulp test-e2e
+gulp
+rsync -a Distribution/ root@${1}:/var/www/labeling-ui/
