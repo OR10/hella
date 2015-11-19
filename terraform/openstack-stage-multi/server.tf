261d02d15d1dad252afa8562d83dc4d10b78498e
Christian Hammerl <chh@crosscan.com>
2015-11-19 17:17:40 +0100

    terraform: support dynamic ips for database servers

--- terraform/openstack-stage-multi/server.tf
+++ terraform/openstack-stage-multi/server.tf
@@ -2,21 +2,21 @@ module "mysql" {
     source = "./modules/server"
     server_type = "mysql"
     count = 1
-    ipv4_address = "192.168.217.220"
+    ipv4_address = ""
 }
 
 module "couch" {
     source = "./modules/server"
     server_type = "couch"
     count = 1
-    ipv4_address = "192.168.217.221"
+    ipv4_address = ""
 }
 
 module "workerqueue" {
     source = "./modules/server"
     server_type = "workerqueue"
     count = 1
-    ipv4_address = "192.168.217.222"
+    ipv4_address = ""
 }
 
 module "app" {
@@ -27,6 +27,17 @@ module "app" {
 }
 
 resource "null_resource" "provisioning" {
+    provisioner "remote-exec" {
+        inline = [
+            "echo '${module.mysql.ipv4_address} mysql' >> /etc/hosts",
+            "echo '${module.couch.ipv4_address} couch' >> /etc/hosts",
+            "echo '${module.workerqueue.ipv4_address} workerqueue' >> /etc/hosts",
+        ]
+        connection {
+            host = "${module.app.ipv4_address}"
+        }
+    }
+
     provisioner "local-exec" {
         command = "cd ../../labeling-api/ && CAP_DEPLOY_IP=${module.app.ipv4_address} cap multi-staging-machine deploy"
         connection {
