8e13eacc955ed5703c781d469240e59302512300
Christian Hammerl <chh@crosscan.com>
2015-11-20 17:04:50 +0100

    labeling_api: improve error handling in kitti export

--- labeling-api/src/AppBundle/Service/TaskExporter/Exception/Kitti.php
+++ labeling-api/src/AppBundle/Service/TaskExporter/Exception/Kitti.php
@@ -6,4 +6,34 @@ use AppBundle\Exception;
 
 class Kitti extends Exception
 {
+    /**
+     * @var integer
+     */
+    private $frameNumber;
+
+    public function __construct($message = null, $code = 0, \Exception $previousException = null)
+    {
+        parent::__construct($message, $code, $previousException);
+    }
+
+    public function getFrameNumber()
+    {
+        return $this->frameNumber;
+    }
+
+    public function setFrameNumber($frameNumber)
+    {
+        $this->frameNumber = (int) $frameNumber;
+    }
+
+    public function getMessage()
+    {
+        $message = parent::getMessage();
+
+        if ($this->frameNumber === null) {
+            $message .= sprintf(', frameNumber: %d', $this->frameNumber);
+        }
+
+        return $message;
+    }
 }
