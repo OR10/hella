a6ccf50c13e86c9cdd17fb4b15f210fbf1e5064e
Felix Knischewski <fek@crosscan.com>
2015-11-23 10:14:46 +0100

    labeling_ui: fix scale tool for circles

--- labeling-ui/Application/Viewer/Tools/ShapeScaleTool.js
+++ labeling-ui/Application/Viewer/Tools/ShapeScaleTool.js
@@ -7,8 +7,9 @@ export default class ShapeScaleTool extends Tool {
   /**
    * @param {DrawingContext} drawingContext
    * @param {Object} options
+   * @param {Map} typeByPaperShapeId
    */
-  constructor(drawingContext, options) {
+  constructor(drawingContext, options, typeByPaperShapeId) {
     super(drawingContext, options);
     /**
      * Hit test result
@@ -18,12 +19,21 @@ export default class ShapeScaleTool extends Tool {
      */
     this._hitResult = null;
     /**
+     * Shape type of the hit result
+     *
+     * @type {string}
+     * @private
+     */
+    this._hitType = null;
+    /**
      * Variable that holds the modified state of the current rectangle
      *
      * @type {boolean}
      * @private
      */
     this._modified = false;
+
+    this._typeByPaperShapeId = typeByPaperShapeId;
 
     this._tool.onMouseDown = this._mouseDown.bind(this);
     this._tool.onMouseUp = this._mouseUp.bind(this);
@@ -43,9 +53,16 @@ export default class ShapeScaleTool extends Tool {
       if (hitResult) {
         this._hitResult = hitResult;
         this._hitResult.item.selected = true;
+        this._hitType = this._typeByPaperShapeId.get(hitResult.item.id);
 
         if (this._hitResult.type === 'bounds') {
-          this._scaleAnchor = this._getScaleAnchor(event.point, this._hitResult.item);
+          switch (this._hitType) {
+            case 'circle':
+              this._scaleAnchor = this._getCircleScaleAnchor(event.point, this._hitResult.item);
+              break;
+            default:
+              this._scaleAnchor = this._getScaleAnchor(event.point, this._hitResult.item);
+          }
         }
       } else {
         this._hitResult = null;
@@ -75,10 +92,16 @@ export default class ShapeScaleTool extends Tool {
   }
 
   _mouseDrag(event) {
-    if (!this._hitResult) return;
+    if (!this._hitResult || this._hitResult.type !== 'bounds') return;
 
     this._modified = true;
-    this._scale(this._hitResult.item, event.point);
+    switch (this._hitType) {
+      case 'circle':
+        this._scaleCircle(this._hitResult.item, event.point);
+        break;
+      default:
+        this._scale(this._hitResult.item, event.point);
+    }
   }
 
   /**
@@ -98,6 +121,19 @@ export default class ShapeScaleTool extends Tool {
     this._scaleAnchor = this._getScaleAnchor(dragPoint, item);
   }
 
+  _scaleCircle(item, dragPoint) {
+    const width = Math.abs(dragPoint.x - this._scaleAnchor.x) || 1;
+    const height = Math.abs(dragPoint.y - this._scaleAnchor.y) || 1;
+
+    const scaleX = width / item.bounds.width || 1;
+    const scaleY = height / item.bounds.height || 1;
+    const scale = Math.max(scaleX, scaleY);
+
+    item.scale(scale, scale, this._scaleAnchor);
+
+    this._scaleAnchor = this._getCircleScaleAnchor(dragPoint, item);
+  }
+
   _getScaleAnchor(dragHandle, item) {
     if (dragHandle.x > item.position.x && dragHandle.y > item.position.y) {
       return this._hitResult.item.bounds.topLeft;
@@ -112,5 +148,26 @@ export default class ShapeScaleTool extends Tool {
     }
 
     return this._hitResult.item.bounds.bottomLeft;
+  }
+
+  _getCircleScaleAnchor(dragHandle, item) {
+    const xDiff = Math.abs(dragHandle.x - item.position.x);
+    const yDiff = Math.abs(dragHandle.y - item.position.y);
+
+    if (xDiff > yDiff) {
+      // Scale on x axis
+      if (dragHandle.x > item.position.x) {
+        return new paper.Point(item.bounds.left, item.position.y);
+      } else {
+        return new paper.Point(item.bounds.right, item.position.y);
+      }
+    } else {
+      // Scale on y axis
+      if (dragHandle.y > item.position.y) {
+        return new paper.Point(item.position.x, item.bounds.top);
+      } else {
+        return new paper.Point(item.position.x, item.bounds.bottom);
+      }
+    }
   }
 }
