078242f4e0131860321459fcbc44bcd52fe8b1e2
Christian Hammerl <chh@crosscan.com>
2015-11-25 09:52:05 +0100

    terraform: remove jenkins node before starting the swarm plugin

--- terraform/openstack-jenkins/provision.sh
+++ terraform/openstack-jenkins/provision.sh
@@ -5,7 +5,7 @@ sudo dpkg -i puppetlabs-release-pc1-trusty.deb
 sudo apt-get update
 sudo apt-get -y upgrade
 sleep 3
-sudo aptitude -y install puppet-agent git openjdk-7-jre icedtea-7-plugin
+sudo aptitude -y install puppet-agent git openjdk-7-jre icedtea-7-plugin python-jenkins
 
 wget http://maven.jenkins-ci.org/content/repositories/releases/org/jenkins-ci/plugins/swarm-client/2.0/swarm-client-2.0-jar-with-dependencies.jar
 
@@ -18,8 +18,37 @@ sudo /opt/puppetlabs/bin/puppet apply \
 
 sudo gem2.0 install capistrano capistrano-scm-copy
 
-echo '#!/bin/bash' > jenkinsSwarmSlaveScreen.sh
-echo '/usr/bin/screen -dmS jenkins /usr/bin/java -Xmx1024m -XX:MaxPermSize=256M -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled -jar swarm-client-2.0-jar-with-dependencies.jar -username cho -password ef5a2b4ec677cbabd94ef3fc753922f2 -name `hostname` -labels "AnnoStation swarm PHP56 MySQL NodeJS CouchDB `hostname`" -master https://jenkins.crosscan.com/' >> jenkinsSwarmSlaveScreen.sh
+cat > jenkinsSwarmSlaveScreen.sh <<EOF
+#!/bin/bash
+
+# remove possibly existing node with same hostname first
+/usr/bin/python <<EoP
+import jenkins
+import platform
+
+nodename = platform.node()
+
+master = jenkins.Jenkins('https://jenkins.crosscan.com', 'cho', 'ef5a2b4ec677cbabd94ef3fc753922f2');
+
+if master.node_exists(nodename):
+        master.delete_node(nodename)
+EoP
+
+# run swarm plugin in screen session
+/usr/bin/screen -dmS jenkins \\
+    /usr/bin/java \\
+    -Xmx1024m \\
+    -XX:MaxPermSize=256M \\
+    -XX:+CMSClassUnloadingEnabled \\
+    -XX:+CMSPermGenSweepingEnabled \\
+    -jar swarm-client-2.0-jar-with-dependencies.jar \\
+    -username cho \\
+    -password ef5a2b4ec677cbabd94ef3fc753922f2 \\
+    -name `hostname` \\
+    -labels "AnnoStation swarm PHP56 MySQL NodeJS CouchDB `hostname`" \\
+    -master https://jenkins.crosscan.com/ \\
+    -disableClientsUniqueId
+EOF
 
 chmod +x jenkinsSwarmSlaveScreen.sh
 sudo mv jenkinsSwarmSlaveScreen.sh /usr/local/bin/jenkinsSwarmSlaveScreen.sh
