2c47cfb2ceb7e72c8db60b0db7c46c177dc73c1b
Jakob Westhoff <jakob@westhoffswelt.de>
2015-11-26 12:47:29 +0100

    Implemented StatusGateway

---
+++ labeling-ui/Application/Common/Interfaces/Status.js
@@ -0,0 +1,20 @@
+/**
+ * Interface describing a Status belonging to a {@link Job} processed by the backend
+ *
+ * @interface Status
+ */
+
+/**
+ * Current state of the {@link Job}
+ *
+ * Acceptable values are:
+ *
+ * - `success`
+ * - `waiting`
+ * - `running`
+ * - `error`
+ *
+ * @name Status#status
+ * @type {string}
+ */
+
