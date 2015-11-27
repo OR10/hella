c29081bfe187446ceadabae4f700875ffb63aa45
Christian Hammerl <chh@crosscan.com>
2015-11-27 13:13:33 +0100

    labeling_api: implement interpolation and status routes

---
+++ labeling-api/src/AppBundle/Controller/Api/Task/Interpolate.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AppBundle\Controller\Api\Task;
+
+use AppBundle\Database\Facade;
+use AppBundle\Model;
+use AppBundle\View;
+use AppBundle\Worker\Jobs;
+use crosscan\WorkerPool\AMQP;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpKernel\Exception;
+
+/**
+ * @Rest\Prefix("/api/task")
+ * @Rest\Route(service="annostation.labeling_api.controller.api.task.interpolate")
+ */
+class Interpolate
+{
+    /**
+     * @var Facade\LabelingTask
+     */
+    private $labelingTaskFacade;
+
+    /**
+     * @var Facade\LabeledThing
+     */
+    private $labeledThingFacade;
+
+    /**
+     * @var Facade\Status
+     */
+    private $statusFacade;
+
+    /**
+     * @var AMQP\FacadeAMQP
+     */
+    private $amqpFacade;
+
+    /**
+     * @param Facade\LabelingTask $labelingTaskFacade
+     * @param Facade\LabeledThing $labeledThingFacade
+     * @param AMQP\FacadeAMQP     $amqpFacade
+     */
+    public function __construct(
+        Facade\LabelingTask $labelingTaskFacade,
+        Facade\LabeledThing $labeledThingFacade,
+        Facade\Status $statusFacade,
+        AMQP\FacadeAMQP $amqpFacade
+    ) {
+        $this->labelingTaskFacade = $labelingTaskFacade;
+        $this->labeledThingFacade = $labeledThingFacade;
+        $this->statusFacade       = $statusFacade;
+        $this->amqpFacade         = $amqpFacade;
+    }
+
+    /**
+     * TODO: add support for offset/limit to restrict the frame range
+     *
+     * @Rest\Post("/{task}/interpolate/{labeledThing}")
+     *
+     * @param Model\LabelingTask     $task
+     * @param Model\LabeledThing     $labeledThing
+     * @param HttpFoundation\Request $request
+     */
+    public function startInterpolationAction(
+        Model\LabelingTask $task,
+        Model\LabeledThing $labeledThing,
+        HttpFoundation\Request $request
+    ) {
+        if ($labeledThing->getTaskId() !== $task->getId()) {
+            throw new Exception\BadRequestHttpException();
+        }
+
+        if (($algorithm = $request->request->get('type')) === null) {
+            throw new Exception\BadRequestHttpException();
+        }
+
+        $status = new Model\Interpolation\Status();
+        $this->statusFacade->save($status);
+        $job = new Jobs\Interpolation($labeledThing->getId(), $algorithm, $labeledThing->getFrameRange(), $status);
+        $this->amqpFacade->addJob($job);
+
+        return View\View::create()
+            ->setStatusCode(HttpFoundation\Response::HTTP_ACCEPTED)
+            ->setData([
+                'result' => [
+                    'id' => $status->getId(),
+                    'type' => str_replace('\\', '.', Model\Interpolation\Status::class),
+                ]
+            ]);
+    }
+}
---
+++ labeling-api/src/AppBundle/Worker/JobInstruction/Interpolation.php
@@ -0,0 +1,71 @@
+<?php
+namespace AppBundle\Worker\JobInstruction;
+
+use crosscan\Logger;
+use crosscan\WorkerPool;
+use crosscan\WorkerPool\Exception;
+use crosscan\WorkerPool\Job;
+use AppBundle\Database\Facade;
+use AppBundle\Model;
+use AppBundle\Service;
+use AppBundle\Worker\Jobs;
+
+class Interpolation extends WorkerPool\JobInstruction
+{
+    /**
+     * @var Service\Interpolation
+     */
+    private $interpolationService;
+
+    /**
+     * @var Facade\LabeledThing
+     */
+    private $labeledThingFacade;
+
+    /**
+     * @var Facade\Status
+     */
+    private $statusFacade;
+
+    /**
+     * @param Service\Interpolation $interpolationService
+     * @param Facade\LabeledThing   $labeledThingFacade
+     * @param Facade\Status         $statusFacade
+     */
+    public function __construct(
+        Service\Interpolation $interpolationService,
+        Facade\LabeledThing $labeledThingFacade,
+        Facade\Status $statusFacade
+    ) {
+        $this->interpolationService = $interpolationService;
+        $this->labeledThingFacade   = $labeledThingFacade;
+        $this->statusFacade         = $statusFacade;
+    }
+
+    /**
+     * @param Job                        $job
+     * @param Logger\Facade\LoggerFacade $logger
+     */
+    public function run(Job $job, Logger\Facade\LoggerFacade $logger)
+    {
+        if (!$job instanceof Jobs\Interpolation) {
+            throw new \RuntimeException("Invalid job type");
+        }
+
+        $labeledThing = $this->labeledThingFacade->find($job->getLabeledThingId());
+
+        // TODO: throw exception if labeledThing could not be found
+
+        // TODO: throw exception if status could not be found? or at least log sth about it?
+        $status = $job->getStatusId() !== null
+                ? $this->statusFacade->find(Model\Interpolation\Status::class, $job->getStatusId())
+                : null;
+
+        $this->interpolationService->interpolateForRange(
+            $job->getAlgorithm(),
+            $labeledThing,
+            $job->getFrameRange(),
+            $status
+        );
+    }
+}
