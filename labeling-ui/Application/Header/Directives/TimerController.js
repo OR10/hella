a93434e8f39d70e66df29be37e0d630d3d5a06ad
Felix Knischewski <fek@crosscan.com>
2015-12-02 17:20:23 +0100

    ui: timer counting

--- labeling-ui/Application/Header/Directives/TimerController.js
+++ labeling-ui/Application/Header/Directives/TimerController.js
@@ -2,17 +2,21 @@
  * Controller of the {@link TimerDirective}
  */
 class TimerController {
-  constructor() {
+  constructor($interval) {
     this.elapsedTime = 15360;
+    this.elapsedHours = Math.floor(this.elapsedTime / 3600);
+    this.elapsedMinutes = Math.floor((this.elapsedTime - (this.elapsedHours * 3600)) / 60);
+
+    $interval(this.interval.bind(this), 1000);
   }
 
-  getMinutes() {
-    return Math.floor((this.elapsedTime - (this.getHours() * 3600)) / 60);
-  }
-
-  getHours() {
-    return Math.floor(this.elapsedTime / 3600);
+  interval() {
+    this.elapsedTime++;
+    this.elapsedHours = Math.floor(this.elapsedTime / 3600);
+    this.elapsedMinutes = Math.floor((this.elapsedTime - (this.elapsedHours * 3600)) / 60);
   }
 }
+
+TimerController.$inject = ['$interval'];
 
 export default TimerController;
