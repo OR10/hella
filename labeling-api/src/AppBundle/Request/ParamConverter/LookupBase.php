45ad26445c9155bd877b885dff1ac4a874564558
Christian Hammerl <chh@crosscan.com>
2015-12-03 23:52:05 +0100

    labeling_api: add explaining comment for optional controller parameters

--- labeling-api/src/AppBundle/Request/ParamConverter/LookupBase.php
+++ labeling-api/src/AppBundle/Request/ParamConverter/LookupBase.php
@@ -9,6 +9,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter;
 use Symfony\Component\HttpKernel\Exception;
 use Symfony\Component\HttpFoundation;
 
+/**
+ * Tries to find the model object for a parameter.
+ *
+ * If the parameter is set to the id and the parameter is optional, the request
+ * attribute is set to `null` and the original value is stored in the
+ * attributes with the key `_unresolved<ParameterName>Id` with the first
+ * character of the parameter name converted to uppercase.
+ *
+ * Example:
+ *
+ *     public function fooAction(Request $request, SomeModel $model = null)
+ *     {
+ *         $originalValue = $request->attributes->get('_unresolvedModelId');
+ *     }
+ */
 abstract class LookupBase implements ParamConverter\ParamConverterInterface
 {
     public function apply(HttpFoundation\Request $request, Configuration\ParamConverter $configuration)
