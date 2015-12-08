a50f8626576231a7acc89853195913c71d15d65c
Jan Dworschak <jad@crosscan.com>
2015-12-08 14:40:26 +0100

    Refactored viewer
    Removed the viewer stage directive as it has become redundant

--- labeling-ui/Application/Viewer/Viewer.js
+++ labeling-ui/Application/Viewer/Viewer.js
@@ -1,7 +1,6 @@
 import Module from '../Module';
 
 import ViewerDirective from './Directives/ViewerDirective';
-import ViewerStageDirective from './Directives/ViewerStageDirective';
 
 import PaperShapeFactory from './Shapes/PaperShapeFactory';
 
@@ -17,7 +16,6 @@ export default class Viewer extends Module {
     this.module = angular.module('AnnoStation.Viewer', ['cfp.hotkeys', 'angularRangeSlider', 'ui.bootstrap.popover']);
 
     this.registerDirective('viewer', ViewerDirective);
-    this.registerDirective('viewerStage', ViewerStageDirective);
 
     this.module.service('paperShapeFactory', PaperShapeFactory);
 
