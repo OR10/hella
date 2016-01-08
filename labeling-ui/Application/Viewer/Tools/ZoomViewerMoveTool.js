11936705a0caf310ade8a59f5093b60d99f22e0f
Felix Knischewski <fek@crosscan.com>
2016-01-08 14:25:50 +0100

    ui: fix rectangle movement if viewer is not zoomed

--- labeling-ui/Application/Viewer/Tools/ZoomViewerMoveTool.js
+++ labeling-ui/Application/Viewer/Tools/ZoomViewerMoveTool.js
@@ -65,8 +65,40 @@ export default class ShapeMoveTool extends Tool {
 
     if (newPoint.x !== this._paperShape.position.x || newPoint.y !== this._paperShape.position.y) {
       this._paperShape.position = newPoint;
+      this._paperShape.position = this._restrictToViewport(this._paperShape.position, this._paperShape.bounds.width, this._paperShape.bounds.height);
       this.emit('shape:update', this._paperShape);
     }
   }
 
+
+  _restrictToViewport(point, width, height) {
+    this._context.withScope(scope => {
+      this._viewportDimensions = {
+        width: scope.view.viewSize.width,
+        height: scope.view.viewSize.height,
+      };
+    });
+
+    let correctedX = point.x;
+    let correctedY = point.y;
+
+    if (point.x - width / 2 < 0) {
+      correctedX = width / 2;
+    }
+
+    if (point.y - height / 2 < 0) {
+      correctedY = height / 2;
+    }
+
+    if (point.x + width / 2 > this._viewportDimensions.width) {
+      correctedX = this._viewportDimensions.width - width / 2;
+    }
+
+    if (point.y + height / 2 > this._viewportDimensions.height) {
+      correctedY = this._viewportDimensions.height - height / 2;
+    }
+
+    return new paper.Point(correctedX, correctedY);
+  }
 }
+
