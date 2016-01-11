c033b0087139286e2050d9a5cf034b32151f0a4d
Christian Hammerl <chh@crosscan.com>
2016-01-11 13:16:55 +0100

    puppet: install tideways for php7

--- labeling-api/puppet/modules/tideways/manifests/init.pp
+++ labeling-api/puppet/modules/tideways/manifests/init.pp
@@ -1,5 +1,17 @@
 class tideways(
+  $php7 = false,
 ) {
+  if $php7 {
+    $_packages = [
+      'tideways-daemon',
+    ]
+  } else {
+    $_packages = [
+      'tideways-php',
+      'tideways-daemon',
+    ]
+  }
+
   apt::source { 'tideways':
     location => 'http://s3-eu-west-1.amazonaws.com/qafoo-profiler/packages',
     release => 'debian',
@@ -10,13 +22,38 @@ class tideways(
     }
   }
 
-  $packages = [
-    'tideways-php',
-    'tideways-daemon',
-  ]
-
-  package { $packages:
+  package { $_packages:
     ensure => present,
     require => Apt::Source['tideways'],
+  }
+
+  if $php7 {
+    file { '/tmp/tideways-php_4.0.1_amd64.deb':
+      ensure => file,
+      source => 'puppet:///modules/tideways/tideways-php_4.0.1_amd64.deb',
+    }
+
+    package { 'tideways-php':
+      ensure => installed,
+      provider => dpkg,
+      source => '/tmp/tideways-php_4.0.1_amd64.deb',
+      require => [
+        File['/tmp/tideways-php_4.0.1_amd64.deb'],
+        Package[$_packages],
+      ],
+    }
+
+    file { '/etc/php/mods-available/tideways.ini':
+      ensure => link,
+      target => '/etc/php5/mods-available/tideways.ini',
+      require => Package['tideways-php'],
+    }
+
+    file { ['/etc/php/7.0/cli/conf.d/20-tideways.ini', '/etc/php/7.0/fpm/conf.d/20-tideways.ini']:
+      ensure => link,
+      target => '/etc/php/mods-available/tideways.ini',
+      require => File['/etc/php/mods-available/tideways.ini'],
+      notify => Service[$::php::fpm::service::service_name],
+    }
   }
 }
