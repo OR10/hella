b8dae73b8998e5de81abb533a6a3c9992281119f
Felix Knischewski <fek@crosscan.com>
2016-02-17 14:00:28 +0100

    ui: Hide other labeledThingsInFrame on selection

--- labeling-ui/Application/Header/Directives/ViewerTitleBarController.js
+++ labeling-ui/Application/Header/Directives/ViewerTitleBarController.js
@@ -141,6 +141,7 @@ class ViewerTitleBarController {
             const shape = labeledThingInFrame.paperShapes[0];
             this.selectedPaperShape = shape;
             this.selectedPaperShape.select();
+            this.hideLabeledThingsInFrame = true;
             this.thingLayer.update();
           });
         });
