b1e6cd4b2a6288eda5a5bfc0eea3dc91b0680a0f
Jakob Westhoff <jakob@westhoffswelt.de>
2016-03-31 14:34:38 +0200

    Adapted Viewer to frameIndex changes

--- labeling-ui/Application/Viewer/Directives/ViewerController.js
+++ labeling-ui/Application/Viewer/Directives/ViewerController.js
@@ -22,7 +22,7 @@ import ResizeSensor from 'css-element-queries/src/ResizeSensor';
  * @property {Viewport} viewport
  * @property {boolean} hideLabeledThingsInFrame
  * @property {string} newShapeDrawingTool
- * @property {integer} bookmarkedFrameNumber
+ * @property {integer} bookmarkedFrameIndex
  * @property {integer} fps
  * @property {integer} frameSkip
  * @property {ThingLayer} thingLayer
@@ -430,8 +430,8 @@ class ViewerController {
 
     $scope.$watchGroup(
       [
-        'vm.selectedPaperShape.labeledThingInFrame.labeledThing.frameRange.startFrameNumber',
-        'vm.selectedPaperShape.labeledThingInFrame.labeledThing.frameRange.endFrameNumber',
+        'vm.selectedPaperShape.labeledThingInFrame.labeledThing.frameRange.startFrameIndex',
+        'vm.selectedPaperShape.labeledThingInFrame.labeledThing.frameRange.endFrameIndex',
       ],
       ([newStart, newEnd], [oldStart, oldEnd]) => {
         if (this._currentFrameRemovedFromFrameRange(oldStart, newStart, oldEnd, newEnd)) {
@@ -559,16 +559,16 @@ class ViewerController {
     this.thingLayer.on('shape:new', shape => this._onNewShape(shape));
 
     this._debouncedOnShapeUpdate = this._debouncerService.multiplexDebounce(
-      (shape, frameNumber) => this._onUpdatedShape(shape, frameNumber),
-      (shape, frameNumber) => shape.labeledThingInFrame.ghost
-        ? `${frameNumber}.${shape.id}`
-        : `${shape.labeledThingInFrame.frameNumber}.${shape.id}`,
+      (shape, frameIndex) => this._onUpdatedShape(shape, frameIndex),
+      (shape, frameIndex) => shape.labeledThingInFrame.ghost
+        ? `${frameIndex}.${shape.id}`
+        : `${shape.labeledThingInFrame.frameIndex}.${shape.id}`,
       500
     );
 
     this.thingLayer.on('shape:update', shape => {
-      const frameNumber = this.framePosition.position;
-      this._debouncedOnShapeUpdate.debounce(shape, frameNumber);
+      const frameIndex = this.framePosition.position;
+      this._debouncedOnShapeUpdate.debounce(shape, frameIndex);
     });
 
     this._layerManager.addLayer('annotations', this.thingLayer);
@@ -643,10 +643,10 @@ class ViewerController {
    * The frame change includes things like loading all frame relevant data from the backend,
    * as well as propagating this information to all subcomponents
    *
-   * @param {int} frameNumber
+   * @param {int} frameIndex
    * @private
    */
-  _handleFrameChange(frameNumber) {
+  _handleFrameChange(frameIndex) {
     if (this._frameChangeInProgress) {
       this._logger.warn('ViewerController', 'frame change already in progress');
     }
@@ -656,9 +656,9 @@ class ViewerController {
     this.framePosition.lock.acquire();
     this._$q.all(
       [
-        this._backgroundBuffer.add(this._loadFrameImage(frameNumber)),
-        this._labeledThingInFrameBuffer.add(this._loadLabeledThingsInFrame(frameNumber)),
-        this._fetchGhostedLabeledThingInFrame(frameNumber),
+        this._backgroundBuffer.add(this._loadFrameImage(frameIndex)),
+        this._labeledThingInFrameBuffer.add(this._loadLabeledThingsInFrame(frameIndex)),
+        this._fetchGhostedLabeledThingInFrame(frameIndex),
       ]
     ).then(
       ([newFrameImage, labeledThingsInFrame, ghostedLabeledThingInFrame]) => {
@@ -709,11 +709,11 @@ class ViewerController {
   }
 
   /**
-   * @param {int} frameNumber
+   * @param {int} frameIndex
    * @returns {Promise.<LabeledThingInFrame|null>}
    * @private
    */
-  _fetchGhostedLabeledThingInFrame(frameNumber) {
+  _fetchGhostedLabeledThingInFrame(frameIndex) {
     if (this.selectedPaperShape === null) {
       return Promise.resolve(null);
     }
@@ -723,7 +723,7 @@ class ViewerController {
     return this._ghostedLabeledThingInFrameBuffer.add(
       this._labeledThingInFrameGateway.getLabeledThingInFrame(
         this.task,
-        frameNumber,
+        frameIndex,
         selectedLabeledThing
       )
     ).then(
@@ -742,14 +742,14 @@ class ViewerController {
   /**
    * Load all {@link LabeledThingInFrame} for a corresponding frame
    *
-   * The frameNumber is 1-Indexed
+   * The frameIndex is 0-Indexed
    *
-   * @param {int} frameNumber
+   * @param {int} frameIndex
    * @returns {AbortablePromise<LabeledThingInFrame[]>}
    * @private
    */
-  _loadLabeledThingsInFrame(frameNumber) {
-    return this._labeledThingInFrameGateway.listLabeledThingInFrame(this.task, frameNumber);
+  _loadLabeledThingsInFrame(frameIndex) {
+    return this._labeledThingInFrameGateway.listLabeledThingInFrame(this.task, frameIndex);
   }
 
   /**
@@ -775,45 +775,43 @@ class ViewerController {
   /**
    * Fetch the frame image corresponding to the given frame number
    *
-   * The frame number is 1-indexed
+   * The frameIndex is 0-indexed
    *
-   * @param frameNumber
+   * @param frameIndex
    * @returns {AbortablePromise<HTMLImageElement>}
    * @private
    */
-  _loadFrameImage(frameNumber) {
-    const relativeFrameNumber = frameNumber - this.task.frameRange.startFrameNumber;
-
+  _loadFrameImage(frameIndex) {
     return this._frameLocations.then(
-      frameLocations => this._frameGateway.getImage(frameLocations[relativeFrameNumber])
+      frameLocations => this._frameGateway.getImage(frameLocations[frameIndex])
     );
   }
 
   /**
    * @param {PaperShape} shape
-   * @param {Integer} frameNumber
+   * @param {Integer} frameIndex
    * @private
    */
-  _onUpdatedShape(shape, frameNumber) {
+  _onUpdatedShape(shape, frameIndex) {
     const labeledThingInFrame = shape.labeledThingInFrame;
     const labeledThing = labeledThingInFrame.labeledThing;
 
     if (labeledThingInFrame.ghost) {
       labeledThingInFrame.ghostBust(
         this._entityIdService.getUniqueId(),
-        frameNumber
+        frameIndex
       );
     }
 
     let frameRangeUpdated = false;
 
-    if (frameNumber > labeledThing.frameRange.endFrameNumber) {
-      labeledThing.frameRange.endFrameNumber = frameNumber;
+    if (frameIndex > labeledThing.frameRange.endFrameIndex) {
+      labeledThing.frameRange.endFrameIndex = frameIndex;
       frameRangeUpdated = true;
     }
 
-    if (frameNumber < labeledThing.frameRange.startFrameNumber) {
-      labeledThing.frameRange.startFrameNumber = frameNumber;
+    if (frameIndex < labeledThing.frameRange.startFrameIndex) {
+      labeledThing.frameRange.startFrameIndex = frameIndex;
       frameRangeUpdated = true;
     }
 
@@ -846,12 +844,12 @@ class ViewerController {
 
     this.activeTool = null;
 
-    this.bookmarkedFrameNumber = this.framePosition.position;
+    this.bookmarkedFrameIndex = this.framePosition.position;
   }
 
   _calculatePlaybackStartPosition() {
     if (this.selectedPaperShape && this.playbackSpeedFactor === 1) {
-      return this.selectedPaperShape.labeledThingInFrame.labeledThing.frameRange.startFrameNumber;
+      return this.selectedPaperShape.labeledThingInFrame.labeledThing.frameRange.startFrameIndex;
     }
 
     return this._frameIndexService.getFrameIndexLimits().lowerLimit;
@@ -859,7 +857,7 @@ class ViewerController {
 
   _calculatePlaybackEndPosition() {
     if (this.selectedPaperShape && this.playbackSpeedFactor === 1) {
-      const limitingProperty = this.playbackDirection === 'forwards' ? 'endFrameNumber' : 'startFrameNumber';
+      const limitingProperty = this.playbackDirection === 'forwards' ? 'endFrameIndex' : 'startFrameIndex';
       return this.selectedPaperShape.labeledThingInFrame.labeledThing.frameRange[limitingProperty];
     }
 
