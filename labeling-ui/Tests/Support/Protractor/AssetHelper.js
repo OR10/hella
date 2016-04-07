9c2441071ebaf716a7c8e5ae1a6e699d083f7cb7
Felix Knischewski <fek@crosscan.com>
2016-04-07 09:27:26 +0200

    ui: add e2e asset helper

---
+++ labeling-ui/Tests/Support/Protractor/AssetHelper.js
@@ -0,0 +1,84 @@
+import fs from 'fs';
+import path from 'path';
+
+class AssetHelper {
+  /**
+   * @param {string} fixturePath
+   * @param {string} mockPath
+   */
+  constructor(fixturePath, mockPath) {
+    /**
+     * @type {string}
+     * @private
+     */
+    this._fixturePath = fixturePath;
+
+    /**
+     * @type {string}
+     * @private
+     */
+    this._mockPath = mockPath;
+
+    /**
+     * @type {Object|null}
+     * @private
+     */
+    this._fixtureStructure = null;
+
+    /**
+     * @type {Object|null}
+     * @private
+     */
+    this._mockStructure = null;
+  }
+
+  /**
+   * @param {string} pathName
+   * @param {string} suffix
+   * @returns {Object}
+   * @private
+   */
+  _loadDirStructure(pathName, suffix) {
+    const structure = {};
+    const files = fs.readdirSync(pathName);
+
+    files.forEach(file => {
+      const filePath = path.resolve(`${pathName}/${file}`);
+      const stats = fs.statSync(filePath);
+      const key = path.basename(file, suffix).replace(/[^a-zA-Z0-9_]/g, '_');
+
+      if (stats.isDirectory()) {
+        structure[key] = this._loadDirStructure(filePath, suffix);
+      } else if (path.extname(filePath) === suffix) {
+        structure[key] = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      }
+    });
+
+
+    return structure;
+  }
+
+  /**
+   * @returns {Object}
+   */
+  get fixtures() {
+    if (this._fixtureStructure === null) {
+      this._fixtureStructure = this._loadDirStructure(this._fixturePath, '.json');
+    }
+
+    return this._fixtureStructure;
+  }
+
+  /**
+   * @returns {Object}
+   */
+  get mocks() {
+    if (this._mockStructure === null) {
+      this._mockStructure = this._loadDirStructure(this._mockPath, '.json');
+    }
+
+    return this._mockStructure;
+  }
+}
+
+export default AssetHelper;
