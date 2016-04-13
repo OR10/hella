5b1b0722869e79854c04488223a589658a78f777
Felix Knischewski <fek@crosscan.com>
2016-04-13 10:19:25 +0200

    ui: add tests

--- labeling-ui/Tests/Unit/Project/ProjectGateway.spec.js
+++ labeling-ui/Tests/Unit/Project/ProjectGateway.spec.js
@@ -33,6 +33,27 @@ describe('ProjectGateway', () => {
     expect(gateway instanceof ProjectGateway).toBe(true);
   });
 
+  it('should load a single project', (done) => {
+    const projectId = '7999cf8d8d5275330fa67fc69501d977';
+    const response = {
+      result: {
+        id: projectId,
+        name: 'example project',
+        taskCount: 1,
+        taskFinishedCount: 1,
+      },
+    };
+
+    $httpBackend.expectGET('/backend/api/project/7999cf8d8d5275330fa67fc69501d977').respond(response);
+
+    gateway.getProject(projectId).then(project => {
+      expect(project).toEqual(response.result);
+      done();
+    });
+
+    $httpBackend.flush();
+  });
+
   it('should load a list of projects', (done) => {
     const response = {
       result: [
@@ -55,6 +76,32 @@ describe('ProjectGateway', () => {
 
     gateway.getProjects().then(projects => {
       expect(projects).toEqual(response.result);
+      done();
+    });
+
+    $httpBackend.flush();
+  });
+
+  it('should load list of export for the project', (done) => {
+    const projectId = '7999cf8d8d5275330fa67fc69501d977';
+    const response = {
+      result: [{
+        id: 'bc23be6c5f427907b8b891ae1e06b805',
+        projectId: 'bc23be6c5f427907b8b891ae1e032543',
+        filename: 'csv_20160413_074730.zip',
+        taskIds: ['bc23be6c5f427907b8b891ae1e031ecf', 'bc23be6c5f427907b8b891ae1e030363'],
+      }, {
+        id: 'bc23be6c5f427907b8b891ae1e06b805',
+        projectId: 'bc23be6c5f427907b8b891ae1e032543',
+        filename: 'csv_20160413_074730.zip',
+        taskIds: ['bc23be6c5f427907b8b891ae1e030850', 'bc23be6c5f427907b8b891ae1e0315d3'],
+      }],
+    };
+
+    $httpBackend.expectGET('/backend/api/project/7999cf8d8d5275330fa67fc69501d977/export').respond(response);
+
+    gateway.getExports(projectId).then(project => {
+      expect(project).toEqual(response.result);
       done();
     });
 
