f9b5d02f50c6d8f3bf8da4d9ee6f521a6b651dfe
Felix Knischewski <fek@crosscan.com>
2016-04-13 10:06:12 +0200

    ui: update project gateway

--- labeling-ui/Application/Project/Gateways/ProjectGateway.js
+++ labeling-ui/Application/Project/Gateways/ProjectGateway.js
@@ -4,11 +4,21 @@
 class ProjectGateway {
   /**
    * @param {ApiService} apiService
-   * @param {angular.$http} $http
+   * @param {angular.bufferedHttp} bufferedHttp
    */
-  constructor(apiService, $http) {
-    this._$http = $http;
+  constructor(apiService, bufferedHttp) {
+    this._bufferedHttp = bufferedHttp;
     this._apiService = apiService;
+  }
+
+  /**
+   * @returns {Promise<Project>}
+   */
+  getProject(projectId) {
+    const url = this._apiService.getApiUrl(`/project/${projectId}`);
+
+    return this._bufferedHttp.get(url, undefined, 'export')
+      .then(response => response.data.result);
   }
 
   /**
@@ -17,11 +27,42 @@ class ProjectGateway {
   getProjects() {
     const url = this._apiService.getApiUrl('/project');
 
-    return this._$http.get(url)
+    return this._bufferedHttp.get(url, undefined, 'export')
       .then(response => response.data.result);
+  }
+
+  /**
+   * @returns {Promise<Export>}
+   */
+  getExports(projectId) {
+    const url = this._apiService.getApiUrl(`/project/${projectId}/export`);
+
+    return this._bufferedHttp.get(url, undefined, 'export')
+      .then(response => response.data.result);
+  }
+
+  /**
+   * Starts export for the given {@link Project}
+   *
+   * @param {string} projectId
+   * @param {string} exportType
+   *
+   * @returns {AbortablePromise<string|Error>}
+   */
+  startExport(projectId, exportType = 'csv') {
+    const url = this._apiService.getApiUrl(`/project/${projectId}/export/${exportType}`);
+
+    return this._bufferedHttp.post(url, {}, undefined, 'export')
+      .then(response => {
+        if (response.data && response.data.message) {
+          return response.data.message;
+        }
+
+        throw new Error('Failed starting export');
+      });
   }
 }
 
-ProjectGateway.$inject = ['ApiService', '$http'];
+ProjectGateway.$inject = ['ApiService', 'bufferedHttp'];
 
 export default ProjectGateway;
