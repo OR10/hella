e6acfd3d86c4296cbd529f7268d90d149b32a7d5
Jakob Westhoff <jakob@westhoffswelt.de>
2016-05-18 15:10:58 +0200

    mend

--- labeling-ui/Application/ThirdDimension/Support/Projection3dFlatWorld.js
+++ labeling-ui/Application/ThirdDimension/Support/Projection3dFlatWorld.js
@@ -1,4 +1,5 @@
 import {Vector3, Vector4, Matrix4} from 'three-math';
+
 import CameraCalibration from '../Models/CameraCalibration';
 
 /**
@@ -14,82 +15,23 @@ class Projection3dFlatWorld {
      * @private
      */
     this._calibration = new CameraCalibration(calibrationObject);
-
-    const {x, y, z} = this._calibration.translation;
-
-    /**
-     * @type {Matrix4}
-     * @protected
-     */
-    this._translationMatrix = new Matrix4();
-    this._translationMatrix.makeTranslation(-x, -y, -z);
-
-    /**
-     * @type {Matrix4}
-     * @protected
-     */
-    this._viewMatrix = new Matrix4();
-    this._viewMatrix
-      .multiply(this._calibration.rotationMatrix)
-      .multiply(this._translationMatrix);
   }
 
   /**
+   * Project a 2d cuboid into 3d vehicle coordinates using the flat world assumption
    *
-   * @param {THREE.Vector4} inVector
-   * @return {THREE.Vector3}
+   * The Array is supposed to contain all cuboid points clockwise of the front and then back side
+   * The resulting array as the same order
+   *
+   * Only full cuboids can be transformed, as we need information about the corrdinate position relative to the cuboid
+   * to apply the flat world assumption
+   *
+   * @param {Array.<THREE.Vector3>} vertices
+   * @return {Array.<THREE.Vector4>}
    */
-  project3dTo2d(inVector) {
-    return this._applyCameraMatrix(
-      this._applyDistortion(
-        this._projectIntoCameraPlane(
-          this._transformIntoCameraSpace(
-            inVector
-          )
-        )
-      )
-    );
-  }
-
-  _transformIntoCameraSpace(inVector) {
-    const cameraVector = inVector.clone();
-    cameraVector.applyMatrix4(this._viewMatrix);
-    return cameraVector;
-  }
-
-  _projectIntoCameraPlane(inVector) {
-    const {x, y, z} = inVector;
-    const projectedVector = new Vector3();
-    projectedVector.set(
-      x / z,
-      y / z,
-      1
-    );
-
-    return projectedVector;
-  }
-
-  _applyDistortion(inVector) {
-    const {x, y} = inVector;
-    const [k1, k2] = this._calibration.distortionCoefficients;
-
-    const r2 = Math.pow(x, 2) + Math.pow(y, 2);
-    const r4 = Math.pow(r2, 2);
-
-    const undistortedVector = new Vector3(
-      x + x * (k1 * r2 + k2 * r4),
-      y + y * (k1 * r2 + k2 * r4),
-      1
-    );
-
-    return undistortedVector;
-  }
-
-  _applyCameraMatrix(inVector) {
-    const intrinsicVector = inVector.clone();
-    intrinsicVector.applyMatrix4(this._calibration.cameraMatrix);
-    return intrinsicVector;
+  project2dCuboidInto3dCuboid(vertices) {
+    
   }
 }
 
-export default Projection2d;
+export default Projection3dFlatWorld;
