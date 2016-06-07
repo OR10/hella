743f7819d6418679b4b678253a76232562c03879
Jakob Westhoff <jakob@westhoffswelt.de>
2016-06-07 16:21:09 +0200

    Tested Crosshairs drawing

---
+++ labeling-ui/Tests/E2E/CrosshairsDrawing.spec.js
@@ -0,0 +1,153 @@
+import mock from 'protractor-http-mock';
+import CanvasInstructionLogManager from '../Support/CanvasInstructionLogManager';
+import {getMockRequestsMade, initApplication} from '../Support/Protractor/Helpers';
+import AssetHelper from '../Support/Protractor/AssetHelper';
+
+const canvasInstructionLogManager = new CanvasInstructionLogManager(browser);
+
+describe('Crosshairs drawing', () => {
+  let assets;
+  let sharedMocks;
+  let viewer;
+  let crosshairsToggleButton;
+
+  beforeEach(() => {
+    assets = new AssetHelper(`${__dirname}/../Fixtures`, `${__dirname}/../ProtractorMocks`);
+    sharedMocks = [
+      assets.mocks.Shared.UserProfile,
+      assets.mocks.Shared.UserPermissions,
+      assets.mocks.Shared.Task,
+      assets.mocks.Shared.Video,
+      assets.mocks.Shared.LabelStructure,
+      assets.mocks.Shared.GetTimer,
+      assets.mocks.Shared.PutTimer,
+      assets.mocks.Shared.LabeledThingIncompleteCount,
+      assets.mocks.Shared.FrameLocations.SourceJpg.frameIndex0,
+      assets.mocks.Shared.FrameLocations.SourceJpg.frameIndex0to4,
+      assets.mocks.Shared.FrameLocations.Thumbnail.frameIndex0,
+      assets.mocks.Shared.FrameLocations.Thumbnail.frameIndex0to4,
+    ];
+
+    viewer = element(by.css('.layer-container'));
+    crosshairsToggleButton = element(by.css('.task-bar-right button .fa-crosshairs')).element(by.xpath('..'));
+  });
+
+  it('should not draw crosshair if disabled', (done) => {
+    mock(sharedMocks.concat([
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0,
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0to4,
+    ]));
+
+    initApplication('/labeling/task/TASKID-TASKID')
+      .then(() => {
+          browser.actions()
+            .mouseMove(viewer, {x: 200, y: 200})
+            .perform();
+      })
+      .then(
+        () => canvasInstructionLogManager.getCrosshairsCanvasLogs()
+      )
+      .then((drawingStack) => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CrosshairsDrawing.NoCrosshairs);
+        done();
+      });
+  });
+
+  it('should draw crosshair if enabled top left', (done) => {
+    mock(sharedMocks.concat([
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0,
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0to4,
+    ]));
+
+    initApplication('/labeling/task/TASKID-TASKID')
+      .then(() => {
+        browser.actions()
+          .click(crosshairsToggleButton)
+          .mouseMove(viewer, {x: 20, y: 20})
+          .perform();
+      })
+      .then(
+        // () => canvasInstructionLogManager.getCrosshairsCanvasLogs('CrosshairsDrawing', 'TopLeft')
+        () => canvasInstructionLogManager.getCrosshairsCanvasLogs()
+      )
+      .then((drawingStack) => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CrosshairsDrawing.TopLeft);
+        done();
+      });
+  });
+
+  it('should draw crosshair if enabled center', (done) => {
+    mock(sharedMocks.concat([
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0,
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0to4,
+    ]));
+
+    initApplication('/labeling/task/TASKID-TASKID')
+      .then(() => {
+        browser.actions()
+          .click(crosshairsToggleButton)
+          .mouseMove(viewer, {x: 1024 / 2, y: 620 / 2})
+          .perform();
+      })
+      .then(
+        // () => canvasInstructionLogManager.getCrosshairsCanvasLogs('CrosshairsDrawing', 'Center')
+        () => canvasInstructionLogManager.getCrosshairsCanvasLogs()
+      )
+      .then((drawingStack) => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CrosshairsDrawing.Center);
+        done();
+      });
+  });
+
+  it('should draw crosshair if enabled bottom right', (done) => {
+    mock(sharedMocks.concat([
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0,
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0to4,
+    ]));
+
+    initApplication('/labeling/task/TASKID-TASKID')
+      .then(() => {
+        browser.actions()
+          .click(crosshairsToggleButton)
+          .mouseMove(viewer, {x: 1024 - 20, y: 620 - 20})
+          .perform();
+      })
+      .then(
+        // () => canvasInstructionLogManager.getCrosshairsCanvasLogs('CrosshairsDrawing', 'BottomRight')
+        () => canvasInstructionLogManager.getCrosshairsCanvasLogs()
+      )
+      .then((drawingStack) => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CrosshairsDrawing.BottomRight);
+        done();
+      });
+  });
+
+  it('should not draw crosshair if enabled and mousecursor outside of viewer', (done) => {
+    mock(sharedMocks.concat([
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0,
+      assets.mocks.CrosshairsDrawing.Shared.LabeledThingInFrame.frameIndex0to4,
+    ]));
+
+    initApplication('/labeling/task/TASKID-TASKID')
+      .then(() => {
+        browser.actions()
+          .click(crosshairsToggleButton)
+          .mouseMove(viewer, {x: 200, y: 200})
+          .perform();
+        browser.actions()
+          .mouseMove(viewer, {x: 100, y: -50})
+          .perform();
+      })
+      .then(
+        () => canvasInstructionLogManager.getCrosshairsCanvasLogs()
+      )
+      .then((drawingStack) => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CrosshairsDrawing.NoCrosshairs);
+        done();
+      });
+  });
+
+  afterEach(() => {
+    mock.teardown();
+  });
+});
--- labeling-ui/Tests/Support/CanvasInstructionLogManager.js
+++ labeling-ui/Tests/Support/CanvasInstructionLogManager.js
@@ -10,14 +10,46 @@ class CanvasInstructionLogManager {
   }
 
   /**
-   * @param {Boolean} createFixture
    * @returns {Promise<String>}
+   * @param {string|null} testName
+   * @param {string|null} fixtureName
    */
   getCanvasLogs(testName = null, fixtureName = null) {
     this._browser.waitForAngular();
 
     return this._browser.executeScript(() => {
       const canvas = document.getElementsByClassName('annotation-layer')[0];
+      const context = canvas.getContext('2d');
+      const width = canvas.width;
+      const height = canvas.height;
+
+      return {
+        width,
+        height,
+        operations: context.json({
+          decimalPoints: 8,
+        }),
+      };
+    }).then((obj) => {
+      obj.operations = JSON.parse(obj.operations);
+
+      if (testName !== null && fixtureName !== null) {
+        this._createFixture(testName, fixtureName, obj);
+      }
+      return obj;
+    });
+  }
+
+  /**
+   * @returns {Promise<String>}
+   * @param {string|null} testName
+   * @param {string|null} fixtureName
+   */
+  getCrosshairsCanvasLogs(testName = null, fixtureName = null) {
+    this._browser.waitForAngular();
+
+    return this._browser.executeScript(() => {
+      const canvas = document.getElementsByClassName('crosshairs-layer')[0];
       const context = canvas.getContext('2d');
       const width = canvas.width;
       const height = canvas.height;
