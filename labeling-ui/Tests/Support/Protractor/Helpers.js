ad2b615c02b6b2a85410331211c989c4b9f45361
Jakob Westhoff <jakob@westhoffswelt.de>
2016-06-14 15:05:40 +0200

    Implemented request dump helper

--- labeling-ui/Tests/Support/Protractor/Helpers.js
+++ labeling-ui/Tests/Support/Protractor/Helpers.js
@@ -17,6 +17,29 @@ export function getMockRequestsMade(mock) {
   });
 }
 
+export function dumpAllRequestsMade(mock) {
+  return mock.allRequestsMade().then(requests => {
+    const strippedRequests = requests.map(request => {
+      const strippedRequest = {
+        method: request.method,
+        path: request.url,
+      };
+
+      if (request.data) {
+        strippedRequest.data = request.data;
+      }
+      
+      return strippedRequest;
+    });
+
+   console.log( // eslint-disable-line no-console
+      `The following requests were made against the backend. Not all of them may have been mocked!\n${JSON.stringify(strippedRequests, undefined, 2)}`
+    );
+
+    // fail('Dumping all requests causes automatic test fail.');
+    return Promise.reject(new Error('Dumping all requests causes automatic test fail.'));
+  });
+}
 
 function waitForApplicationReady() {
   return browser.executeAsyncScript((next) => {
