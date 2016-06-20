cebfb3d1a5c82b3c33d40221ea5c070a269a294f
Christian Hammerl <chh@crosscan.com>
2016-06-20 09:16:45 +0200

    terraform: add security group for couchdb

--- terraform/stackone-labeltool/security_groups.tf
+++ terraform/stackone-labeltool/security_groups.tf
@@ -9,3 +9,15 @@ resource "openstack_compute_secgroup_v2" "frame-cdn" {
         cidr = "0.0.0.0/0"
     }
 }
+
+resource "openstack_compute_secgroup_v2" "couchdb" {
+    name = "anno.couchdb"
+    description = "Annostation CouchDB"
+
+    rule {
+        from_port = 5984
+        to_port = 5984
+        ip_protocol = "tcp"
+        cidr = "0.0.0.0/0"
+    }
+}
