3171ef8034b884f346b4bb88f9fb43da43b868af
Jakob Westhoff <jakob@westhoffswelt.de>
2016-06-21 10:38:21 +0200

    Fixed Cuboid2d constructor

--- labeling-ui/Application/ThirdDimension/Models/Cuboid2d.js
+++ labeling-ui/Application/ThirdDimension/Models/Cuboid2d.js
@@ -6,9 +6,31 @@ class Cuboid2d {
    * @param {Array.<Array.<number>>} vertices
    * @param {Array.<boolean>} vertexVisibility
    */
-  constructor(vertices, vertexVisibility = [true, true, true, true, true, true, true, true]) {
+  constructor(vertices,
+              vertexVisibility = [true, true, true, true, true, true, true, true]) {
+    /**
+     * @type {Array.<Array.<number>>}
+     * @private
+     */
     this._vertices = vertices;
+
+    /**
+     * @type {Array.<boolean>}
+     * @private
+     */
     this._vertexVisibility = vertexVisibility;
+  }
+
+  /**
+   * Clone the current {@link Cuboid2d} object
+   *
+   * @returns {Cuboid2d}
+   */
+  clone() {
+    return new Cuboid2d(
+      this._vertices,
+      this._vertexVisibility
+    );
   }
 
   get vertices() {
@@ -94,7 +116,10 @@ class Cuboid2d {
  * @returns {Cuboid2d}
  */
 Cuboid2d.createFromCuboid2dAndVectors = (cuboid2d, vertices) => {
-  return new Cuboid2d(vertices.map(vertex => [vertex.x, vertex.y]), cuboid2d.primaryVertices, cuboid2d.vertexVisibility);
+  return new Cuboid2d(
+    vertices.map(vertex => [vertex.x, vertex.y]),
+    cuboid2d.vertexVisibility
+  );
 };
 
 export default Cuboid2d;
