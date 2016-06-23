0f7e798630e3d4df8144a62cc04a33a13ca473cc
Jakob Westhoff <jakob@westhoffswelt.de>
2016-06-23 14:46:53 +0200

    Adapted new fat-arrow parents codingstyle for Common

--- labeling-ui/Application/Common/Common.js
+++ labeling-ui/Application/Common/Common.js
@@ -87,7 +87,7 @@ class Common extends Module {
         },
       ]);
 
-    this.module.run(['$rootScope', '$location', ($rootScope) => {
+    this.module.run(['$rootScope', '$location', $rootScope => {
       $rootScope.$on('unauthorized', () => {
         const targetUrl = encodeURIComponent(window.location.pathname + window.location.hash);
         window.location.assign(`/login?targetUrl=${targetUrl}`);
