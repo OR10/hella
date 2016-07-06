b21a1964108a0bbf6a095d272d35f69a4246eca0
Jakob Westhoff <jakob@westhoffswelt.de>
2016-07-06 16:43:55 +0200

    Removed initentional test focus

--- labeling-ui/Tests/E2E/CuboidDrawing.spec.js
+++ labeling-ui/Tests/E2E/CuboidDrawing.spec.js
@@ -9,7 +9,7 @@ import AssetHelper from '../Support/Protractor/AssetHelper';
 
 const canvasInstructionLogManager = new CanvasInstructionLogManager(browser);
 
-fdescribe('Cuboid', () => {
+describe('Cuboid', () => {
   let assets;
   let sharedMocks;
   let viewer;
