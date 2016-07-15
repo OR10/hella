5bdc74db6f247ab3d0fdcdefd88085a485ac3692
Felix Knischewski <fek@crosscan.com>
2016-07-15 13:47:28 +0200

    Add docblocks

--- labeling-ui/Application/ManagementBoard/Controllers/TasksController.js
+++ labeling-ui/Application/ManagementBoard/Controllers/TasksController.js
@@ -2,10 +2,25 @@
  * Controller for the initial entrypoint route into the application
  */
 class TasksController {
+  /**
+   * @param {$stateParams} $stateParams
+   * @param {User} user
+   * @param {UserPermissions} userPermissions
+   */
   constructor($stateParams, user, userPermissions) {
+    /**
+     * @type {User}
+     */
     this.user = user;
+
+    /**
+     * @type {UserPermissions}
+     */
     this.userPermissions = userPermissions;
 
+    /**
+     * @type {string}
+     */
     this.projectId = $stateParams.projectId;
   }
 }
