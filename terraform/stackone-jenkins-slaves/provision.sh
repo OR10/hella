b1e29a6cdb7805427ae0b44028d3ce94e67fbd3e
Christian Hammerl <chh@crosscan.com>
2016-07-18 13:54:56 +0200

    terraform: stackone-jenkins-slaves: remove deprecated jenkins swarm slave startup script

--- terraform/stackone-jenkins-slaves/provision.sh
+++ terraform/stackone-jenkins-slaves/provision.sh
@@ -22,42 +22,8 @@ sudo \
 
 sudo gem2.0 install capistrano capistrano-scm-copy
 
-cat > jenkinsSwarmSlaveScreen.sh <<EOF
-#!/bin/bash
-
-# remove possibly existing node with same hostname first
-/usr/bin/python <<EoP
-import jenkins
-import platform
-
-nodename = platform.node()
-
-master = jenkins.Jenkins('https://jenkins.crosscan.com', 'cho', 'ef5a2b4ec677cbabd94ef3fc753922f2');
-
-if master.node_exists(nodename):
-        master.delete_node(nodename)
-EoP
-
-# run swarm plugin in screen session
-/usr/bin/screen -dmS jenkins \\
-    /usr/bin/java \\
-    -Xmx1024m \\
-    -XX:MaxPermSize=256M \\
-    -XX:+CMSClassUnloadingEnabled \\
-    -XX:+CMSPermGenSweepingEnabled \\
-    -jar swarm-client-2.0-jar-with-dependencies.jar \\
-    -username cho \\
-    -password ef5a2b4ec677cbabd94ef3fc753922f2 \\
-    -name `hostname` \\
-    -labels "AnnoStation swarm PHP7 MySQL NodeJS CouchDB ElasticSearch `hostname`" \\
-    -master https://jenkins.crosscan.com \\
-    -disableClientsUniqueId
-EOF
-
-chmod +x jenkinsSwarmSlaveScreen.sh
-sudo mv jenkinsSwarmSlaveScreen.sh /usr/local/bin/jenkinsSwarmSlaveScreen.sh
-
 echo '@reboot sleep 30 && /usr/local/bin/jenkinsSwarmSlaveScreen.sh' | crontab -u ubuntu -
 
 sleep 3
+
 sudo reboot
