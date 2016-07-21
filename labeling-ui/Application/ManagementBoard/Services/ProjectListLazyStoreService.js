9bfeaa016b475e036732c6ee5d284e44fcec3377
Jakob Westhoff <jakob@westhoffswelt.de>
2016-07-21 09:40:26 +0200

    Added `projectStatus` filter to `ProjectGateway`

--- labeling-ui/Application/ManagementBoard/Services/ProjectListLazyStoreService.js
+++ labeling-ui/Application/ManagementBoard/Services/ProjectListLazyStoreService.js
@@ -12,24 +12,16 @@ class ProjectListLazyStoreService extends AbstractLazyStoreService {
   getProjects(status, limit, offset, force = false) {
     return this._lazyFetch(
       `projects-${status}-${limit}-${offset}`,
-      () => this._projectGateway.getProjects(limit, offset),
+      () => this._projectGateway.getProjects(status, limit, offset),
       30 * 1000,
       force
-    ).then(data => {
-      // Store the maxCount independently
-      this._storeFetchedData(
-        `totalRows-${status}`,
-        data.totalRows
-      );
-
-      return data;
-    });
+    );
   }
 
   getProjectCount(status, force = false) {
     return this._lazyFetch(
       `totalRows-${status}`,
-      () => this._projectGateway.getProjects(0, 0).then(data => data.totalRows),
+      () => this._projectGateway.getProjects(status, 0, 0).then(data => data.totalRows),
       30 * 1000,
       force
     );
