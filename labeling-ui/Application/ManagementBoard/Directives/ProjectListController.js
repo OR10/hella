bf99785912e2a60429ee35525a4a97232ebf1dae
Jakob Westhoff <jakob@westhoffswelt.de>
2016-07-21 21:34:26 +0200

    Migrated ProjectList to new pagination-table

--- labeling-ui/Application/ManagementBoard/Directives/ProjectListController.js
+++ labeling-ui/Application/ManagementBoard/Directives/ProjectListController.js
@@ -1,24 +1,14 @@
-import ProjectActionCellTemplate from '../Views/Grid/ProjectActionButtonCell.html!';
-import DateCellTemplate from '../Views/Grid/DateCell.html!';
-import ClickableCellTemplate from '../Views/Grid/ClickableCell.html!';
-import Environment from '../../Common/Support/Environment';
+import moment from 'moment';
 
 /**
  * Controller of the {@link ProjectListDirective}
  */
 class ProjectListController {
   /**
-   * @param {$scope} $scope
    * @param {$state} $state
    * @param {ProjectGateway} projectGateway
    */
-  constructor($scope, $state, projectGateway) {
-    /**
-     * @type {$rootScope.$scope}
-     * @private
-     */
-    this._$scope = $scope;
-
+  constructor($state, projectGateway) {
     /**
      * @type {$state}
      * @private
@@ -37,202 +27,142 @@ class ProjectListController {
     this.projects = [];
 
     /**
-     * @type {{pageNumber: number, pageSize: number}}
+     * @type {Array}
      */
-    this._paginationOptions = {
-      pageNumber: 1,
-      pageSize: 5,
-    };
+    this.columns = [];
+
+    /**
+     * @type {number}
+     */
+    this.totalRows = 0;
 
     /**
      * @type {boolean}
      */
     this.loadingInProgress = false;
-
-    this.projectGridOptions = {
-      paginationPageSize: 5,
-      paginationPageSizes: [5, 10, 50],
-      useExternalPagination: true,
-      enableColumnMenus: false,
-      enableSorting: false,
-      gridMenuShowHideColumns: false,
-      columnDefs: [],
-      onRegisterApi: gridApi => {
-        gridApi.pagination.on.paginationChanged($scope, (newPage, pageSize) => {
-          this._paginationOptions.pageNumber = newPage;
-          this._paginationOptions.pageSize = pageSize;
-          this._loadProjects();
-        });
-      },
-      data: this.projects,
-    };
-
-    this._loadProjects();
   }
 
-  rowClick(rowEntity) {
-    this._$state.go('labeling.tasks.list', {projectId: rowEntity.id});
-  }
-
-  _loadProjects() {
+  updatePage(page, itemsPerPage) {
     this.loadingInProgress = true;
-    const limit = this._paginationOptions.pageSize;
-    const offset = (this._paginationOptions.pageNumber - 1) * limit;
+
+    const limit = itemsPerPage;
+    const offset = (page - 1) * itemsPerPage;
 
     this._projectGateway.getProjects(this.projectStatus, limit, offset)
       .then(response => {
-        this._buildColumnDefs(response.result[0]);
-        this._filterData(response.result);
+        this.totalRows = response.totalRows;
 
-        this.projectGridOptions.data = this.projects;
-        this.projectGridOptions.totalItems = response.totalRows;
+        if (response.result.length === 0) {
+          this.columns = [];
+          this.projects = [];
+          return;
+        }
+
+        this.projects = this._createViewData(response.result);
+        this.columns = this._buildColumns(this.projects[0]);
+
         this.loadingInProgress = false;
       });
   }
 
-  _buildColumnDefs(project) {
-    const defs = [
-      {field: 'id', visible: false},
-    ];
-
-    if (!project) {
-      return;
-    }
-
-    if (project.hasOwnProperty('status')) {
-      defs.push({displayName: 'Status',
-        field: 'status',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('name')) {
-      defs.push({displayName: 'Name',
-        field: 'name',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('videoCount')) {
-      defs.push({displayName: 'Video Count',
-        field: 'videoCount',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('taskCount')) {
-      defs.push({displayName: 'Task count',
-        field: 'taskCount',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('taskInProgressCount')) {
-      defs.push({
-        displayName: 'Task in progress count',
-        field: 'taskInProgressCount',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('taskFinishedCount')) {
-      defs.push({displayName: 'Task done count',
-        field: 'taskFinishedCount',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('taskFinishedCount') && project.hasOwnProperty('taskCount')) {
-      defs.push({displayName: '% finished',
-        field: 'percentage',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('objectFrameCount')) {
-      defs.push({displayName: 'Object frames',
-        field: 'objectFrameCount',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('timeInProject')) {
-      defs.push({displayName: 'Time spent in project',
-        field: 'timeInProject',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: ClickableCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('creationTimestamp')) {
-      defs.push({
-        displayName: 'Start date',
-        field: 'creationTimestamp',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: DateCellTemplate,
-      });
-    }
-
-    if (project.hasOwnProperty('dueTimestamp')) {
-      defs.push({
-        displayName: 'Due date',
-        field: 'dueTimestamp',
-        width: '*',
-        enableSorting: false,
-        cellTemplate: DateCellTemplate,
-      });
-    }
-
-    if (this._showActionColumn()) {
-      defs.push({
-        name: 'actions',
-        width: 200,
-        enablePinning: true,
-        pinnedRight: true,
-        cellTemplate: ProjectActionCellTemplate,
-      });
-    }
-
-    this.projectGridOptions.columnDefs = defs;
+  /**
+   * @param {number} projectId
+   */
+  openProject(projectId) {
+    this._$state.go('labeling.tasks.list', {projectId});
   }
 
-  _showActionColumn() {
-    // TODO: fix
-    return true;
+  /**
+   * @param {number} projectId
+   */
+  exportProject(projectId) {
+    // @TODO: Implement
   }
 
-  _filterData(projects) {
-    this.projects = projects.map(project => {
-      project.percentage = Math.round((project.taskFinishedCount / project.taskCount) * 100) + '%';
-      project.creationTimestamp = project.creationTimestamp * 1000;
-      project.dueTimestamp = project.dueTimestamp ? project.dueTimestamp * 1000 : '';
-      return project;
+  /**
+   * @param {number} projectId
+   */
+  deleteProject(projectId) {
+    // @TODO: Implement
+  }
+
+  /**
+   * @param {number} projectId
+   */
+  closeProject(projectId) {
+    // @TODO: Implement
+  }
+
+  /**
+   * @param {number} projectId
+   */
+  reopenProject(projectId) {
+    // @TODO: Implement
+  }
+
+  /**
+   * @param {number} projectId
+   */
+  openReport(projectId) {
+    // @TODO: Implement
+  }
+
+  _buildColumns(row) {
+    const columns = [];
+
+    const propertyToColumnMap = {
+      'statusFormatted': 'Status',
+      'name': 'Name',
+      'videoCount': 'Video Count',
+      'taskCount': 'Task Count',
+      'taskInProgressCount': 'In Progress #',
+      'taskFinishCount': 'Finished #',
+      'percentage': '% finished',
+      'objectFrameCount': 'Object frames',
+      'timeInProject': 'Time spent',
+      'creationTimestampFormatted': 'Started',
+      'dueTimestampFormatted': 'Due date',
+    };
+
+    Object.keys(propertyToColumnMap).forEach(
+      property => {
+        if (row.hasOwnProperty(property)) {
+          columns.push({name: propertyToColumnMap[property], property});
+        }
+      }
+    );
+
+    return columns;
+  }
+
+  _createViewData(projects) {
+    const augmentedMapping = {
+      'statusFormatted': project => {
+        switch (project.status) {
+          case 'todo':
+            return 'Todo';
+          case 'in_progress':
+            return 'In Progress';
+          case 'done':
+            return 'Done';
+        }
+      },
+      'percentage': project => Math.round((project.taskFinishedCount / project.taskCount) * 100) + '%',
+      'creationTimestampFormatted': project => moment.unix(project.creationTimestamp).format('DD.MM.YYYY'),
+      'dueTimestampFormatted': project => moment.unix(project.dueTimestamp).format('DD.MM.YYYY'),
+    };
+
+    return projects.map(project => {
+      const augmentedObject = Object.assign({}, project);
+      Object.keys(augmentedMapping).forEach(
+        property => augmentedObject[property] = augmentedMapping[property](project)
+      );
+      return augmentedObject;
     });
-
-    this.projectGridOptions.data = this.projects;
   }
 }
 
 ProjectListController.$inject = [
-  '$scope',
   '$state',
   'projectGateway',
 ];
