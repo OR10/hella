7a076d36420252384e8adfabd2457bafcdf7f016
Jakob Westhoff <jakob@westhoffswelt.de>
2016-08-02 11:41:10 +0200

    Fixed eslint errors

--- labeling-ui/Application/Task/Model/Task.js
+++ labeling-ui/Application/Task/Model/Task.js
@@ -1,4 +1,4 @@
-import cloneDeep from 'lodash.clonedeep'
+import cloneDeep from 'lodash.clonedeep';
 
 /**
  * Primary Task model
@@ -147,7 +147,7 @@ class Task {
           return -1;
         } else if (firstAssignedAt === secondAssignedAt) {
           return 0;
-        } else {
+        } else { // eslint-disable-line no-else-return
           return 1;
         }
       }
@@ -168,7 +168,7 @@ class Task {
     return this._users[userId];
   }
 
-  /**#
+  /**
    * @param {string} phase
    * @returns {User|null}
    */
