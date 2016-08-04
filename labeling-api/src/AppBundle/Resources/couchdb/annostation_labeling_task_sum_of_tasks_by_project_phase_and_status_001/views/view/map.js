7e75ec7ed07525361efd1acc1b577c27dd5ea17c
Christian Tischer <cht@crosscan.com>
2016-08-04 16:08:03 +0200

    Added new CouchDb view annostation_labeling_task_sum_of_tasks_by_project_phase_and_status_001

---
+++ labeling-api/src/AppBundle/Resources/couchdb/annostation_labeling_task_sum_of_tasks_by_project_phase_and_status_001/views/view/map.js
@@ -0,0 +1,15 @@
+function(doc) {
+    if (doc.type !== 'AppBundle.Model.LabelingTask') {
+        return;
+    }
+
+    if (typeof doc.status === 'object') {
+        Object.keys(doc.status).forEach(function(phase) {
+            emit([doc.projectId, phase, doc.status.labeling], 1);
+        });
+    } else {
+        // Migration of status from string to object
+        // See AppBundle\Migration\LabelingTaskStatusPhase
+        emit([doc.projectId, 'labeling', doc.status.labeling], 1);
+    }
+}
