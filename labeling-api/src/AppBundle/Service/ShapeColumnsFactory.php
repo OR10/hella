c6d7156f3650b23d8fcc7d0a4d62c96aa6290cd6
Jakob Westhoff <jakob@westhoffswelt.de>
2016-08-11 14:42:15 +0200

    Implemented ShapeColumnsFactory service

---
+++ labeling-api/src/AppBundle/Service/ShapeColumnsFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Service;
+
+use AppBundle\Helper\Export;
+
+class ShapeColumnsFactory
+{
+    /**
+     * @var Export\ShapeColumns[]
+     */
+    private $drawingToolToShapeColumnsMapping;
+
+    public function __constructor(array $drawingToolToShapeColumnsMapping) {
+        $this->drawingToolToShapeColumnsMapping = $drawingToolToShapeColumnsMapping;
+    }
+
+    /**
+     * Create an array of Columns needed to represent shapes of the given drawingTool type
+     *
+     * @param string $drawingTool
+     *
+     * @return Export\Column[]
+     */
+    public function create($drawingTool) {
+        if (!array_key_exists($drawingTool, $this->drawingToolToShapeColumnsMapping)) {
+            throw new \RuntimeException('Unable to find ShapeColumns mapping for ' . $drawingTool);
+        }
+
+        $shapeColumns = $this->drawingToolToShapeColumnsMapping[$drawingTool];
+        return $shapeColumns->create();
+    }
+}
