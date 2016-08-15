3ce1ef84168c8387797a4bf2453e51c03bd5bd10
Christian Hammerl <chh@crosscan.com>
2016-08-15 18:46:07 +0200

    terraform: add state of anno-demo system

---
+++ puppet/environments/syseleven_anno_demo/manifests/site.pp
@@ -0,0 +1,13 @@
+Class['apt::update'] -> Package<| title != 'apt-transport-https' and title != 'ca-certificates' |>
+
+include ::apt
+include ::annostation_base
+include ::labeling_api
+
+file { ['/etc/AnnoStation', '/etc/AnnoStation/labeling-api']:
+  ensure  => directory,
+}
+
+node /^anno\-demo(\.*|$)/ {
+    include ::php
+}
