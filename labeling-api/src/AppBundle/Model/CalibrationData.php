d32bd12b0e7b3cb7bd03da866c144477e18c5444
Christian Hammerl <chh@crosscan.com>
2016-08-18 18:47:27 +0200

    labeling-api: add model for calibration data

---
+++ labeling-api/src/AppBundle/Model/CalibrationData.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace AppBundle\Model;
+
+use Doctrine\ODM\CouchDB\Mapping\Annotations as CouchDB;
+
+/**
+ * @CouchDB\Document
+ */
+class CalibrationData
+{
+    /**
+     * @CouchDB\Id
+     */
+    private $id;
+
+    /**
+     * @CouchDB\Field(type="string")
+     */
+    private $name;
+
+    /**
+     * @CouchDB\Field(type="mixed")
+     */
+    private $rawCalibration;
+
+    /**
+     * @CouchDB\Field(type="mixed")
+     */
+    private $calibration;
+
+    /**
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRawCalibration()
+    {
+        return $this->rawCalibration;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCalibration()
+    {
+        return $this->calibration;
+    }
+
+    /**
+     * @param mixed $rawCalibration
+     */
+    public function setRawCalibration($rawCalibration)
+    {
+        $this->rawCalibration = $rawCalibration;
+    }
+
+    /**
+     * @param mixed $cameraMatrix
+     */
+    public function setCameraMatrix($cameraMatrix)
+    {
+        $this->calibration['cameraMatrix'] = $cameraMatrix;
+    }
+
+    /**
+     * @param mixed $rotationMatrix
+     */
+    public function setRotationMatrix($rotationMatrix)
+    {
+        $this->calibration['rotationMatrix'] = $rotationMatrix;
+    }
+
+    /**
+     * @param mixed $translation
+     */
+    public function setTranslation($translation)
+    {
+        $this->calibration['translation'] = $translation;
+    }
+
+    /**
+     * @param mixed $distortionCoefficients
+     */
+    public function setDistortionCoefficients($distortionCoefficients)
+    {
+        $this->calibration['distortionCoefficients'] = $distortionCoefficients;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCameraMatrix()
+    {
+        return $this->calibration['cameraMatrix'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRotationMatrix()
+    {
+        return $this->calibration['rotationMatrix'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTranslation()
+    {
+        return $this->calibration['translation'];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDistortionCoefficients()
+    {
+        return $this->calibration['distortionCoefficients'];
+    }
+}
