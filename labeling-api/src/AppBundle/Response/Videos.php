6067c2a995bf9dbfc844abbdcc9ee1ee1674ebb4
Christian Tischer <cht@crosscan.com>
2016-08-19 09:48:37 +0200

    Added new Video Response

---
+++ labeling-api/src/AppBundle/Response/Videos.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Response;
+
+use AppBundle\Model;
+use AppBundle\Database\Facade;
+
+class Videos
+{
+    /**
+     * @var array
+     */
+    private $result = [];
+
+    /**
+     * @param Model\Video            $videos []
+     * @param Facade\CalibrationData $calibrationDataFacade
+     */
+    public function __construct(
+        $videos,
+        Facade\CalibrationData $calibrationDataFacade
+    )
+    {
+        $result = [];
+        foreach ($videos as $video) {
+            $videoResult = new Video($video, $calibrationDataFacade);
+            $result[] = $videoResult->getResult();
+        }
+
+        $this->result = $result;
+    }
+}
