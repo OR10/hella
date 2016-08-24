b6665087f869cd58399bcd41d9078a14dd1de308
Felix Knischewski <fek@crosscan.com>
2016-08-24 10:25:37 +0200

    Add color coded roles to titlebar +  tooltip so show users roles

--- labeling-ui/Application/Header/Directives/UserNameController.js
+++ labeling-ui/Application/Header/Directives/UserNameController.js
@@ -3,6 +3,33 @@
  */
 class UserNameController {
   constructor() {
+    /**
+     * @type {Array}
+     */
+    this.readableRoles = [];
+
+    this.user.roles.forEach(role => {
+      switch (role) {
+        case 'ROLE_ADMIN':
+          this.readableRoles.push('Admin');
+          break;
+        case 'ROLE_LABEL_COORDINATOR':
+          this.readableRoles.push('Label Coordinator');
+          break;
+        case 'ROLE_CLIENT':
+          this.readableRoles.push('Client');
+          break;
+        case 'ROLE_USER':
+          this.readableRoles.push('User');
+          break;
+        default:
+      }
+    });
+
+    /**
+     * @type {string}
+     */
+    this.roleTooltipText = 'You have the following roles: ' + this.readableRoles.join(', ');
   }
 }
 
