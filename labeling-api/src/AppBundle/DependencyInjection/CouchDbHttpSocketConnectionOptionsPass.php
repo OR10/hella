d1ad3fea9f3af990b683269f707fe803a70731a2
Jakob Westhoff <jakob@westhoffswelt.de>
2016-09-05 13:41:08 +0200

    Added explanation for CouchDbHttpSocketConnectionOptionsPass

--- labeling-api/src/AppBundle/DependencyInjection/CouchDbHttpSocketConnectionOptionsPass.php
+++ labeling-api/src/AppBundle/DependencyInjection/CouchDbHttpSocketConnectionOptionsPass.php
@@ -8,11 +8,25 @@ use Doctrine\CouchDB\HTTP\SocketClient;
 use Symfony\Component\DependencyInjection;
 use Symfony\Component\DependencyInjection\Compiler;
 
+/**
+ * Compiler pass to configure the doctrine couchdb connection
+ *
+ * This should hopefully decrease response times of couchdb access by nearly a factor of 10-15. After a long debugging
+ * session we realized there seems to be a problem with continous `fgets` calls on a long lasting connection.
+ *
+ * On Requests, which transfer a lot of rows this problem still exists, but this fix should at least increase the
+ * speed for all of the smaller queries drastically.
+ *
+ * Until now we have seen response times around 40ms as a minimum for simple couchdb requests. Those requests now take
+ * between 1-3ms. (Measured against localhost connections). Eventhough external connection establishing is more costly.
+ * This should nevertheless give us a noticable speed bump.
+ */
 class CouchDbHttpSocketConnectionOptionsPass implements Compiler\CompilerPassInterface
 {
     public static $couchdbDefaultConnectionServiceId = 'doctrine_couchdb.client.default_connection';
 
-    public static function configureClient(CouchDBClient $client) {
+    public static function configureClient(CouchDBClient $client)
+    {
         $httpClient = $client->getHttpClient();
 
         if ($httpClient instanceof LoggingClient) {
@@ -35,6 +49,8 @@ class CouchDbHttpSocketConnectionOptionsPass implements Compiler\CompilerPassInt
         }
 
         $definition = $container->getDefinition(static::$couchdbDefaultConnectionServiceId);
-        $definition->setConfigurator(['AppBundle\\DependencyInjection\\CouchDbHttpSocketConnectionOptionsPass', 'configureClient']);
+        $definition->setConfigurator(
+            ['AppBundle\\DependencyInjection\\CouchDbHttpSocketConnectionOptionsPass', 'configureClient']
+        );
     }
 }
