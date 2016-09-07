261c36026f8398f42fee6f2a6049abe9834e5fa1
Jakob Westhoff <jakob@westhoffswelt.de>
2016-09-07 17:00:41 +0200

    Implemented AccessChecks for projects

---
+++ labeling-api/src/AppBundle/Voter/AccessCheck/ClientIsProjectCreator.php
@@ -0,0 +1,26 @@
+<?php
+namespace AppBundle\Voter\AccessCheck;
+
+use AppBundle\Model;
+use AppBundle\Voter;
+
+class ClientIsProjectCreator extends Voter\AccessCheck
+{
+    /**
+     * @param Model\User $user
+     * @param object     $object
+     *
+     * @return bool
+     */
+    public function userHasAccessToObject(Model\User $user, $object): bool
+    {
+        if (!($object instanceof Model\Project)) {
+            throw new \RuntimeException('Project AccessCheck got non Project as object.');
+        }
+
+        return (
+            $user->hasRole(Model\User::ROLE_CLIENT) &&
+            $object->getUserId() === $user->getId()
+        );
+    }
+}
