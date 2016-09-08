3bd7b9aedfa3c29f5b9213bb38de3ac1017cacff
Christian Tischer <cht@crosscan.com>
2016-09-08 07:57:36 +0200

    Added new couchdb view

---
+++ labeling-api/src/AppBundle/Resources/couchdb/annostation_labeling_task_by_project_id_class_and_frame_number_001/views/view/map.js
@@ -0,0 +1,9 @@
+function(doc) {
+    if (!doc.type === 'AppBundle.Model.LabeledThingInFrame') {
+        return;
+    }
+
+    doc.classes.forEach(function(class) {
+        emit([doc.projectId, class, doc.frameIndex], 1);
+    });
+}
