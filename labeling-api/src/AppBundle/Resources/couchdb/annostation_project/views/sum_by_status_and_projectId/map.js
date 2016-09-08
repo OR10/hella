6733b3a9a5045d582a256a9e36b3845da1cfed7f
Christian Tischer <cht@crosscan.com>
2016-09-08 13:44:24 +0200

    Improved couchdb view

--- labeling-api/src/AppBundle/Resources/couchdb/annostation_project/views/sum_by_status_and_projectId/map.js
+++ labeling-api/src/AppBundle/Resources/couchdb/annostation_project/views/sum_by_status_and_projectId/map.js
@@ -1,5 +1,18 @@
 function(doc) {
     if (doc.type === 'AppBundle.Model.Project') {
-        emit([doc.status, doc._id], 1);
+        if (typeof doc.status === 'object') {
+            var statusHistory = [];
+            doc.status.forEach(function (history) {
+                statusHistory.push(history);
+            });
+
+            statusHistory.sort(function (a, b) {
+                return b.timestamp - a.timestamp;
+            });
+
+            emit([statusHistory[0].status, doc._id], 1);
+        }else{
+            emit([doc.status, doc._id], 1);
+        }
     }
 }
