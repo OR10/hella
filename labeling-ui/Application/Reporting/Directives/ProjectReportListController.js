f43e0d92afa2b6d24d95c816d1cbd517df006595
Jakob Westhoff <jakob@westhoffswelt.de>
2016-09-09 14:36:03 +0200

    Only allow access to finished reports

--- labeling-ui/Application/Reporting/Directives/ProjectReportListController.js
+++ labeling-ui/Application/Reporting/Directives/ProjectReportListController.js
@@ -3,11 +3,50 @@
  */
 class ProjectReportListController {
   /**
+   * @param {$state} $state
+   * @param {ModalService} modalService
    */
-  constructor() {
+  constructor($state, modalService) {
+    /**
+     * @type {$state}
+     * @private
+     */
+    this._$state = $state;
+
+    /**
+     * @type {ModalService}
+     * @private
+     */
+    this._modalService = modalService;
+  }
+
+  openReport(id) {
+    const report = this.reports.find(possibleReport => possibleReport.id === id);
+
+    if (report === null || report.reportStatus !== 'done') {
+      const modal = this._modalService.getAlertWarningDialog({
+        title: 'Report generation in progress',
+        headline: 'This report is still being generated.',
+        message: 'The requested report is still being generated for you. Please check back in a few moments, when its ready for consumption.',
+        confirmButtonText: 'Understood',
+      });
+      modal.activate();
+      return;
+    }
+
+    this._$state.go(
+      'labeling.reporting.show',
+      {
+        projectId: report.projectId,
+        reportId: report.id,
+      }
+    );
   }
 }
 
-ProjectReportListController.$inject = [];
+ProjectReportListController.$inject = [
+  '$state',
+  'modalService',
+];
 
 export default ProjectReportListController;
