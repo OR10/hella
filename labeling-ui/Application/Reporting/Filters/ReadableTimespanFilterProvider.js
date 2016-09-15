cacc6b8fa2ef1a91b6c85ec13efc86e995907ec4
Christian Tischer <cht@crosscan.com>
2016-09-15 07:34:12 +0200

    Fixed missing day calculation

--- labeling-ui/Application/Reporting/Filters/ReadableTimespanFilterProvider.js
+++ labeling-ui/Application/Reporting/Filters/ReadableTimespanFilterProvider.js
@@ -2,10 +2,14 @@ function ReadableTimespanFilterProvider() {
   return function readableTimespan(timespanInSeconds) {
     const hours = parseInt(timespanInSeconds / 3600, 10) % 24;
     const minutes = parseInt(timespanInSeconds / 60, 10) % 60;
-    if (hours === 0) {
-      return `${minutes}m`;
+    const days = parseInt(timespanInSeconds / 86400, 10) % 86400;
+    if (days > 0) {
+      return `${days}d ${hours}h ${minutes}m`;
     }
-    return `${hours}h ${minutes}m`;
+    if (hours > 0) {
+      return `${hours}h ${minutes}m`;
+    }
+    return `${minutes}m`;
   };
 }
 
