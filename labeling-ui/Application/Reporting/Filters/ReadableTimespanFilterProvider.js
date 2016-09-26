7e5ea4a7eba92b719957e86d935b6e88f00bdcfa
Felix Knischewski <fek@crosscan.com>
2016-09-26 13:38:46 +0200

    Only show hour ans minutes in report view spent time column

--- labeling-ui/Application/Reporting/Filters/ReadableTimespanFilterProvider.js
+++ labeling-ui/Application/Reporting/Filters/ReadableTimespanFilterProvider.js
@@ -2,10 +2,6 @@ function ReadableTimespanFilterProvider() {
   return function readableTimespan(timespanInSeconds) {
     const hours = parseInt(timespanInSeconds / 3600, 10) % 24;
     const minutes = parseInt(timespanInSeconds / 60, 10) % 60;
-    const days = parseInt(timespanInSeconds / 86400, 10) % 86400;
-    if (days > 0) {
-      return `${days}d ${hours}h ${minutes}m`;
-    }
     if (hours > 0) {
       return `${hours}h ${minutes}m`;
     }
