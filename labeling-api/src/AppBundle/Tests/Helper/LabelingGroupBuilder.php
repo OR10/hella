763c343b7b6ff83d76a85677a62590ac2da20ff6
Christian Tischer <cht@crosscan.com>
2016-10-13 10:15:14 +0200

    Added new helper builder for LabelingGroups

---
+++ labeling-api/src/AppBundle/Tests/Helper/LabelingGroupBuilder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Tests\Helper;
+
+use AppBundle\Model;
+
+/**
+ * Helper class to create LabelingGroup.
+ */
+class LabelingGroupBuilder
+{
+    /**
+     * @var array
+     */
+    private $users = [];
+
+    /**
+     * @var array
+     */
+    private $coordinators = [];
+
+    /**
+     * @return LabelingGroupBuilder
+     */
+    public static function create()
+    {
+        $labelingGroupBuilder = new self();
+
+        return $labelingGroupBuilder;
+    }
+
+    public function withUsers(array $users)
+    {
+        $this->users = $users;
+
+        return $this;
+    }
+
+    public function withCoordinators(array $coordinators)
+    {
+        $this->coordinators = $coordinators;
+
+        return $this;
+    }
+
+    /**
+     * @return Model\LabelingGroup
+     */
+    public function build()
+    {
+        $labelingGroup = Model\LabelingGroup::create($this->coordinators, $this->users);
+
+        return $labelingGroup;
+    }
+}
