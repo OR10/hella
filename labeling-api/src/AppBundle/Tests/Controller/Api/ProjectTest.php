c4eae887065a234140479892fa0d53d2760f71a1
Christian Tischer <cht@crosscan.com>
2016-10-14 07:47:29 +0200

    fixed test

--- labeling-api/src/AppBundle/Tests/Controller/Api/ProjectTest.php
+++ labeling-api/src/AppBundle/Tests/Controller/Api/ProjectTest.php
@@ -427,7 +427,7 @@ class ProjectTest extends Tests\WebTestCase
         $project = $this->projectFacade->save($projectBuilder);
 
         $requestWrapper = $this->createRequest('/api/project/%s/delete', [$project->getId()])
-            ->setMethod(HttpFoundation\Request::METHOD_POST)
+            ->setMethod(HttpFoundation\Request::METHOD_DELETE)
             ->withCredentialsFromUsername($this->client)
             ->execute();
 
