d043af618b1a864b2721f27525778147fa3b9b46
Felix Knischewski <fek@crosscan.com>
2016-10-19 15:42:09 +0200

    Move remote logger registration to application controller an add user to remote logging

--- labeling-ui/Application/ApplicationController.js
+++ labeling-ui/Application/ApplicationController.js
@@ -1,9 +1,15 @@
-class LabelingController {
+import Environment from './Common/Support/Environment';
+import RemoteLogger from './Common/Loggers/RemoteLogger';
+
+class ApplicationController {
   /**
    * @param {angular.Scope} $scope
    * @param {angular.$location} $location
+   * @param {LoggerService} loggerService
+   * @param {LogGateway} logGateway
+   * @param {User} user
    */
-  constructor($scope, $location) {
+  constructor($scope, $location, loggerService, logGateway, user) {
     /**
      * @type {angular.Scope}
      * @private
@@ -11,12 +17,19 @@ class LabelingController {
     this._$scope = $scope;
 
     this._$location = $location;
+
+    if (!Environment.isDevelopment && !Environment.isTesting && !Environment.isFunctionalTesting) {
+      loggerService.addLogger(new RemoteLogger(logGateway, user));
+    }
   }
 }
 
-LabelingController.$inject = [
+ApplicationController.$inject = [
   '$scope',
   '$location',
+  'loggerService',
+  'logGateway',
+  'user',
 ];
 
-export default LabelingController;
+export default ApplicationController;
