69240f9629a20f6ddae8304ce14f9f389867d7ab
Felix Knischewski <fek@crosscan.com>
2016-11-14 14:06:02 +0100

    Fix readableTimespanFilter

--- labeling-ui/Application/Reporting/Filters/ReadableTimespanFilterProvider.js
+++ labeling-ui/Application/Reporting/Filters/ReadableTimespanFilterProvider.js
@@ -1,6 +1,6 @@
 function ReadableTimespanFilterProvider() {
   return function readableTimespan(timespanInSeconds) {
-    const hours = parseInt(timespanInSeconds / 3600, 10) % 24;
+    const hours = parseInt(timespanInSeconds / 3600, 10);
     const minutes = parseInt(timespanInSeconds / 60, 10) % 60;
     if (hours > 0) {
       return `${hours}h ${minutes}m`;
