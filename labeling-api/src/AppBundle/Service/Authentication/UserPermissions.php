26ac96ce713c29488ccb528cac74fb85d3a5427e
Christian Tischer <cht@crosscan.com>
2016-11-14 09:34:30 +0100

    Added new permission `canMoveTaskInOtherPhase`

--- labeling-api/src/AppBundle/Service/Authentication/UserPermissions.php
+++ labeling-api/src/AppBundle/Service/Authentication/UserPermissions.php
@@ -44,6 +44,7 @@ class UserPermissions
         'canReopenTask'                  => [Model\User::ROLE_ADMIN,                          Model\User::ROLE_LABEL_COORDINATOR                          ],
         'canFlagLabelingTask'            => [Model\User::ROLE_ADMIN,                          Model\User::ROLE_LABEL_COORDINATOR, Model\User::ROLE_LABELER],
         'canUnflagLabelingTask'          => [Model\User::ROLE_ADMIN,                          Model\User::ROLE_LABEL_COORDINATOR                          ],
+        'canMoveTaskInOtherPhase'        => [                                                 Model\User::ROLE_LABEL_COORDINATOR                          ],
 
         // User management
         'canEditLabelingGroups'          => [Model\User::ROLE_ADMIN                                                                                       ],
