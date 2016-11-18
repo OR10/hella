e0d6512a18d57fa043ab474b75f1325cac1b6cbc
Felix Knischewski <fek@crosscan.com>
2016-11-18 15:48:39 +0100

    Add CouchDbModelDeserializer

---
+++ labeling-ui/Application/Common/Services/CouchDbModelDeserializer.js
@@ -0,0 +1,53 @@
+import LabeledThingInFrame from 'Application/LabelingData/Models/LabeledThingInFrame';
+
+/**
+ * The CouchDbModelDeserializer is capable of converting CouchDb document into our internal models.
+ *
+ * It does not implement the serialization of models into their CouchDb Documents.
+ */
+class CouchDbModelDeserializer {
+  /**
+   * Deserialize a labeledThingInFrame to our internal model
+   *
+   * @param {*} labeledThingInFrame
+   * @param {*} labeledThing
+   */
+  deserializeLabeledThingInFrame(labeledThingInFrame, labeledThing) {
+    this._removePrefixFromIdAndRevision(labeledThing);
+    this._removePrefixFromIdAndRevision(labeledThingInFrame);
+
+    labeledThingInFrame.labeledThing = labeledThing;
+
+    return new LabeledThingInFrame(labeledThingInFrame);
+  }
+
+  /**
+   * Remove the prefix the `_id` and `_rev` properties in the given document
+   *
+   * The default prefix is the *underscore*, which CouchDb sets by default.
+   *
+   * If `_id` or `_rev` are not present, they will be silently ignored.
+   *
+   * @param {object} document
+   * @param {string?} prefix
+   * @private
+   */
+  _removePrefixFromIdAndRevision(document, prefix = '_') {
+    console.log(document[`${prefix}id`], document.id);
+    if (document[`${prefix}id`] !== undefined && typeof document[`${prefix}id`] === 'string') {
+      document.id = document[`${prefix}id`];
+      delete document[`${prefix}id`];
+    }
+
+    if (document[`${prefix}rev`] !== undefined && typeof document[`${prefix}rev`] === 'string') {
+      document.rev = document[`${prefix}rev`];
+      delete document[`${prefix}rev`];
+    }
+  }
+}
+
+CouchDbModelDeserializer.TYPE_LABELED_THING_IN_FRAME = 'AppBundle.Model.LabeledThingInFrame';
+
+CouchDbModelDeserializer.$inject = [];
+
+export default CouchDbModelDeserializer;
