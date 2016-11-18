04100d8abf04e0f195becb57e5f4f83731fd7479
Felix Knischewski <fek@crosscan.com>
2016-11-18 16:12:25 +0100

    Add PoudbLabeledThingInFrameGateway

---
+++ labeling-ui/Application/LabelingData/Gateways/PouchDbLabeledThingInFrameGateway.js
@@ -0,0 +1,249 @@
+import LabeledThing from '../Models/LabeledThingInFrame';
+
+/**
+ * Gateway for CRUD operation on {@link LabeledThing}s in a PouchDb
+ */
+class PouchDbLabeledThingInFrameGateway {
+  /**
+   * @param {StorageContextService} storageContextService
+   * @param {RevisionManager} revisionManager
+   * @param {PackagingExecutor} packagingExecutor
+   * @param {CouchDbModelSerializer} couchDbModelSerializer
+   * @param {CouchDbModelDeserializer} couchDbModelDeserializer
+   * @param {PouchDbLabeledThingGateway} pouchLabeledThingGateway
+   */
+  constructor(storageContextService, revisionManager, packagingExecutor, couchDbModelSerializer, couchDbModelDeserializer, pouchLabeledThingGateway) {
+    /**
+     * @type {StorageContextService}
+     * @private
+     */
+    this._storageContextService = storageContextService;
+
+    /**
+     * @type {RevisionManager}
+     * @private
+     */
+    this._revisionManager = revisionManager;
+
+    /**
+     * @type {PackagingExecutor}
+     * @private
+     */
+    this._packagingExecutor = packagingExecutor;
+
+    /**
+     * @type {CouchDbModelSerializer}
+     * @private
+     */
+    this._couchDbModelSerializer = couchDbModelSerializer;
+
+    /**
+     * @type {CouchDbModelDeserializer}
+     * @private
+     */
+    this._couchDbModelDeserializer = couchDbModelDeserializer;
+
+    /**
+     * @type {PouchDbLabeledThingGateway}
+     * @private
+     */
+    this._pouchLabeledThingGateway = pouchLabeledThingGateway;
+  }
+
+  /**
+   * Returns the {@link LabeledThingInFrame} object for the given {@link Task} and `frameIndex`
+   *
+   * @param {Task} task
+   * @param {Number} frameIndex
+   * @param {Number} offset
+   * @param {Number} limit
+   *
+   * @returns {AbortablePromise<LabeledThingInFrame[]|Error>}
+   */
+  listLabeledThingInFrame(task, frameIndex, offset = 0, limit = 1) {
+    const startkey = frameIndex + offset;
+    const endkey = frameIndex + offset + limit - 1;
+
+    const db = this._storageContextService.provideContextForTaskId(task.id);
+
+    this._packagingExecutor.execute('labeledThingInFrame', () => {
+      db.query('annostation_labeled_thing_in_frame/by_taskId_frameIndex', {
+        startkey,
+        endkey,
+        include_docs: true,
+      }).then(result => {
+        if (result.rows) {
+          const docs = [];
+          result.rows.forEach(row => {
+            if (row.doc) {
+              const labeledThingInFrame = row.doc;
+              const labeledThing = this._pouchLabeledThingGateway.getLabeledThing(labeledThingInFrame);
+
+              docs.push(this._couchDbModelDeserializer.deserializeLabeledThingInFrame(labeledThingInFrame, labeledThing));
+            }
+
+            throw new Error('Row does not contain a document');
+          });
+
+          return docs;
+        }
+
+        throw new Error('Failed loading labeled thing in frame list');
+      });
+    });
+  }
+
+
+  /**
+   * Retrieve a {@link LabeledThingInFrame} which is associated to a specific
+   * {@link Task}, {@link LabeledThing} and `frameIndex`.
+   *
+   * If the `LabeledThingInFrame` does not exist in the database an interpolated ghost frame is returned
+   *
+   * Optionally an `offset` and `limit` may be specified, which relates to the specified `frameIndex`.
+   * By default `offset = 0` and `limit = 1` is assumed.
+   *
+   * @param {Task} task
+   * @param {int} frameIndex
+   * @param {LabeledThing} labeledThing
+   * @param {int?} offset
+   * @param {int?} limit
+   */
+  getLabeledThingInFrame(task, frameIndex, labeledThing, offset = 0, limit = 1) {
+    const startkey = [labeledThing.id, frameIndex + offset];
+    const endkey = [labeledThing.id, frameIndex + offset + limit - 1];
+
+    const db = this._storageContextService.provideContextForTaskId(task.id);
+
+    this._packagingExecutor.execute('labeledThingInFrame', () => {
+      db.query('annostation_labeled_thing_in_frame/by_labeledThingId_frameIndex', {
+        startkey,
+        endkey,
+        include_docs: true,
+      }).then(result => {
+        if (result.rows) {
+          const docs = [];
+          result.rows.forEach(row => {
+            if (row.doc) {
+              const labeledThingInFrame = row.doc;
+
+              docs.push(this._couchDbModelDeserializer.deserializeLabeledThingInFrame(labeledThingInFrame, labeledThing));
+            }
+
+            throw new Error('Row does not contain a document');
+          });
+
+          return docs;
+        }
+
+        throw new Error(`Failed loading labeled thing in frame for the given labeledThing: ${labeledThing.id}`);
+      });
+    });
+  }
+
+  /**
+   * Returns the next incomplete labeled things in frame.
+   * The count can be specified, the default is one.
+   *
+   * @param {Task} task
+   * @param {int?} count
+   *
+   * @returns {AbortablePromise<Array.<LabeledThingInFrame>>|Error}
+   */
+  getNextIncomplete(task, count = 1) {
+    // const url = this._apiService.getApiUrl(
+    //   `/task/${task.id}/labeledThingInFrame`,
+    //   {
+    //     incompleteOnly: true,
+    //     limit: count,
+    //   }
+    // );
+    //
+    // return this.bufferedHttp.get(url, undefined, 'labeledThing')
+    //   .then(response => {
+    //     if (response.data && response.data.result) {
+    //       return response.data.result;
+    //     }
+    //
+    //     throw new Error('Failed loading incomplete labeled thing in frame');
+    //   });
+  }
+
+  /**
+   * Update the {@link LabeledThingInFrame} with the given `id`.
+   *
+   * @param {LabeledThingInFrame} labeledThingInFrame
+   *
+   * @returns {AbortablePromise<LabeledThingInFrame|Error>}
+   */
+  saveLabeledThingInFrame(labeledThingInFrame) {
+    // if (labeledThingInFrame.ghost === true) {
+    //   throw new Error('Tried to store a ghosted LabeledThingInFrame. This is not possible!');
+    // }
+    //
+    // const url = this._apiService.getApiUrl(
+    //   `/labeledThingInFrame/${labeledThingInFrame.id}`
+    // );
+    //
+    // if (labeledThingInFrame.ghostClasses !== null && !!labeledThingInFrame.classes) {
+    //   delete labeledThingInFrame.classes;
+    // }
+    //
+    // return this.bufferedHttp.put(url, labeledThingInFrame, undefined, 'labeledThing')
+    //   .then(response => {
+    //     if (response.data && response.data.result) {
+    //       return new LabeledThingInFrame(
+    //         Object.assign(
+    //           {},
+    //           response.data.result.labeledThingInFrame,
+    //           {
+    //             labeledThing: new LabeledThing(
+    //               Object.assign(
+    //                 {},
+    //                 response.data.result.labeledThing,
+    //                 {task: labeledThingInFrame.labeledThing.task}
+    //               )
+    //             ),
+    //           }
+    //         )
+    //       );
+    //     }
+    //
+    //     throw new Error('Failed updating LabeledThingInFrame');
+    //   });
+  }
+
+  /**
+   * Associate the labeledThingsInFrame with their labeledThings
+   *
+   * After the {@link LabeledThing} is Retrieved it will be combined into a new {@link LabeledThingInFrame}
+   *
+   * @param {Task} task
+   * @param {Object} result
+   * @returns {Array.<LabeledThingInFrame>}
+   * @protected
+   */
+  _associateWithLabeledThings(task, result) {
+    // return result.labeledThingsInFrame.map(data => {
+    //   const labeledThing = result.labeledThings[data.labeledThingId];
+    //   labeledThing.task = task;
+    //
+    //   return new LabeledThingInFrame(
+    //     Object.assign({}, data, {
+    //       labeledThing: new LabeledThing(labeledThing),
+    //     })
+    //   );
+    // });
+  }
+}
+
+PouchDbLabeledThingInFrameGateway.$inject = [
+  'storageContextService',
+  'revisionManager',
+  'packagingExecutor',
+  'couchDbModelSerializer',
+  'couchDbModelDeserializer',
+  'pouchLabeledThingGateway',
+];
+
+export default PouchDbLabeledThingInFrameGateway;
