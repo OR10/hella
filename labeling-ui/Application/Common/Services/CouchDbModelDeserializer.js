e6e9b8f3f1a36644838485459b11fe85f35d87bd
Jakob Westhoff <jakob@westhoffswelt.de>
2016-11-22 14:59:56 +0100

    Implemented couchdb deserialization of LabeledThings and Tasks

--- labeling-ui/Application/Common/Services/CouchDbModelDeserializer.js
+++ labeling-ui/Application/Common/Services/CouchDbModelDeserializer.js
@@ -1,3 +1,6 @@
+import angular from 'angular';
+import Task from 'Application/Task/Model/Task';
+import LabeledThing from 'Application/LabelingData/Models/LabeledThing';
 import LabeledThingInFrame from 'Application/LabelingData/Models/LabeledThingInFrame';
 
 /**
@@ -7,17 +10,96 @@ import LabeledThingInFrame from 'Application/LabelingData/Models/LabeledThingInF
  */
 class CouchDbModelDeserializer {
   /**
-   * Deserialize a labeledThingInFrame to our internal model
+   * Deserialize a labeledThing to our frontend model
+   *
+   * In order to create a {@link LabeledThing} the corresponding video document is needed as a dependency.
+   * The video document does not have a corresponding frontend model and is directly used as couchdb document.
+   *
+   * The video document needs to correspond to the `videoId` property of the given `taskDocument`.
+   *
+   * Optionally a `userMapping` may be provided. The `userMapping` is a key-value-object containing mappings between
+   * userIds and their corresponding {@link User} models.
+   *
+   * If a `userMapping` is provided it needs to be a mapping for all {@link User}s used inside the tasks
+   * `assignmentHistory`.
+   *
+   * @param {object} taskDocument
+   * @param {object} videoDocument
+   * @param {object.<string, User>?} userMapping
+   * @return {Task}
+   */
+  deserializeTask(taskDocument, videoDocument, userMapping = {}) {
+    const document = this._cloneDocument(taskDocument);
+    const video = this._cloneDocument(videoDocument);
+
+    this._removePrefixFromIdAndRevision(document);
+    this._removePrefixFromIdAndRevision(video);
+
+    if (document.videoId !== video.id) {
+      throw new Error(`Can not deserialize task with non corresponding video document: ${document.videoId} !== ${video.id}`);
+    }
+
+    // Remove video attributes, which should not be in frontend model
+    [
+      'type',
+      'rev'
+    ].forEach(property => {
+      if (video[property] !== undefined) {
+        delete video[property];
+      }
+    });
+
+    return new Task(
+      Object.assign({}, document, {video}),
+      userMapping
+    );
+  }
+
+  /**
+   * Deserialize a labeledThing to our frontend model
+   *
+   * In order to create a {@link LabeledThing} the corresponding {@link Task}
+   * is needed as a dependency. The dependency needs to be provided as {@link Task} instance.
+   * In order to create this model from a CouchDb document {@link CouchDbModelDeserializer#deserializeTask}
+   * may be used.
+   *
+   * @param {object} labeledThingDocument
+   * @param {Task} task
+   * @return {LabeledThing}
+   */
+  deserializeLabeledThing(labeledThingDocument, task) {
+    const document = this._cloneDocument(labeledThingDocument);
+    this._removePrefixFromIdAndRevision(document);
+
+    // Embedded document frameRange has a type, which should not exist in frontend model
+    if (document.frameRange !== undefined && document.frameRange.type !== undefined) {
+      delete document.frameRange.type;
+    }
+
+    return new LabeledThing(
+      Object.assign({}, document, {task})
+    );
+  }
+
+  /**
+   * Deserialize a labeledThingInFrame to our frontend model
+   *
+   * In order to create a {@link LabeledThingInFrame} the corresponding {@link LabeledThing}
+   * is needed as a dependency. The dependency needs to be provided as {@link LabeledThing} instance.
+   * In order to create this model from a CouchDb document {@link CouchDbModelDeserializer#deserializeLabeledThing}
+   * may be used.
    *
    * @param {Object} labeledThingInFrameDocument
-   * @param {LabeledThing} labeledThingModel
+   * @param {LabeledThing} labeledThing
+   * @return {LabeledThingInFrame}
    */
-  deserializeLabeledThingInFrame(labeledThingInFrameDocument, labeledThingModel) {
-    this._removePrefixFromIdAndRevision(labeledThingInFrameDocument);
+  deserializeLabeledThingInFrame(labeledThingInFrameDocument, labeledThing) {
+    const document = this._cloneDocument(labeledThingInFrameDocument);
+    this._removePrefixFromIdAndRevision(document);
 
-    labeledThingInFrameDocument.labeledThing = labeledThingModel;
-
-    return new LabeledThingInFrame(labeledThingInFrameDocument);
+    return new LabeledThingInFrame(
+      Object.assign({}, document, {labeledThing})
+    );
   }
 
   /**
@@ -42,9 +124,19 @@ class CouchDbModelDeserializer {
       delete document[`${prefix}rev`];
     }
   }
-}
 
-CouchDbModelDeserializer.TYPE_LABELED_THING_IN_FRAME = 'AppBundle.Model.LabeledThingInFrame';
+  /**
+   * Create a deep clone of a CouchDB document.
+   *
+   * @param {Object} document
+   * @return {Object}
+   * @private
+   */
+  _cloneDocument(document) {
+    // Replace with framework agnostic version in case of usage outside of angular.
+    return angular.copy(document)
+  }
+}
 
 CouchDbModelDeserializer.$inject = [];
 
