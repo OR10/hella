974adbd00feef16456b15f8a478df5dc6272a82e
Jakob Westhoff <jakob@westhoffswelt.de>
2016-11-22 17:36:31 +0100

    Implemented method to wait for PouchDB promise inside of angular promise chain
    This wrapper is needed for tests, as due to PouchDB and angular they will be a mixture of `Promise` and `angular.$q` promise-like structures, which interact with each other. As the pouch is not mocked completely it will actually take time (async) to complete certain requests.

--- labeling-ui/Tests/Support/PouchDb/PouchDbHelper.js
+++ labeling-ui/Tests/Support/PouchDb/PouchDbHelper.js
@@ -60,6 +60,62 @@ class PouchDbHelper {
   }
 
   /**
+   * Wait until an async pouchDb operation has been finished.
+   *
+   * The given `promise` is any promise associated with a PouchDB operation
+   *
+   * This wrapper is needed for tests, as due to PouchDB and angular they will be a mixture of `Promise` and
+   * `angular.$q` promise-like structures, which interact with each other. As the pouch is not mocked completely
+   * it will actually take time (async) to complete certain requests.
+   *
+   * The returned promise will wrap the initial PouchDB related promise and return once the operation completes, calling
+   * `$rootScope.$apply` intermittently in order to ensure proper processing of both interacting promise chains.
+   *
+   * The returned promise will fail and or succeed with the same data as the original promise.
+   *
+   * @param {$rootScope} $rootScope
+   * @param promise
+   * @returns {Promise}
+   */
+  waitForPouchDb($rootScope, promise) {
+    return new Promise((resolve, reject) => {
+      let state = 'waiting';
+      let data = null;
+
+      promise
+        .then(result => {
+          state = 'resolved';
+          data = result;
+        })
+        .catch(error => {
+          state = 'rejected';
+          data = error;
+        });
+
+      function _cycle() {
+        setTimeout(() => {
+          switch (state) {
+            case 'waiting':
+              _cycle();
+              break;
+            case 'resolved':
+              resolve(data);
+              break;
+            case 'rejected':
+              reject(data);
+              break;
+            default:
+              throw new Error(`Invalid state: ${state}`);
+          }
+          $rootScope.$apply();
+        }, 10);
+      }
+
+      _cycle();
+    });
+  }
+
+  /**
    * Generate a random database name for the used testdb
    *
    * @returns {string}
