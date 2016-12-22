48d48233d806e0c4df791eb5172274960f1a4341
Felix Knischewski <fek@crosscan.com>
2016-12-22 15:41:12 +0100

    Register ghosting service

--- labeling-ui/Application/LabelingData/LabelingData.js
+++ labeling-ui/Application/LabelingData/LabelingData.js
@@ -7,6 +7,7 @@ import InterpolationService from './Services/InterpolationService';
 import LinearBackendInterpolation from './Interpolations/LinearBackendInterpolation';
 import CacheService from './Services/CacheService';
 import CacheHeaterService from './Services/CacheHeaterService';
+import GhostingService from './Services/GhostingService';
 
 import PouchDbLabeledThingGateway from './Gateways/PouchDbLabeledThingGateway';
 import PouchDbLabeledThingInFrameGateway from './Gateways/PouchDbLabeledThingInFrameGateway';
@@ -33,6 +34,8 @@ class LabelingData extends Module {
 
     this.module.service('cacheService', CacheService);
     this.module.service('cacheHeaterService', CacheHeaterService);
+
+    this.module.service('ghostingService', GhostingService);
 
     if (featureFlags.pouchdb === true) {
       this.module.service('labeledThingInFrameGateway', PouchDbLabeledThingInFrameGateway);
