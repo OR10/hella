023f622391a233b9d263d840cf80dde0b047d26f
Felix Knischewski <fek@crosscan.com>
2016-12-23 14:22:27 +0100

    Delete ltifs on lt deletion

--- labeling-ui/Application/LabelingData/Gateways/PouchDbLabeledThingGateway.js
+++ labeling-ui/Application/LabelingData/Gateways/PouchDbLabeledThingGateway.js
@@ -3,13 +3,21 @@
  */
 class PouchDbLabeledThingGateway {
   /**
+   * @param {angular.$q} $q
    * @param {PouchDbContextService} pouchDbContextService
    * @param {PackagingExecutor} packagingExecutor
    * @param {CouchDbModelSerializer} couchDbModelSerializer
    * @param {CouchDbModelDeserializer} couchDbModelDeserializer
    * @param {RevisionManager} revisionManager
+   * @param {PouchDbSyncManager} pouchDbSyncManager
    */
-  constructor(pouchDbContextService, packagingExecutor, couchDbModelSerializer, couchDbModelDeserializer, revisionManager, pouchDbSyncManager) {
+  constructor($q, pouchDbContextService, packagingExecutor, couchDbModelSerializer, couchDbModelDeserializer, revisionManager, pouchDbSyncManager) {
+    /**
+     * @type {angular.$q}
+     * @private
+     */
+    this._$q = $q;
+
     /**
      * @type {PouchDbContextService}
      * @private
@@ -69,17 +77,17 @@ class PouchDbLabeledThingGateway {
       this._injectRevisionOrFailSilently(serializedLabeledThing);
       return dbContext.put(serializedLabeledThing);
     })
-    .then(dbResponse => {
-      storedLabeledThingId = dbResponse.id;
-      return this._pouchDbSyncManager.waitForRemoteToConfirm(dbContext);
-    })
-    .then(() => {
-      return dbContext.get(storedLabeledThingId);
-    })
-    .then(readLabeledThing => {
-      this._revisionManager.extractRevision(readLabeledThing);
-      return this._couchDbModelDeserializer.deserializeLabeledThing(readLabeledThing, task);
-    });
+      .then(dbResponse => {
+        storedLabeledThingId = dbResponse.id;
+        return this._pouchDbSyncManager.waitForRemoteToConfirm(dbContext);
+      })
+      .then(() => {
+        return dbContext.get(storedLabeledThingId);
+      })
+      .then(readLabeledThing => {
+        this._revisionManager.extractRevision(readLabeledThing);
+        return this._couchDbModelDeserializer.deserializeLabeledThing(readLabeledThing, task);
+      });
   }
 
   /**
@@ -109,10 +117,10 @@ class PouchDbLabeledThingGateway {
     const synchronizedDbPromize = this._packagingExecutor.execute('labeledThing', () => {
       return dbContext.get(labeledThingId);
     })
-    .then(dbDocument => {
-      this._revisionManager.extractRevision(dbDocument);
-      return this._couchDbModelDeserializer.deserializeLabeledThing(dbDocument, task);
-    });
+      .then(dbDocument => {
+        this._revisionManager.extractRevision(dbDocument);
+        return this._couchDbModelDeserializer.deserializeLabeledThing(dbDocument, task);
+      });
     return synchronizedDbPromize;
   }
 
@@ -125,16 +133,35 @@ class PouchDbLabeledThingGateway {
   deleteLabeledThing(labeledThing) {
     const task = labeledThing.task;
     const dbContext = this._pouchDbContextService.provideContextForTaskId(task.id);
-    const document = this._couchDbModelSerializer.serialize(labeledThing);
+    const labeledThingDocument = this._couchDbModelSerializer.serialize(labeledThing);
 
     // @TODO: What about error handling here? No global handling is possible this easily?
     //       Monkey-patch pouchdb? Fix error handling at usage point?
     const synchronizedPromise = this._packagingExecutor.execute('labeledThing', () => {
-      this._injectRevisionOrFailSilently(document);
-      return dbContext.remove(document);
-      // TODO: delete labeledThingsInFrame
-    })
-    .then(dbDocument => this._pouchDbSyncManager.waitForRemoteToConfirm(dbContext, dbDocument));
+      this._injectRevisionOrFailSilently(labeledThingDocument);
+
+      const ltPromise = dbContext.remove(labeledThingDocument);
+
+      const ltifPromise = dbContext.query((dbDocument, emit) => {
+        // Find all associated document to this labeledThing
+        if (dbDocument.type === 'AppBundle.Model.LabeledThingInFrame' && dbDocument.labeledThingId === labeledThing.id) {
+          emit(dbDocument);
+        }
+      }, {include_docs: true}).then(documents => {
+        // Mark found documents as deleted
+        const docs = documents.rows.map(document => {
+          const doc = document.doc;
+          doc._deleted = true;
+          return doc;
+        });
+
+        // Bulk update as deleted marked documents
+        return dbContext.bulkDocs(docs);
+      });
+
+      // Return promise of the deletion of lt and associated ltifs
+      return this._$q.all(ltPromise, ltifPromise);
+    }).then(dbDocument => this._pouchDbSyncManager.waitForRemoteToConfirm(dbContext, dbDocument));
 
     return synchronizedPromise;
   }
@@ -166,6 +193,7 @@ class PouchDbLabeledThingGateway {
 }
 
 PouchDbLabeledThingGateway.$inject = [
+  '$q',
   'pouchDbContextService',
   'packagingExecutor',
   'couchDbModelSerializer',
