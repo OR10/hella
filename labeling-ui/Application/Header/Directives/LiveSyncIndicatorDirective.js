5e73fef5f3ec8a90bed150e955d7ef570f80a5ca
Felix Knischewski <fek@crosscan.com>
2017-01-09 10:24:30 +0100

    improve sync indicator styling and add tooltip

--- labeling-ui/Application/Header/Directives/LiveSyncIndicatorDirective.js
+++ labeling-ui/Application/Header/Directives/LiveSyncIndicatorDirective.js
@@ -1,7 +1,7 @@
 import liveSyncIndicatorTemplate from './LiveSyncIndicatorDirective.html!';
 
 /**
- * Directive to display syncprogress
+ * Directive to display sync progress
  */
 class LiveSyncIndicatorDirective {
   constructor(pouchDbSyncManager) {
@@ -12,20 +12,31 @@ class LiveSyncIndicatorDirective {
 
   link(scope) {
     const stateToIcon = {
-      'offline': 'plug',
-      'alive': 'heartbeat',
-      'transfer': 'bullseye',
+      'offline': 'chain-broken',
+      'alive': 'signal',
+      'transfer': 'exchange',
+    };
+
+    const stateTooltipText = {
+      'offline': 'There is currently no connection to the server',
+      'alive': 'Connection to the server established, no data to transfer',
+      'transfer': 'Data is synced with the server',
     };
 
     scope.syncState = stateToIcon.offline;
+    scope.syncTooltip = stateTooltipText.offline;
+
     this._pouchDbSyncManager.on('offline', () => {
       scope.syncState = stateToIcon.offline;
+      scope.syncTooltip = stateTooltipText.offline;
     });
     this._pouchDbSyncManager.on('alive', () => {
       scope.syncState = stateToIcon.alive;
+      scope.syncTooltip = stateTooltipText.alive;
     });
     this._pouchDbSyncManager.on('transfer', () => {
       scope.syncState = stateToIcon.transfer;
+      scope.syncTooltip = stateTooltipText.transfer;
     });
   }
 }
