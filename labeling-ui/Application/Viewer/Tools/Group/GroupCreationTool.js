0ab5844d542d50e71ee4470919e76acb32b4190e
Felix Knischewski <fek@crosscan.com>
2017-02-06 19:16:56 +0100

    Add group creation tool

---
+++ labeling-ui/Application/Viewer/Tools/Group/GroupCreationTool.js
@@ -0,0 +1,93 @@
+import paper from 'paper';
+import CreationTool from '../CreationTool';
+import PaperGroupRectangle from '../../Shapes/PaperGroupRectangle';
+
+class GroupCreationTool extends CreationTool {
+  /**
+   * @param {Context} drawingContext
+   * @param {$scope} $scope
+   * @param {$q} $q
+   * @param {LoggerService} loggerService
+   * @param {EntityIdService} entityIdService
+   * @param {EntityColorService} entityColorService
+   * @param {ToolService} toolService
+   * @param {LabeledThingGroupService} labeledThingGroupService
+   */
+  constructor(drawingContext, $scope, $q, loggerService, entityIdService, entityColorService, toolService, labeledThingGroupService) {
+    super(drawingContext, $scope, $q, loggerService, entityIdService, entityColorService);
+
+    /**
+     * @type {ToolService}
+     * @private
+     */
+    this._toolService = toolService;
+
+    /**
+     * @type {LabeledThingGroupService}
+     * @private
+     */
+    this._labeledThingGroupService = labeledThingGroupService;
+
+    /**
+     * @type {RectangleDrawingTool}
+     * @private
+     */
+    this._rectangleCreationTool = this._toolService.getTool(drawingContext, 'rectangle');
+  }
+
+  /**
+   * @param {paper.Event} event
+   */
+  onMouseDown(event) {
+    // Delegating the event down to the used tool
+    this._rectangleCreationTool.onMouseDown(event);
+  }
+
+  /**
+   * @param toolActionStruct
+   * @return {Promise}
+   */
+  invokeShapeCreation(toolActionStruct) {
+    const promise = super.invokeShapeCreation(toolActionStruct);
+
+    this._rectangleCreationTool.invokeShapeCreation(toolActionStruct).then(paperShape => {
+      const shapes = this._labeledThingGroupService.getShapesWithinBounds(this._context, paperShape.bounds);
+      const {point: topLeft, width, height} = this._labeledThingGroupService.getBoundsForShapes(shapes);
+      const bottomRight = new paper.Point(topLeft.x + width, topLeft.y + height);
+
+      let paperGroup;
+      this._context.withScope(() => {
+        paperGroup = new PaperGroupRectangle(
+          paperShape.labeledThingInFrame,
+          paperShape.id,
+          topLeft,
+          bottomRight,
+          paperShape.color,
+          paperShape.isDraft
+        );
+      });
+
+      this._complete(paperGroup);
+    });
+
+    return promise;
+  }
+
+  invokeDefaultShapeCreation() {
+    this._reject(new Error('Cannot create default shape for groups'));
+  }
+
+}
+
+GroupCreationTool.$inject = [
+  'drawingContext',
+  '$rootScope',
+  '$q',
+  'loggerService',
+  'entityIdService',
+  'entityColorService',
+  'toolService',
+  'labeledThingGroupService',
+];
+
+export default GroupCreationTool;
