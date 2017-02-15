1cf2bcef6e7cb7c2b51464a4c1ccff74519bc3ff
Felix Knischewski <fek@crosscan.com>
2017-02-15 16:11:41 +0100

    Pass labeledThingGroupInFrame to PaperGroupShape

--- labeling-ui/Application/Viewer/Shapes/PaperGroupShape.js
+++ labeling-ui/Application/Viewer/Shapes/PaperGroupShape.js
@@ -4,21 +4,21 @@ class PaperGroupShape extends PaperShape {
 
   /**
    *
-   * @param {LabeledThingGroup} labeledThingGroup
+   * @param {LabeledThingGroupInFrame} labeledThingGroupInFrame
    * @param {String} shapeId
    * @param {{primary: string, secondary: string}} color
    * @param {boolean?} draft
    */
-  constructor(labeledThingGroup, shapeId, color, draft = false) {
+  constructor(labeledThingGroupInFrame, shapeId, color, draft = false) {
     super(shapeId, color, draft);
 
     /**
      * {@link LabeledThingGroup} associated with this `PaperGroupShape`
      *
      * @type {LabeledThingGroup}
-     * @private
+     * @protected
      */
-    this._labeledThingGroup = labeledThingGroup;
+    this._labeledThingGroupInFrame = labeledThingGroupInFrame;
   }
 
 
@@ -27,8 +27,8 @@ class PaperGroupShape extends PaperShape {
    *
    * @returns {LabeledThingGroup}
    */
-  get labeledThingGroup() {
-    return this._labeledThingGroup;
+  get labeledThingGroupInFrame() {
+    return this._labeledThingGroupInFrame;
   }
 }
 
