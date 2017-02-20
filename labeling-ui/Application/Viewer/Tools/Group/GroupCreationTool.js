11b527a61ead9e6e289d987d3c089217d8cbf5a7
Felix Knischewski <fek@crosscan.com>
2017-02-20 10:25:20 +0100

    Add missing functions to groupCreationTool

--- labeling-ui/Application/Viewer/Tools/Group/GroupCreationTool.js
+++ labeling-ui/Application/Viewer/Tools/Group/GroupCreationTool.js
@@ -114,6 +114,57 @@ class GroupCreationTool extends CreationTool {
   }
 }
 
+/**
+ * Return the name of the tool. The name needs to be unique within the application.
+ * Therefore something like a prefix followed by the className is advisable.
+ *
+ * @return {string}
+ * @public
+ * @abstract
+ * @static
+ */
+GroupCreationTool.getToolName = function () {
+  return 'GroupCreationTool';
+};
+
+/**
+ * Check if the given ShapeClass ({@link PaperShape#getClass}) is supported by this Tool.
+ *
+ * It specifies mostly which shape is affected by the given tool (eg. `rectangle`, `cuboid`, `multi`, ...)
+ *
+ * There maybe multiple Tools with the same name, but different action identifiers. (`rectangle` and ´move`,
+ * `rectangle` and `scale`, ...)
+ *
+ * @return {bool}
+ * @public
+ * @abstract
+ * @static
+ */
+GroupCreationTool.isShapeClassSupported = function (shapeClass) {
+  return [
+    'group-rectangle',
+  ].includes(shapeClass);
+};
+
+/**
+ * Check if the given actionIdentifer is supported by this tool.
+ *
+ * Currently supported actions are:
+ * - `creating`
+ * - `scale`
+ * - `move`
+ *
+ * @return {bool}
+ * @public
+ * @abstract
+ * @static
+ */
+GroupCreationTool.isActionIdentifierSupported = function (actionIdentifier) {
+  return [
+    'creation',
+  ].includes(actionIdentifier);
+};
+
 GroupCreationTool.$inject = [
   'drawingContext',
   '$rootScope',
