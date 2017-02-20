7bbf84e2098e06f5995cb3f08e995b60b98c2584
Jakob Westhoff <jakob@westhoffswelt.de>
2017-02-20 15:03:44 +0100

    Fixed usage of async protractor actions

--- labeling-ui/Tests/E2E/CuboidCreation.spec.js
+++ labeling-ui/Tests/E2E/CuboidCreation.spec.js
@@ -49,13 +49,12 @@ describe('Cuboid creation', () => {
         viewerWidth: 1104,
         viewerHeight: 620,
       })
-        .then(() => {
-          browser.actions()
+        .then(() => browser.actions()
             .mouseMove(viewer, {x: 405, y: 372}) // initial position
             .mouseDown()
             .mouseMove(viewer, {x: 405, y: 525}) // drag
-            .perform();
-        })
+            .perform()
+        )
         .then(
           // () => canvasInstructionLogManager.getAnnotationCanvasLogs('CuboidCreation', 'DrawMiddleCuboid1')
           () => canvasInstructionLogManager.getAnnotationCanvasLogs()
@@ -63,12 +62,11 @@ describe('Cuboid creation', () => {
         .then(drawingStack => {
           expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CuboidCreation.DrawMiddleCuboid1);
         })
-        .then(() => {
-          browser.actions()
+        .then(() => browser.actions()
             .mouseUp()
             .mouseMove(viewer, {x: 673, y: 525}) // width
-            .perform();
-        })
+            .perform()
+        )
         .then(
           // () => canvasInstructionLogManager.getAnnotationCanvasLogs('CuboidCreation', 'DrawMiddleCuboid2')
           () => canvasInstructionLogManager.getAnnotationCanvasLogs()
@@ -76,11 +74,10 @@ describe('Cuboid creation', () => {
         .then(drawingStack => {
           expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CuboidCreation.DrawMiddleCuboid2);
         })
-        .then(() => {
-          browser.actions()
+        .then(() => browser.actions()
             .click()
-            .perform();
-        })
+            .perform()
+        )
         .then(
           // () => canvasInstructionLogManager.getAnnotationCanvasLogs('CuboidCreation', 'DrawMiddleCuboid3')
           () => canvasInstructionLogManager.getAnnotationCanvasLogs()
@@ -88,19 +85,18 @@ describe('Cuboid creation', () => {
         .then(drawingStack => {
           expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CuboidCreation.DrawMiddleCuboid3);
         })
-        .then(() => {
-          browser.actions()
+        .then(() => browser.actions()
             .mouseMove(viewer, {x: 673, y: 486}) // depth
             .click()
-            .perform();
-        })
+            .perform()
+        )
         .then(
           // () => canvasInstructionLogManager.getAnnotationCanvasLogs('CuboidCreation', 'DrawMiddleCuboid4')
           () => canvasInstructionLogManager.getAnnotationCanvasLogs()
         )
         .then(drawingStack => {
           expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CuboidCreation.DrawMiddleCuboid4);
-          browser.sleep(1000);
+          return browser.sleep(1000);
         })
         // .then(() => dumpAllRequestsMade(mock))
         .then(() => getMockRequestsMade(mock))
@@ -110,7 +106,7 @@ describe('Cuboid creation', () => {
         });
     });
 
-    it('should create a new 3d cuboid starting bottom to top', done => {
+    fit('should create a new 3d cuboid starting bottom to top', done => {
       mock(sharedMocks.concat([
         assets.mocks.CuboidCreation.DrawCuboids.LabeledThingInFrame.frameIndex0,
         assets.mocks.CuboidCreation.DrawCuboids.LabeledThingInFrame.frameIndex0to1,
