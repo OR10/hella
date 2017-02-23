6433dfdf48bb4b07d65c8de82f17db642960fbfa
Christian Tischer <cht@crosscan.com>
2017-02-23 10:36:22 +0100

    Added campaign model

---
+++ labeling-api/src/AnnoStationBundle/Model/Campaign.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AnnoStationBundle\Model;
+
+use Doctrine\ODM\CouchDB\Mapping\Annotations as CouchDB;
+use JMS\Serializer\Annotation as Serializer;
+
+/**
+ * @CouchDB\Document
+ */
+class Campaign
+{
+    /**
+     * @CouchDB\Id
+     */
+    private $id;
+
+    /**
+     * @CouchDB\Version
+     */
+    private $rev;
+
+    /**
+     * @CouchDB\Field(type="string")
+     */
+    private $name;
+
+    /**
+     * @CouchDB\Field(type="string")
+     */
+    private $organisationId;
+
+    public function __construct($name, $quota = 0)
+    {
+        $this->name  = $name;
+    }
+}
