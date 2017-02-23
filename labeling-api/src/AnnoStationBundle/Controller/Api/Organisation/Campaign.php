cb89765bf0d4abc54c9b3679a8ea70e64484ac92
Christian Tischer <cht@crosscan.com>
2017-02-23 10:38:07 +0100

    Added Campaign controller

---
+++ labeling-api/src/AnnoStationBundle/Controller/Api/Organisation/Campaign.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace AnnoStationBundle\Controller\Api\Organisation;
+
+use AppBundle\Annotations\CloseSession;
+use AnnoStationBundle\Annotations\CheckPermissions;
+use AnnoStationBundle\Controller;
+use AnnoStationBundle\Database\Facade;
+use AnnoStationBundle\Service;
+use AnnoStationBundle\Model as AnnoStationBundleModel;
+use AppBundle\Database\Facade as AppFacade;
+use AppBundle\Model;
+use AppBundle\View;
+use AnnoStationBundle\Response;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpKernel\Exception;
+use Symfony\Component\Security\Core\Authentication\Token\Storage;
+
+/**
+ * @Rest\Prefix("/api/organisation")
+ * @Rest\Route(service="annostation.labeling_api.controller.api.organisation.campaign")
+ *
+ * @CloseSession
+ */
+class Campaign extends Controller\Base
+{
+    /**
+     * @var Facade\Campaign
+     */
+    private $campaignFacade;
+
+    /**
+     * @var Service\Authorization
+     */
+    private $authorizationService;
+
+    /**
+     * Campaign constructor.
+     *
+     * @param Facade\Campaign       $campaignFacade
+     * @param Service\Authorization $authorizationService
+     */
+    public function __construct(Facade\Campaign $campaignFacade, Service\Authorization $authorizationService)
+    {
+        $this->campaignFacade       = $campaignFacade;
+        $this->authorizationService = $authorizationService;
+    }
+
+    /**
+     *
+     * @Rest\Get("/{organisation}/campaign")
+     *
+     * @param AnnoStationBundleModel\Organisation $organisation
+     *
+     * @return View\View
+     */
+    public function listCampaignsAction(AnnoStationBundleModel\Organisation $organisation)
+    {
+        $this->authorizationService->denyIfOrganisationIsNotAccessable($organisation);
+
+        return View\View::create()->setData(
+            [
+                'result' => $this->campaignFacade->getCampaignByOrganisation($organisation)
+            ]
+        );
+    }
+}
