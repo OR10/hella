b56f0cb7c2f8aae28a89f46dd3c73f1b18e0b46e
Christian Tischer <cht@crosscan.com>
2017-02-24 13:40:34 +0100

    Moved ProjectCounter route to organisations namespace

--- labeling-api/src/AnnoStationBundle/Controller/Api/Organisation/ProjectCount.php
+++ labeling-api/src/AnnoStationBundle/Controller/Api/Organisation/ProjectCount.php
@@ -1,20 +1,22 @@
 <?php
 
-namespace AnnoStationBundle\Controller\Api;
+namespace AnnoStationBundle\Controller\Api\Organisation;
 
 use AppBundle\Annotations\CloseSession;
 use AnnoStationBundle\Controller;
 use AnnoStationBundle\Database\Facade;
+use AnnoStationBundle\Service;
 use AppBundle\View;
 use AppBundle\Model;
+use AnnoStationBundle\Model as AnnoStationBundleModel;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation;
 use Symfony\Component\HttpKernel\Exception;
 use Symfony\Component\Security\Core\Authentication\Token\Storage;
 
 /**
- * @Rest\Prefix("/api/projectCount")
- * @Rest\Route(service="annostation.labeling_api.controller.api.project_count")
+ * @Rest\Prefix("/api/organisation")
+ * @Rest\Route(service="annostation.labeling_api.controller.api.organisation.project_count")
  *
  * @CloseSession
  */
@@ -31,25 +33,38 @@ class ProjectCount extends Controller\Base
     private $tokenStorage;
 
     /**
-     * ProjectCount constructor.
-     * @param Facade\Project       $projectFacade
-     * @param Storage\TokenStorage $tokenStorage
+     * @var Service\Authorization
      */
-    public function __construct(Facade\Project $projectFacade, Storage\TokenStorage $tokenStorage)
-    {
-        $this->projectFacade = $projectFacade;
-        $this->tokenStorage  = $tokenStorage;
+    private $authorizationService;
+
+    /**
+     * ProjectCount constructor.
+     *
+     * @param Facade\Project        $projectFacade
+     * @param Storage\TokenStorage  $tokenStorage
+     * @param Service\Authorization $authorizationService
+     */
+    public function __construct(
+        Facade\Project $projectFacade,
+        Storage\TokenStorage $tokenStorage,
+        Service\Authorization $authorizationService
+    ) {
+        $this->projectFacade        = $projectFacade;
+        $this->tokenStorage         = $tokenStorage;
+        $this->authorizationService = $authorizationService;
     }
 
     /**
-     * @Rest\Get("")
+     * @Rest\Get("/{organisation}/projectCount")
      *
-     * @param HttpFoundation\Request $request
+     * @param AnnoStationBundleModel\Organisation $organisation
      *
      * @return \FOS\RestBundle\View\View
      */
-    public function getProjectCountAction(HttpFoundation\Request $request)
+    public function getProjectCountAction(AnnoStationBundleModel\Organisation $organisation)
     {
+        $this->authorizationService->denyIfOrganisationIsNotAccessable($organisation);
+
         /** @var Model\User $user */
         $user = $this->tokenStorage->getToken()->getUser();
 
@@ -65,7 +80,8 @@ class ProjectCount extends Controller\Base
             if (!array_key_exists($status, $sum)) {
                 $sum[$status] = 0;
             }
-            foreach ($this->projectFacade->findAllByUserAndStatus($user, $status, true)->toArray() as $sumByStatus) {
+            foreach ($this->projectFacade->findAllByUserAndStatus($organisation, $user, $status, true)->toArray(
+            ) as $sumByStatus) {
                 $sum[$status] = $sumByStatus['value'];
             }
         }
