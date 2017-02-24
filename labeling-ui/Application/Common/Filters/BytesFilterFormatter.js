d690cfac82383f9297dbe8ff89169de417c5aec4
Christian Tischer <cht@crosscan.com>
2017-02-24 12:57:00 +0100

    Fixed 0 bytes fromat

--- labeling-ui/Application/Common/Filters/BytesFilterFormatter.js
+++ labeling-ui/Application/Common/Filters/BytesFilterFormatter.js
@@ -14,6 +14,9 @@ export default class BytesFilterFormatter {
     if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) {
       return '-';
     }
+    if (bytes === 0) {
+      return '0 bytes';
+    }
     const units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
     const number = Math.floor(Math.log(bytes) / Math.log(1024));
     return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
