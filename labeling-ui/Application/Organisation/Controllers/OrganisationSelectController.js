522db0f2121263f76ba5350fdf6cfbb8c2557109
Jakob Westhoff <jakob@westhoffswelt.de>
2017-02-27 16:28:29 +0100

    Fixed Organisation Select View actually selecting organisation

--- labeling-ui/Application/Organisation/Controllers/OrganisationSelectController.js
+++ labeling-ui/Application/Organisation/Controllers/OrganisationSelectController.js
@@ -1,20 +1,51 @@
 class OrganisationSelectController {
   /**
+   * @param {$state} $state
    * @param {CurrentUserService} currentUserService
+   * @param {OrganisationService} organisationService
    */
-  constructor(currentUserService) {
+  constructor($state, currentUserService, organisationService) {
+    /**
+     * @type {$state}
+     * @private
+     */
+    this._$state = $state;
+
+    /**
+     * @type {OrganisationService}
+     * @private
+     */
+    this._organisationService = organisationService;
+
     /**
      * @type {Array.<Organisation>}
      */
     this.organisationsOfCurrentUser = currentUserService.getOrganisations();
 
+    /**
+     * @type {User}
+     */
     this.user = currentUserService.get();
+
+    /**
+     * @type {Object}
+     */
     this.userPermissions = currentUserService.getPermissions();
+  }
+
+  /**
+   * @param {Organisation} organisation
+   */
+  gotoOrganisation(organisation) {
+    this._organisationService.set(organisation.id);
+    this._$state.go('labeling.projects.list');
   }
 }
 
 OrganisationSelectController.$inject = [
-  'currentUserService'
+  '$state',
+  'currentUserService',
+  'organisationService',
 ];
 
 export default OrganisationSelectController;
