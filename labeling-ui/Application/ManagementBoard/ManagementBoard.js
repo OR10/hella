8c928ef8de59294926e11bdfd2ccf808a2fd1b65
Felix Knischewski <fek@crosscan.com>
2017-02-27 11:53:37 +0100

    Fix project resolver

--- labeling-ui/Application/ManagementBoard/ManagementBoard.js
+++ labeling-ui/Application/ManagementBoard/ManagementBoard.js
@@ -154,7 +154,11 @@ class ManagementBoard extends Module {
         project: [
           '$stateParams',
           'projectGateway',
-          ($stateParams, projectGateway) => projectGateway.getProject($stateParams.projectId),
+          'organisationService',
+          ($stateParams, projectGateway, organisationService) => {
+            organisationService.set($stateParams.organisationId);
+            return projectGateway.getProject($stateParams.projectId);
+          },
         ],
       },
     });
