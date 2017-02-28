c7d76c0da5cddd6459624028320f4ed8684d8a74
Felix Knischewski <fek@crosscan.com>
2017-02-28 12:58:44 +0100

    Switch file size filter to base 1024

--- labeling-ui/Application/ManagementBoard/Filters/FileSizeFilterProvider.js
+++ labeling-ui/Application/ManagementBoard/Filters/FileSizeFilterProvider.js
@@ -7,13 +7,13 @@ function FileSizeFilterProvider() {
       return zeroBytesString;
     }
     const units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
-    const number = Math.floor(Math.log(bytes) / Math.log(1000));
+    const number = Math.floor(Math.log(bytes) / Math.log(1024));
 
     if (number === 0) {
       return `${bytes} ${units[number]}`;
     }
 
-    return (bytes / Math.pow(1000, Math.floor(number))).toFixed(precision) + ' ' + units[number];
+    return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) + ' ' + units[number];
   };
 }
 
