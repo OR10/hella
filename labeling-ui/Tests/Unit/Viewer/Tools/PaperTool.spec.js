077a061192b9a95504bac2f92067f57f188720c0
Dominik Ehrenberg <doe@crosscan.com>
2017-03-03 20:27:06 +0100

    Test that this error will not turn up again

--- labeling-ui/Tests/Unit/Viewer/Tools/PaperTool.spec.js
+++ labeling-ui/Tests/Unit/Viewer/Tools/PaperTool.spec.js
@@ -9,8 +9,29 @@ fdescribe('PaperTool test suite', function() {
       drawContext.withScope.and.callFake((callback) => callback());
   });
 
+  function createPaperToolInstance() {
+    return new PaperTool(drawContext);
+  }
+
   it('is of type Tool', function() {
-    const paperTool = new PaperTool(drawContext);
+    const paperTool = createPaperToolInstance();
     expect(paperTool).toEqual(jasmine.any(Tool));
+  });
+
+  describe('#delegateMouseEvent()', () => {
+    describe('drag', () => {
+      it('does not break if there is no last drag point', () => {
+        const paperTool = createPaperToolInstance();
+        const event = {
+          point: { x: 0, y: 0}
+        };
+
+        function throwWrapper() {
+          paperTool.delegateMouseEvent('drag', event);
+        }
+
+        expect(throwWrapper).not.toThrow();
+      });
+    });
   });
 });
