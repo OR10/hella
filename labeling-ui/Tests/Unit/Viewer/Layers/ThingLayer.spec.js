eb900a61818240a9f03c752519603434be8072bc
Dominik Ehrenberg <doe@crosscan.com>
2017-03-03 17:45:26 +0100

    Making the test work one step at a time

--- labeling-ui/Tests/Unit/Viewer/Layers/ThingLayer.spec.js
+++ labeling-ui/Tests/Unit/Viewer/Layers/ThingLayer.spec.js
@@ -7,17 +7,20 @@ fdescribe('ThingLayer test suite', function() {
   let scope;
   let drawingContext;
 
+  // Service mocks
+  let loggerService;
   let toolService;
+  let viewerMouseCursorService;
 
   beforeEach(module(($provide) => {
     // Service mocks
-    const logger = jasmine.createSpyObj('$logger', ['warn']);
-    $provide.service('loggerService', () => logger);
+    loggerService = jasmine.createSpyObj('$logger', ['groupStart', 'log', 'groupEnd', 'groupStartOpened']);
+    $provide.service('loggerService', () => loggerService);
 
-    toolService = jasmine.createSpyObj('toolService', ['']);
+    toolService = jasmine.createSpyObj('toolService', ['getTool']);
     $provide.service('toolService', () => toolService);
 
-    const viewerMouseCursorService = jasmine.createSpyObj('viewerMouseCursorService', ['']);
+    viewerMouseCursorService = jasmine.createSpyObj('viewerMouseCursorService', ['setMouseCursor']);
     $provide.service('viewerMouseCursorService', () => viewerMouseCursorService);
   }));
 
@@ -26,13 +29,36 @@ fdescribe('ThingLayer test suite', function() {
     scope = $rootScope.$new();
   }));
 
-  it('is a PanAndZoomPaperLayer', function() {
+  function createThingLayerInstance() {
     const drawingContext = jasmine.createSpyObj('drawingContext', ['withScope']);
-    drawingContext.withScope.and.callFake((callback) => callback());
+    drawingContext.withScope.and.callFake((callback) => callback(scope));
 
     const framePosition = jasmine.createSpyObj('framePosition', ['beforeFrameChangeAlways', 'afterFrameChangeAlways']);
 
-    const thing = new ThingLayer(0, 0, scope, injector, drawingContext, toolService, null, null, null, framePosition);
+    return new ThingLayer(0, 0, scope, injector, drawingContext, toolService, null, loggerService, null, framePosition, viewerMouseCursorService);
+  }
+
+  it('is a PanAndZoomPaperLayer', function() {
+    const thing = createThingLayerInstance();
     expect(thing).toEqual(jasmine.any(PanAndZoomPaperLayer));
+  });
+
+  it('updates the view when leaving the canvas', function() {
+    const task = {
+      minimalVisibleShapeOverflow: null
+    };
+
+    scope.view = jasmine.createSpyObj('scope.view', ['update']);
+    scope.vm = { task: task };
+
+    const keyboardTool = jasmine.createSpyObj('keyboardTool', ['invokeKeyboardShortcuts']);
+    toolService.getTool.and.returnValue(keyboardTool);
+
+    const thing = createThingLayerInstance();
+    const event = {type: 'mouseenter'};
+    thing.activateTool('multi', {});
+    thing.dispatchDOMEvent(event);
+
+    expect(scope.view.update).toHaveBeenCalled();
   });
 });
