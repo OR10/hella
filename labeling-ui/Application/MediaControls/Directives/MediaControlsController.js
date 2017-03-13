2030d10520a9c0291554d7880673dedaac1d9926
Konstantin Heinrich <kheinrich188@googlemail.com>
2017-03-13 11:14:09 +0100

    Fix Bug TTANNO-1327
    Cleanup code and restyling

--- labeling-ui/Application/MediaControls/Directives/MediaControlsController.js
+++ labeling-ui/Application/MediaControls/Directives/MediaControlsController.js
@@ -348,25 +348,23 @@ class MediaControlsController {
           this.paperThingShapes = this.paperThingShapes.filter(
             paperThingShape => paperThingShape.labeledThingInFrame.id !== selectedLabeledThingInFrame.id
           );
-          this._applicationState.enableAll();
-          this._$rootScope.$emit('shape:delete:after');
 
           return selectedLabeledThing;
         })
         .then(() => {
-          for (let groupIdIndex = 0, groupIdLength = selectedLabeledThing.groupIds.length;
-               groupIdIndex < groupIdLength; groupIdIndex++) {
-            const gid = selectedLabeledThing.groupIds[groupIdIndex];
+          selectedLabeledThing.groupIds.forEach(groupId => {
             const relatedThingShapes = this.paperThingShapes.filter(thingShape =>
-                thingShape.labeledThingInFrame.labeledThing.groupIds.indexOf(gid) !== -1);
-            const relatedLabeledThings = relatedThingShapes.map(thingShape =>
-                thingShape.labeledThingInFrame.labeledThing);
+                thingShape.labeledThingInFrame.labeledThing.groupIds.indexOf(groupId) !== -1);
             const shapeGroup = this.paperGroupShapes.find(
-                paperGroupShape => paperGroupShape.labeledThingGroupInFrame.labeledThingGroup.id === gid);
-            if (relatedLabeledThings.length === 0) {
+                paperGroupShape => paperGroupShape.labeledThingGroupInFrame.labeledThingGroup.id === groupId);
+
+            if (relatedThingShapes.length === 0) {
               this._deleteGroupShape(shapeGroup);
             }
-          }
+
+            this._applicationState.enableAll();
+            this._$rootScope.$emit('shape:delete:after');
+          });
         })
         .catch(() => this._onDeletionError());
     } catch (error) {
