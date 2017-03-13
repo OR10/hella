5cb2257e1996b7bb2faa2ce8fab4666639d481a7
Dominik Ehrenberg <doe@crosscan.com>
2017-03-13 13:07:01 +0100

    Couch db mock

--- labeling-api/src/AnnoStationBundle/Service/TaskDatabaseCreator.php
+++ labeling-api/src/AnnoStationBundle/Service/TaskDatabaseCreator.php
@@ -2,7 +2,22 @@
 
 namespace AnnoStationBundle\Service;
 
+use Doctrine\ODM\CouchDB;
+
 class TaskDatabaseCreator
 {
+    /**
+     * @var CouchDB\DocumentManager
+     */
+    private $documentManager;
 
+    /**
+     * LabelingTask constructor.
+     *
+     * @param CouchDB\DocumentManager $documentManager
+     */
+    public function __construct(CouchDB\DocumentManager $documentManager)
+    {
+        $this->documentManager = $documentManager;
+    }
 }
--- labeling-api/src/AnnoStationBundle/Tests/Service/TaskDatabaseCreatorTest.php
+++ labeling-api/src/AnnoStationBundle/Tests/Service/TaskDatabaseCreatorTest.php
@@ -4,12 +4,23 @@ namespace AnnoStationBundle\Tests\Service;
 
 use AnnoStationBundle\Service\TaskDatabaseCreator;
 use AppBundle\Tests;
+use Doctrine\ODM\CouchDB;
 
 class TaskDatabaseCreatorTest extends Tests\KernelTestCase
 {
+    private function getCouchMock()
+    {
+        $couchMock = $this->getMockBuilder(CouchDB\DocumentManager::class)
+            ->disableOriginalConstructor()
+//            ->setMethods(['getCouchDBClient'])
+            ->getMock();
+        return $couchMock;
+    }
+
     public function testSmoke()
     {
-        $creator = new TaskDatabaseCreator();
+        $couchMock = $this->getCouchMock();
+        $creator = new TaskDatabaseCreator($couchMock);
         $this->assertInstanceOf(TaskDatabaseCreator::class, $creator);
     }
 }
