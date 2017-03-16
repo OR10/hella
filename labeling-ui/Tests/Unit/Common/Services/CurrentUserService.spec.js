874ad26db35ed754a30d2a4dcdac90053cd1cf2e
Dominik Ehrenberg <doe@crosscan.com>
2017-03-16 16:55:14 +0100

    Tests for CurrentUserService

--- labeling-ui/Tests/Unit/Common/Services/CurrentUserService.spec.js
+++ labeling-ui/Tests/Unit/Common/Services/CurrentUserService.spec.js
@@ -1,6 +1,6 @@
 import CurrentUserService from 'Application/Common/Services/CurrentUserService';
 
-fdescribe('CurrentUserService specs', () => {
+describe('CurrentUserService specs', () => {
   it('can be created', () => {
     const currentUserService = new CurrentUserService();
     expect(currentUserService).toEqual(jasmine.any(CurrentUserService));
@@ -21,8 +21,48 @@ fdescribe('CurrentUserService specs', () => {
       const user = {};
       const currentUserService = new CurrentUserService();
       currentUserService.set(user);
-      const returnedUser = currentUserService.get(user);
+      const returnedUser = currentUserService.get();
       expect(returnedUser).toBe(user);
+    });
+  });
+
+  describe('setPermissions/getPermissions', () => {
+    it('throws if there are no permissions', () => {
+      const currentUserService = new CurrentUserService();
+
+      function throwWrapper() {
+        currentUserService.getPermissions();
+      }
+
+      expect(throwWrapper).toThrow();
+    });
+
+    it('returns the permissions that are set', () => {
+      const permissions = {};
+      const currentUserService = new CurrentUserService();
+      currentUserService.setPermissions(permissions);
+      const returnedPermissions = currentUserService.getPermissions();
+      expect(returnedPermissions).toBe(permissions);
+    });
+  });
+
+  describe('setOrganisations/getOrganisations', () => {
+    it('throws if there are no organistations', () => {
+      const currentUserService = new CurrentUserService();
+
+      function throwWrapper() {
+        currentUserService.getOrganisations();
+      }
+
+      expect(throwWrapper).toThrow();
+    });
+
+    it('returns the organisations that are set', () => {
+      const organisations = {};
+      const currentUserService = new CurrentUserService();
+      currentUserService.setOrganisations(organisations);
+      const returnedOrganisations = currentUserService.getOrganisations();
+      expect(returnedOrganisations).toBe(organisations);
     });
   });
 });
