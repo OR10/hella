27dc0b41c53fce6bffdbab284ec5ef1acb3a2afc
Dominik Ehrenberg <doe@crosscan.com>
2017-03-17 16:09:21 +0100

    check that window.addEventListener is called

--- labeling-ui/Application/Common/Services/InProgressService.js
+++ labeling-ui/Application/Common/Services/InProgressService.js
@@ -1,10 +1,22 @@
 class InProgressService {
+  constructor($window) {
+    /**
+     * @var {window}
+     */
+    this.$window = $window;
+  }
+
   _uninstallNavigationInterceptions() {
+
+  }
+
+  _windowBeforeUnload() {
 
   }
 
   start($scope) {
     $scope.$on('$destroy', () => this._uninstallNavigationInterceptions());
+    this.$window.addEventListener('beforeunload', this._windowBeforeUnload);
   }
 }
 
--- labeling-ui/Tests/Unit/Common/Services/InProgressService.spec.js
+++ labeling-ui/Tests/Unit/Common/Services/InProgressService.spec.js
@@ -1,22 +1,36 @@
 import InProgressService from 'Application/Common/Services/InProgressService';
+import {inject} from 'angular-mocks';
 
 fdescribe('InProgressService test suite', () => {
   let inProgress;
+  let windowMock;
 
-  beforeEach(() => {
-    inProgress = new InProgressService();
-  });
+  beforeEach(inject($window => {
+    windowMock = $window;
+    inProgress = new InProgressService(windowMock);
+  }));
 
   it('can be created', () => {
     expect(inProgress).toEqual(jasmine.any(InProgressService));
   });
 
   describe('#start()', () => {
+    let scopeMock;
+
+    beforeEach(() => {
+      scopeMock = jasmine.createSpyObj('$scope', ['$on']);
+    });
+
     it('adds an event listener to the $scope destroy event', () => {
-      const scopeMock = jasmine.createSpyObj('$scope', ['$on']);
       inProgress.start(scopeMock);
 
       expect(scopeMock.$on).toHaveBeenCalledWith('$destroy', jasmine.any(Function));
+    });
+
+    it('adds a callback to beforeunload', () => {
+      spyOn(windowMock, 'addEventListener');
+      inProgress.start(scopeMock);
+      expect(windowMock.addEventListener).toHaveBeenCalledWith('beforeunload', jasmine.any(Function));
     });
   });
 });
