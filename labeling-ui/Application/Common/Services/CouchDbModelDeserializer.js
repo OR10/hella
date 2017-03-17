850b1d5db8ab670eee72f2153fdd053eed54b174
Felix Knischewski <fek@crosscan.com>
2017-03-17 09:17:20 +0100

    Add pouchdb (de)serializer for labeled thing groups

--- labeling-ui/Application/Common/Services/CouchDbModelDeserializer.js
+++ labeling-ui/Application/Common/Services/CouchDbModelDeserializer.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import Task from 'Application/Task/Model/Task';
 import LabeledThing from 'Application/LabelingData/Models/LabeledThing';
 import LabeledThingInFrame from 'Application/LabelingData/Models/LabeledThingInFrame';
+import LabeledThingGroup from 'Application/LabelingData/Models/LabeledThingGroup';
 
 /**
  * The CouchDbModelDeserializer is capable of converting CouchDb document into our internal models.
@@ -81,6 +82,28 @@ class CouchDbModelDeserializer {
       Object.assign({}, document, {task})
     );
   }
+
+  /**
+   * Deserialize a labeledThingGroup to our frontend model
+   *
+   * In order to create a {@link LabeledThingGroup} the corresponding {@link Task}
+   * is needed as a dependency. The dependency needs to be provided as {@link Task} instance.
+   * In order to create this model from a CouchDb document {@link CouchDbModelDeserializer#deserializeTask}
+   * may be used.
+   *
+   * @param {object} labeledThingGroupDocument
+   * @param {Task} task
+   * @return {LabeledThingGroup}
+   */
+  deserializeLabeledThingGroup(labeledThingGroupDocument, task) {
+    const document = this._cloneDocument(labeledThingGroupDocument);
+    this._removePrefixFromIdAndRevision(document);
+
+    return new LabeledThingGroup(
+      Object.assign({}, document, {task})
+    );
+  }
+
 
   /**
    * Deserialize a labeledThingInFrame to our frontend model
