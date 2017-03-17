21b6eae158d320b2d7ba2dd523f4dc6f72b98e28
Konstantin Heinrich <kheinrich188@googlemail.com>
2017-03-17 15:05:03 +0100

    Remove duplicated inheritance code

--- labeling-ui/Application/Viewer/Shapes/PaperPolygon.js
+++ labeling-ui/Application/Viewer/Shapes/PaperPolygon.js
@@ -17,100 +17,10 @@ class PaperPolygon extends PaperPath {
   }
 
   /**
-   * @returns {{width: number, height: number}}
-   */
-  get bounds() {
-    return super.bounds;
-  }
-
-  /**
-   * @return {Array.<Point>}
-   */
-  get points() {
-    return super.points;
-  }
-
-  /**
-   * Select the shape
-   *
-   * @param {Boolean} drawHandles
-   */
-  select(drawHandles = true) {
-    super.select(drawHandles)
-  }
-
-  /**
-   * Deselect the shape
-   */
-  deselect() {
-    super.deselect();
-  }
-
-  /**
    * @returns {string}
    */
   getClass() {
     return PaperPolygon.getClass();
-  }
-
-  /**
-   * @param {Handle|null} handle
-   * @returns {string}
-   */
-  getToolActionIdentifier(handle) {
-    return super.getToolActionIdentifier(handle);
-  }
-
-  /**
-   * @param {Point} point
-   */
-  moveTo(point) {
-    super.moveTo(point);
-  }
-
-  /**
-   * @param {Handle|null} handle
-   * @param {boolean} mouseDown
-   * @returns {string}
-   */
-  getCursor(handle, mouseDown = false) {
-    super.getCursor(handle, mouseDown);
-  }
-
-  /**
-   * @param {Handle} handle
-   * @param {Point} point
-   */
-  resize(handle, point) {
-    super.resize(handle, point);
-  }
-
-  /**
-   * @param {Point} point
-   */
-  addPoint(point) {
-    super.addPoint(point);
-  }
-
-  /**
-   * @param {Point} point
-   */
-  setSecondPoint(point) {
-    super.setSecondPoint(point);
-  }
-
-  /**
-   * Fix the points of the shape to represent the right coordinates
-   */
-  fixOrientation() {
-    super.fixOrientation();
-  }
-
-  /**
-   * @returns {Point}
-   */
-  get position() {
-    return super.position;
   }
 
   toJSON() {
