7461c6476b911ee52aa4125f5fe59a812f76a3e1
Dominik Ehrenberg <doe@crosscan.com>
2017-03-23 15:05:53 +0100

    Add more tests for inProgressService

--- labeling-ui/Tests/Unit/Common/Services/InProgressService.spec.js
+++ labeling-ui/Tests/Unit/Common/Services/InProgressService.spec.js
@@ -27,23 +27,106 @@ describe('InProgressService test suite', () => {
   });
 
   it('can be created', () => {
-    expect(inProgress).toEqual(jasmine.any(InProgressService));
+      expect(inProgress).toEqual(jasmine.any(InProgressService));
   });
 
+  describe('start()', () => {
+    beforeEach(() => {
+      // Always spy on addEventListener, otherwise you will be prompted to really leave the page
+      // at every test run
+      spyOn(windowMock, 'addEventListener');
+    });
 
-  it('adds an event listener to the $rootScope destroy event', () => {
-    spyOn(rootScopeMock, '$on');
-    inProgress.start();
+    it('adds an event listener to beforeunload', () => {
+      inProgress.start();
 
-    expect(rootScopeMock.$on).toHaveBeenCalledWith('$stateChangeStart', jasmine.any(Function));
-    expect(rootScopeMock.$on).toHaveBeenCalledWith('$destroy', jasmine.any(Function));
+      expect(windowMock.addEventListener).toHaveBeenCalledWith('beforeunload', jasmine.any(Function));
+    });
+
+    it('adds an event listener to the $rootScope destroy event', () => {
+      spyOn(rootScopeMock, '$on');
+
+      inProgress.start();
+
+      expect(rootScopeMock.$on).toHaveBeenCalledWith('$stateChangeStart', jasmine.any(Function));
+      expect(rootScopeMock.$on).toHaveBeenCalledWith('$destroy', jasmine.any(Function));
+    });
+
+    it('adds an event listener to the $rootScope $stateChangeStart event', () => {
+      spyOn(rootScopeMock, '$on');
+
+      inProgress.start();
+
+      expect(rootScopeMock.$on).toHaveBeenCalledWith('$stateChangeStart', jasmine.any(Function));
+    });
   });
 
-  it('should remove eventlistners on .end() call', () => {
-    spyOn(windowMock, 'removeEventListener');
+  describe('end()', () => {
+    it('removes the beforeunload event listener', () => {
+      spyOn(windowMock, 'removeEventListener');
 
-    inProgress.end();
+      inProgress.end();
 
-    expect(windowMock.removeEventListener).toHaveBeenCalledWith('beforeunload', jasmine.any(Function));
+      expect(windowMock.removeEventListener).toHaveBeenCalledWith('beforeunload', jasmine.any(Function));
+    });
+
+    it('removes the navigation intercepter', () => {
+      spyOn(windowMock, 'addEventListener');
+
+      inProgress.start();
+      spyOn(inProgress, '_uninstallNavigationInterceptor');
+      expect(inProgress._uninstallNavigationInterceptor).not.toBeNull();
+      inProgress.end();
+
+      expect(inProgress._uninstallNavigationInterceptor).toBeNull();
+    });
+  });
+
+  describe('events', () => {
+    describe('onunload', () => {
+      let beforeUnloadCallback;
+
+      beforeEach(() => {
+        spyOn(windowMock, 'addEventListener').and.callFake((event, callback) => {
+          if (event == 'beforeunload') {
+            beforeUnloadCallback = callback;
+          }
+        });
+      });
+
+      it('returns a message', () => {
+        const event = {};
+        inProgress.start();
+        const message = beforeUnloadCallback(event);
+        expect(message).toEqual(jasmine.any(String));
+      });
+
+      it('sets the returnValue of the event, which is the same as the returned message', () => {
+        const event = {returnValue: 'blablubb'};
+        inProgress.start();
+        const message = beforeUnloadCallback(event);
+        expect(event.returnValue).toEqual(message);
+      });
+    });
+
+    describe('$stateChangeStart', () => {
+      let stateMock;
+
+      beforeEach(inject(($state) => {
+        stateMock = $state;
+        spyOn(stateMock, 'go');
+      }));
+
+      beforeEach(() => {
+        spyOn(windowMock, 'addEventListener');
+        inProgress.start();
+      });
+
+      // it('prevents the default and does not call state.go', () => {
+      //   inProgress.start();
+      //   rootScopeMock.$emit('$stateChangeStart');
+      //   expect(stateMock.go).not.toHaveBeenCalled();
+      // });
+    });
   });
 });
