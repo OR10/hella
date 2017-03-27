6db04b8836bd5655750e55480a44c4b6a8aaaada
Christian Hammerl <chh@crosscan.com>
2017-03-27 17:52:54 +0200

    terraform: syseleven-anno-playgrounds: resize playground

--- terraform/syseleven-anno-playgrounds/anno-demo-1.tf
+++ terraform/syseleven-anno-playgrounds/anno-demo-1.tf
@@ -5,7 +5,7 @@ resource "openstack_compute_floatingip_v2" "anno-demo" {
 resource "openstack_compute_instance_v2" "anno-demo" {
     name = "anno-demo"
     image_id = "39d64de3-c562-4a80-b968-ab46a6838cf4"
-    flavor_name = "m1.small"
+    flavor_name = "m1.tiny"
     key_pair = "${openstack_compute_keypair_v2.crosscan-chh.name}"
 
     security_groups = [
