2938794d0dcd5d56df73671f3c267d2b83543fd9
Konstantin Heinrich <kheinrich188@googlemail.com>
2017-03-29 11:07:31 +0200

    Try to do UT with injection - possible failed

--- labeling-ui/Tests/Unit/LabelingData/PouchDbInterpolation.spec.js
+++ labeling-ui/Tests/Unit/LabelingData/PouchDbInterpolation.spec.js
@@ -9,7 +9,8 @@ import PouchDbHelper from 'Tests/Support/PouchDb/PouchDbHelper';
 import LabeledThing from 'Application/LabelingData/Models/LabeledThing';
 import LinearBackendInterpolation from 'Application/LabelingData/Interpolations/LinearBackendInterpolation'
 import InterpolationService from "Application/LabelingData/Services/InterpolationService";
-
+import TaskModule from "Application/Task/TaskModule";
+import OrganisationModule from "Application/Organisation/Organisation";
 // import PouchDbTimerGateway from 'Application/Header/Gateways/TimerGateway';
 
 // import taskTimerCouchDbModel from 'Tests/Fixtures/Models/CouchDb/TaskTimer';
@@ -19,53 +20,62 @@ fdescribe('PouchDbInterpolation', () => {
   let injector;
   let rootScope;
   let angularQ;
+  let httpBackend;
   
-  let labeledThingGateway;
-  let cache;
-  let cacheHeater;
-  let featureFlags;
-  let pouchDbSyncManager;
+  const featureFlags = { pouchdb: true };
+  const appConfig = {Common: {backendPrefix: '', apiPrefix: ''}};
   let pouchDbContextService;
-  let interpolations;
   let interpolationService;
-  let containerMock;
   
-  beforeEach(done => {
-    Promise.resolve()
-        .then(() => {
-          pouchDbHelper = new PouchDbHelper();
-          return pouchDbHelper.initialize();
-        })
-        .then(() => done());
-    
-  });
   beforeEach(() => {
-    featureFlags = {
-      pouchdb: true,
-    };
+    const commonModule = new Common();
+    commonModule.registerWithAngular(angular, featureFlags);
+    module('AnnoStation.Common');
     
     const labelingDataModule = new LabelingData();
     labelingDataModule.registerWithAngular(angular, featureFlags);
     module('AnnoStation.LabelingData');
+    
+    const applicationTaskModule = new TaskModule();
+    applicationTaskModule.registerWithAngular(angular, featureFlags);
+    module('AnnoStation.Task');
   
-    const pouchDbContextServiceMock = jasmine.createSpyObj('pouchDbContextService', ['provideContextForTaskId']);
+    const orgaModule = new OrganisationModule();
+    orgaModule.registerWithAngular(angular, featureFlags);
+    module('AnnoStation.Organisation');
+  });
+  beforeEach(() => {
+    const app = angular.module('AnnoStation.Common');
+    app.constant('featureFlags', featureFlags);
+    app.constant('applicationConfig', appConfig);
+  });
+  
+  beforeEach(done => {
+    Promise.resolve()
+      .then(() => {
+        pouchDbHelper = new PouchDbHelper();
+        return pouchDbHelper.initialize();
+      })
+      .then(() => done());
+  });
+  beforeEach(() => {
+    const pouchDbContextServiceMock = jasmine.createSpyObj('pouchDbContextService', ['provideContextForTaskId', 'queryTaskIdForContext']);
     pouchDbContextServiceMock.provideContextForTaskId.and.returnValue(pouchDbHelper.database);
-  
+    pouchDbContextServiceMock.queryTaskIdForContext.and.returnValue('some-task-id');
+    
     module($provide => {
        $provide.value('pouchDbContextService', pouchDbContextServiceMock);
-       //labeledThingGateway = jasmine.createSpy('labeledThingGateway');
     });
-    inject(($injector, $rootScope, $q) => {
+    inject(($injector, $rootScope, $q, $httpBackend) => {
       injector = $injector;
       rootScope = $rootScope;
       angularQ = $q;
+      httpBackend = $httpBackend;
     
       interpolationService = injector.instantiate(InterpolationService);
     
     });
   });
-  
-  
   
   function createLabeledThing(startFrameIndex = 0, endFrameIndex = 99, task = {id: 'some-task-id'}, id = 'some-labeled-thing-id') {
     return new LabeledThing({
@@ -77,24 +87,24 @@ fdescribe('PouchDbInterpolation', () => {
     });
   }
   
-  fit('should be able to instantiate without non injected arguments', () => {
+  it('should be able to instantiate without non injected arguments', () => {
     expect(interpolationService instanceof InterpolationService).toEqual(true);
   });
   
-  
-  it('should interpolate something',() => {
+  it('It doesnt throw when interpolating', () => {
     const db = pouchDbHelper.database;
     const task = {id: 'some-task-id'};
     const labeledThingId = 'some-labeled-thing-id';
     const labeledThing = createLabeledThing(0, 200, task, labeledThingId);
-    expect(db).not.toBeNull();
-    expect(labeledThing).not.toBeNull();
-    
-    expect(interpolationService).not.toBeNull();
-    interpolationService.interpolate('default', task, labeledThing);
-    rootScope.$apply();
+    httpBackend.expectGET('/task/some-task-id/replication').respond(500);
+    function throwWrapper() {
+      interpolationService.interpolate('default', task, labeledThing);
+      rootScope.$apply();
+    }
+    throwWrapper();
+    expect(throwWrapper).not.toThrow();
+    httpBackend.flush();
   });
-
   afterEach(done => {
     Promise.resolve()
       .then(() => pouchDbHelper.destroy())
