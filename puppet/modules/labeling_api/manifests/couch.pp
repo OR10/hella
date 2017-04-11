e4a7cedc6252551ccf64417af3f947d0303fbe52
Christian Hammerl <chh@crosscan.com>
2017-04-11 10:28:00 +0200

    wip: secure couchdb

--- puppet/modules/labeling_api/manifests/couch.pp
+++ puppet/modules/labeling_api/manifests/couch.pp
@@ -2,16 +2,53 @@ class labeling_api::couch(
   $database_name = $labeling_api::params::database_name,
   $database_name_read_only = $labeling_api::params::database_name_read_only,
   $prepare_test_environment = $labeling_api::params::prepare_test_environment,
+  $max_open_files = undef,
+  $max_dbs_open = 1000,
+  $max_http_connections = 4048,
 ) {
   include ::couchdb
 
   ::couchdb::database { $database_name:
+    admins => ['admin'],
   }
+
   ::couchdb::database { $database_name_read_only:
   }
 
   if $prepare_test_environment {
     ::couchdb::database { "${database_name}_test":
     }
+  }
+
+  if $max_open_files {
+    ::limits::fragment { 'couchdb/-/nofile':
+      value => $max_open_files,
+    }
+  }
+
+  #file { '/etc/couchdb/local.ini':
+  #  ensure  => present,
+  #  content => '',
+  #  notify  => Service['couchdb'],
+  #}
+
+  file { '/etc/couchdb/local.d/labeling-api.ini':
+    ensure  => file,
+    notify  => Service['couchdb'],
+    content => template('labeling_api/couchdb/labeling-api.ini.erb'),
+    require => Package['couchdb'],
+  }
+
+  file { '/etc/default/couchdb':
+    ensure => present,
+  }
+
+  ->
+
+  file_line { '/etc/default/couchdb':
+    path    => '/etc/default/couchdb',
+    line    => 'ERL_MAX_PORTS=4096',
+    match   => '^.*ERL_MAX_PORTS\s*=.*',
+    notify  => Service['couchdb'],
   }
 }
