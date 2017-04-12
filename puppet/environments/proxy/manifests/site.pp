b71ceb2989512584edaa60a1c2c10cf9033e2a81
Christian Hammerl <chh@crosscan.com>
2017-04-12 10:47:33 +0200

    puppet: labeling-api: create resources defined by hiera

--- puppet/environments/proxy/manifests/site.pp
+++ puppet/environments/proxy/manifests/site.pp
@@ -4,3 +4,7 @@ include ::apt
 include ::annostation_base
 include ::annostation_letsencrypt
 include ::annostation_proxy
+
+create_resources('couchdb::user', hiera('couchdb::users', {}))
+create_resources('limits::fragment', hiera('limits::fragment', {}))
+
