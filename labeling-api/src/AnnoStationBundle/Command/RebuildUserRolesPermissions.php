e6852c0bb8cf0bde462aa36fbfb9c18e59764c12
Christian Tischer <cht@crosscan.com>
2017-04-19 14:01:01 +0200

    Added commandline script to rebuild all user roles

---
+++ labeling-api/src/AnnoStationBundle/Command/RebuildUserRolesPermissions.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AnnoStationBundle\Command;
+
+use AppBundle\Model;
+use AppBundle\Database\Facade as AppBundleFacade;
+use Symfony\Component\Console\Input;
+use Symfony\Component\Console\Output;
+use Symfony\Component\Console\Helper\ProgressBar;
+use crosscan\WorkerPool\AMQP;
+use AnnoStationBundle\Worker\Jobs;
+
+class RebuildUserRolesPermissions extends Base
+{
+    /**
+     * @var AMQP\FacadeAMQP
+     */
+    private $amqpFacade;
+
+    /**
+     * @var AppBundleFacade\User
+     */
+    private $userFacade;
+
+    public function __construct(
+        AppBundleFacade\User $userFacade,
+        AMQP\FacadeAMQP $amqpFacade
+    ) {
+        parent::__construct();
+        $this->amqpFacade = $amqpFacade;
+        $this->userFacade       = $userFacade;
+    }
+
+    protected function configure()
+    {
+        $this->setName('annostation:RebuildUserRolesPermissions')
+            ->setDescription('Rebuild all user roles permissions')
+            ->addArgument(
+                'userId',
+                Input\InputArgument::OPTIONAL,
+                'Rebuild the roles only for this userId.'
+            );
+    }
+
+    protected function execute(Input\InputInterface $input, Output\OutputInterface $output)
+    {
+        $userId = $input->getArgument('userId');
+        if ($userId === null) {
+            $users = $this->userFacade->getUserList();
+        } else {
+            $users = [$this->userFacade->getUserById($userId)];
+        }
+        $progress = new ProgressBar($output, count($users));
+
+        /** @var Model\User $user */
+        foreach ($users as $user) {
+            $this->amqpFacade->addJob(new Jobs\UserRolePermissionRebuilder($user->getId()));
+            $progress->advance();
+        }
+        $progress->finish();
+    }
+}
