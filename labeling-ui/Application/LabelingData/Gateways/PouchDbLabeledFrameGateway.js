6b7a47bbd78bb49abf7099445f30688f700b35cb
Jakob Westhoff <jakob@westhoffswelt.de>
2017-04-26 15:01:05 +0200

    Added empty interface for LabeledFrameGateway

---
+++ labeling-ui/Application/LabelingData/Gateways/PouchDbLabeledFrameGateway.js
@@ -0,0 +1,50 @@
+import LabeledFrame from '../Models/LabeledFrame';
+
+/**
+ * Gateway for saving and retrieving {@link LabeledFrame}s from pouchdb
+ */
+class PouchDbLabeledFrameGateway {
+  constructor() {
+  }
+
+
+  /**
+   * Returns the {@link LabeledFrame} for the given `taskId` and `frameIndex`
+   *
+   * @param {String} taskId
+   * @param {Integer} frameIndex
+   *
+   * @returns {AbortablePromise<LabeledFrame|Error>}
+   */
+  getLabeledFrame(taskId, frameIndex) {
+  }
+
+
+  /**
+   * Updates the labeled frame for the given task and frame number in the database
+   *
+   * @param {String} taskId
+   * @param {Integer} frameIndex
+   * @param {LabeledFrame} labeledFrame
+   *
+   * @returns {AbortablePromise<LabeledFrame|Error>}
+   */
+  saveLabeledFrame(taskId, frameIndex, labeledFrame) {
+  }
+
+  /**
+   * Deletes the labeled thing in frame object in the database
+   *
+   * @param {String} taskId
+   * @param {Integer} frameIndex
+   *
+   * @returns {AbortablePromise<Boolean|Error>}
+   */
+  deleteLabeledFrame(taskId, frameIndex) {
+  }
+}
+
+PouchDbLabeledFrameGateway.$inject = [
+];
+
+export default PouchDbLabeledFrameGateway;
