33b51c11fe3bbe03d9475e83010d91d559d05d50
Christian Tischer <cht@crosscan.com>
2017-04-26 14:22:17 +0200

    Added integration tests for user assignment (_security)

--- labeling-api/src/AnnoStationBundle/Tests/Controller/Api/Task/UserTest.php
+++ labeling-api/src/AnnoStationBundle/Tests/Controller/Api/Task/UserTest.php
@@ -5,9 +5,11 @@ namespace AnnoStationBundle\Tests\Controller\Api\Task;
 use AnnoStationBundle\Tests;
 use AnnoStationBundle\Tests\Controller;
 use AnnoStationBundle\Model as AnnoStationBundleModel;
+use AnnoStationBundle\Service;
 use AppBundle\Model;
 use AnnoStationBundle\Database\Facade;
 use Symfony\Component\HttpFoundation;
+use GuzzleHttp;
 
 class UserTest extends Tests\WebTestCase
 {
@@ -33,6 +35,21 @@ class UserTest extends Tests\WebTestCase
      */
     private $organisationFacade;
 
+    /**
+     * @var GuzzleHttp\Client
+     */
+    private $guzzleClient;
+
+    /**
+     * @var Service\TaskDatabaseCreator
+     */
+    private $taskDatabaseCreatorService;
+
+    /**
+     * @var Service\TaskDatabaseSecurityPermissionService
+     */
+    private $taskDatabaseSecurityPermissionService;
+
     public function testAssignTaskToUser()
     {
         $user = $this->createUser();
@@ -44,17 +61,21 @@ class UserTest extends Tests\WebTestCase
             ->getResponse();
 
         $this->assertEquals(HttpFoundation\Response::HTTP_OK, $response->getStatusCode());
+
+        $this->compareAssignedUserToCouchDbSecurityDocument($task, 'annostation_user');
     }
 
     public function testDeleteAssignedLabelingTask()
     {
         $user = $this->createUser();
         $task = $this->createLabelingTask();
-
-        $this->createRequest(self::ROUTE, [$task->getId(), $user->getId()])
-            ->setMethod(HttpFoundation\Request::METHOD_PUT)
-            ->execute()
-            ->getResponse();
+        $task->addAssignmentHistory(
+            Model\LabelingTask::PHASE_LABELING,
+            Model\LabelingTask::STATUS_TODO,
+            $user,
+            new \DateTime('2017-04-26')
+        );
+        $this->taskDatabaseSecurityPermissionService->updateForTask($task);
 
         $response = $this->createRequest(self::ROUTE, [$task->getId(), $user->getId()])
             ->setMethod(HttpFoundation\Request::METHOD_DELETE)
@@ -62,14 +83,46 @@ class UserTest extends Tests\WebTestCase
             ->getResponse();
 
         $this->assertEquals(HttpFoundation\Response::HTTP_OK, $response->getStatusCode());
+
+        $this->compareAssignedUserToCouchDbSecurityDocument($task, null);
+    }
+
+    private function compareAssignedUserToCouchDbSecurityDocument(Model\LabelingTask $task, $user)
+    {
+        if (!$this->getContainer()->getParameter('pouchdb_feature_enabled')){
+            $this->markTestSkipped('PouchDB not enabled');
+        }
+
+        $resource = $this->guzzleClient->request(
+            'GET',
+            sprintf(
+                'http://%s:%s@%s:%s/%s/_security',
+                $this->getContainer()->getParameter('couchdb_user'),
+                $this->getContainer()->getParameter('couchdb_password'),
+                $this->getContainer()->getParameter('couchdb_host'),
+                $this->getContainer()->getParameter('couchdb_port'),
+                $this->taskDatabaseCreatorService->getDatabaseName(
+                    $task->getProjectId(),
+                    $task->getId()
+                )
+            )
+        );
+
+        $response = json_decode($resource->getBody()->getContents(), true);
+        $this->assertEquals($user, $response['assignedLabeler']);
     }
 
     protected function setUpImplementation()
     {
-        $this->videoFacade        = $this->getAnnostationService('database.facade.video');
-        $this->projectFacade      = $this->getAnnostationService('database.facade.project');
-        $this->labelingTaskFacade = $this->getAnnostationService('database.facade.labeling_task');
-        $this->organisationFacade = $this->getAnnostationService('database.facade.organisation');
+        $this->videoFacade                           = $this->getAnnostationService('database.facade.video');
+        $this->projectFacade                         = $this->getAnnostationService('database.facade.project');
+        $this->labelingTaskFacade                    = $this->getAnnostationService('database.facade.labeling_task');
+        $this->organisationFacade                    = $this->getAnnostationService('database.facade.organisation');
+        $this->taskDatabaseCreatorService            = $this->getAnnostationService('service.task_database_creator');
+        $this->taskDatabaseSecurityPermissionService = $this->getAnnostationService(
+            'service.task_database_security_permission_service'
+        );
+        $this->guzzleClient                          = $this->getService('guzzle.client');
     }
 
     private function createUser(
@@ -97,7 +150,9 @@ class UserTest extends Tests\WebTestCase
             Model\LabelingTask::TYPE_OBJECT_LABELING
         );
         $task->setStatus(Model\LabelingTask::PHASE_LABELING, Model\LabelingTask::STATUS_TODO);
+        $this->labelingTaskFacade->save($task);
+        $this->taskDatabaseCreatorService->createDatabase($project, $task);
 
-        return $this->labelingTaskFacade->save($task);
+        return $task;
     }
 }
