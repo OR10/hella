24e0425ff4c87129710d918e71e7b997a1ae50ab
Konstantin Heinrich <kheinrich188@googlemail.com>
2017-04-26 10:55:56 +0200

    Update frontend interpolation

--- labeling-ui/Application/LabelingData/Interpolations/FrontendInterpolation.js
+++ labeling-ui/Application/LabelingData/Interpolations/FrontendInterpolation.js
@@ -1,4 +1,6 @@
 import uuid from 'uuid';
+import {clone} from 'lodash';
+import {Vector3} from 'three-math';
 
 /**
  * Interpolation base class, for all {@link Interpolation}s, which are executed on the backend
@@ -12,12 +14,14 @@ class FrontendInterpolation {
   /**
    * @param {LabeledThingInFrameGateway} labeledThingInFrameGateway
    */
-  constructor(labeledThingInFrameGateway) {
+  constructor(labeledThingInFrameGateway, $q) {
     /**
      * @type {LabeledThingInFrameGateway}
      * @private
      */
     this._labeledThingInFrameGateway = labeledThingInFrameGateway;
+  
+    this._$q = $q;
   }
 
   /**
@@ -43,93 +47,96 @@ class FrontendInterpolation {
       }
 
       const labeledThingInFrames = labeledThingInFramesWithGhosts.filter(labeledThingInFrame => {
-        return labeledThingInFrame.id !== null;
+        return labeledThingInFrame.ghost === false;
       });
-
+      console.log(labeledThingInFramesWithGhosts)
+      console.log(labeledThingInFrames)
       if (labeledThingInFrames.length <= 1) {
         throw new Error('Error in _doInterpolation: You need more then 1 real labeledThingInFrames for interpolation');
       }
 
-      const indexSet = [];
-      labeledThingInFrames.forEach(labeledThingInFrame => {
-        indexSet.push(labeledThingInFramesWithGhosts.indexOf(labeledThingInFrame));
-      });
+      const labeledThingInFrameIndices = labeledThingInFrames.map(labeledThingInFrame => labeledThingInFrame.frameIndex);
 
-      indexSet.forEach((value, key ) => {
-        if (indexSet.indexOf(indexSet[key + 1]) !== -1) {
-          const lastLabeledThingInFrame = labeledThingInFrames[key + 1];
-          let remainingSteps = indexSet[key + 1] - (key + 1);
-          for (let runner = key + 1; runner < indexSet[key + 1]; runner++) {
-            const currentLabeledThingInFrame = labeledThingInFramesWithGhosts[runner];
+      const savePromises = [];
 
-            const currentShape = currentLabeledThingInFrame.shapes[0];
-            const endShape = lastLabeledThingInFrame.shapes[0];
-
-            this._interpolateShape(currentLabeledThingInFrame, currentShape, endShape, remainingSteps);
-            --remainingSteps;
+      labeledThingInFrameIndices.forEach((currentLtifIndex, ltifIndicesIndex ) => {
+        if (labeledThingInFrameIndices[ltifIndicesIndex + 1 ] !== undefined) {
+          const endLtif = labeledThingInFrames[ltifIndicesIndex + 1];
+          const endLtifIndex = labeledThingInFrameIndices[ltifIndicesIndex + 1];
+          const steps = [];  
+          for (let index = 1; index < (endLtifIndex - currentLtifIndex); index++) {
+            steps.push(index); 
           }
+          steps.forEach(step => {
+            const currentGhostIndex = currentLtifIndex + step;
+            const currentGhost = labeledThingInFramesWithGhosts[currentGhostIndex];
+            const delta = step / (steps.length + 1);
+            savePromises.push(this._interpolateShape(currentGhost, endLtif, delta));
+          });
         }
       });
+      return this._$q.all(savePromises);
     });
   }
 
   /**
    *
-   * @param {LabeledThingInFrame} labeledThingInFrame
-   * @param {PaperThingShape} currentShape
-   * @param {PaperThingShape} endShape
-   * @param {Number} step
+   * @param {LabeledThingInFrame} ghost
+   * @param {LabeledThingInFrame} endLtif
+   * @param {Float} delta
+   * @returns {*}
    * @private
    */
-  _interpolateShape(labeledThingInFrame, currentShape, endShape, step) {
-    switch (currentShape.type) {
+  _interpolateShape(ghost, endLtif, delta) {
+    switch (ghost.shapes[0].type) {
       case 'rectangle':
-        this._interpolateRectangle(labeledThingInFrame, currentShape, endShape, step);
+        return this._interpolateRectangle(ghost, endLtif, delta);
         break;
         /*
       case 'ellipse':
-        this._interpolateEllipse(labeledThingInFrame, currentShape, endShape, step);
+        this._interpolateEllipse(ghost, endLtif, delta);
         break;
-        */
+         */
       case 'pedestrian':
-        this._interpolatePedestrian(labeledThingInFrame, currentShape, endShape, step);
+        return this._interpolatePedestrian(ghost, endLtif, delta);
         break;
       case 'cuboid3d':
-        this._interpolateCuboid3d(labeledThingInFrame, currentShape, endShape, step);
+        return this._interpolateCuboid3d(ghost, endLtif, delta);
         break;
       case 'polygon':
-        this._interpolatePolygonAndPolyline(labeledThingInFrame, currentShape, endShape, step);
+        return this._interpolatePolygonAndPolyline(ghost, endLtif, delta);
         break;
       case 'polyline':
-        this._interpolatePolygonAndPolyline(labeledThingInFrame, currentShape, endShape, step);
+        return this._interpolatePolygonAndPolyline(ghost, endLtif, delta);
         break;
       case 'point':
-        this._interpolatePoint(labeledThingInFrame, currentShape, endShape, step);
+        return this._interpolatePoint(ghost, endLtif, delta);
         break;
       default:
+        throw new Error(`Unknown shape type ${ghost.shapes[0].type}`);
     }
   }
 
-  _interpolateRectangle(labeledThingInFrame, currentShape, endShape, step) {
-    const currentTopLeft = currentShape.topLeft;
-    const currentBottomRight = currentShape.bottomRight;
-    const endTopLeft = endShape.topLeft;
-    const endBottomRight = endShape.bottomRight;
+  _interpolateRectangle(ltifGhost, endLtif, delta) {
+    const currentTopLeft = clone(ltifGhost.shapes[0].topLeft);
+    const currentBottomRight = clone(ltifGhost.shapes[0].bottomRight);
+    const endTopLeft = clone(endLtif.shapes[0].topLeft);
+    const endBottomRight = clone(endLtif.shapes[0].bottomRight);
 
     const topLeft = {
-      x: currentTopLeft.x + (endTopLeft.x - currentTopLeft.x) / step,
-      y: currentTopLeft.y + (endTopLeft.y - currentTopLeft.y) / step,
+      x: currentTopLeft.x + (endTopLeft.x - currentTopLeft.x) * delta,
+      y: currentTopLeft.y + (endTopLeft.y - currentTopLeft.y) * delta,
     };
     const bottomRight = {
-      x: currentBottomRight.x + (endBottomRight.x - currentBottomRight.x) / step,
-      y: currentBottomRight.y + (endBottomRight.y - currentBottomRight.y) / step,
+      x: currentBottomRight.x + (endBottomRight.x - currentBottomRight.x) * delta,
+      y: currentBottomRight.y + (endBottomRight.y - currentBottomRight.y) * delta,
     };
 
-    currentShape.topLeft = topLeft;
-    currentShape.bottomRight = bottomRight;
+    ltifGhost.shapes[0].topLeft = topLeft;
+    ltifGhost.shapes[0].bottomRight = bottomRight;
 
-    this._transformGhostToLabeledThing(labeledThingInFrame);
-    this._saveLabeledThingInFrame(labeledThingInFrame);
+    const transformedGhost = this._transformGhostToLabeledThing(ltifGhost);
+    return this._saveLabeledThingInFrame(transformedGhost);
   }
 
   /*
@@ -138,31 +145,31 @@ class FrontendInterpolation {
   }
   */
 
-  _interpolatePedestrian(labeledThingInFrame, currentShape, endShape, step) {
-    const currentTopCenter = currentShape.topCenter;
-    const currentBottomCenter = currentShape.bottomCenter;
-    const endTopCenter = endShape.topCenter;
-    const endBottomCenter = endShape.bottomCenter;
+  _interpolatePedestrian(ltifGhost, endLtif, delta) {
+    const currentTopCenter = clone(ltifGhost.shapes[0].topCenter);
+    const currentBottomCenter = clone(ltifGhost.shapes[0].bottomCenter);
+    const endTopCenter = clone(endLtif.shapes[0].topCenter);
+    const endBottomCenter = clone(endLtif.shapes[0].bottomCenter);
 
     const topCenter = {
-      x: currentTopCenter.x + (endTopCenter.x - currentTopCenter.x) / step,
-      y: currentTopCenter.y + (endTopCenter.y - currentTopCenter.y) / step,
+      x: currentTopCenter.x + (endTopCenter.x - currentTopCenter.x) * delta,
+      y: currentTopCenter.y + (endTopCenter.y - currentTopCenter.y) * delta,
     };
     const bottomCenter = {
-      x: currentBottomCenter.x + (endBottomCenter.x - currentBottomCenter.x) / step,
-      y: currentBottomCenter.y + (endBottomCenter.y - currentBottomCenter.y) / step,
+      x: currentBottomCenter.x + (endBottomCenter.x - currentBottomCenter.x) * delta,
+      y: currentBottomCenter.y + (endBottomCenter.y - currentBottomCenter.y) * delta,
     };
 
-    currentShape.topCenter = topCenter;
-    currentShape.bottomCenter = bottomCenter;
+    ltifGhost.shapes[0].topCenter = topCenter;
+    ltifGhost.shapes[0].bottomCenter = bottomCenter;
 
-    this._transformGhostToLabeledThing(labeledThingInFrame);
-    this._saveLabeledThingInFrame(labeledThingInFrame);
+    const transformedGhost = this._transformGhostToLabeledThing(ltifGhost);
+    return this._saveLabeledThingInFrame(transformedGhost);
   }
 
-  _interpolatePolygonAndPolyline(labeledThingInFrame, currentShape, endShape, step) {
-    const currentPoints = currentShape.points;
-    const endPoints = endShape.points;
+  _interpolatePolygonAndPolyline(ltifGhost, endLtif, delta) {
+    const currentPoints = clone(ltifGhost.shapes[0].points);
+    const endPoints = clone(endLtif.shapes[0].points);
     const points = [];
 
     if (currentPoints.length !== endPoints.length) {
@@ -171,58 +178,58 @@ class FrontendInterpolation {
 
     currentPoints.forEach((point, index) => {
       const newCalculatePoint = {
-        x: point.x + (endPoints[index].x - point.x) / step,
-        y: point.y + (endPoints[index].y - point.y) / step,
+        x: point.x + (endPoints[index].x - point.x) * delta,
+        y: point.y + (endPoints[index].y - point.y) * delta,
       };
       points.push(newCalculatePoint);
     });
 
-    currentShape.points = points;
+    ltifGhost.shapes[0].points = points;
 
-    this._transformGhostToLabeledThing(labeledThingInFrame);
-    this._saveLabeledThingInFrame(labeledThingInFrame);
+    const transformedGhost = this._transformGhostToLabeledThing(ltifGhost);
+    return this._saveLabeledThingInFrame(transformedGhost);
   }
 
-  _interpolatePoint(labeledThingInFrame, currentShape, endShape, step) {
-    const currentPoint = currentShape.point;
-    const endPoint = endShape.point;
+  _interpolatePoint(ltifGhost, endLtif, delta) {
+    const currentPoint =  clone(ltifGhost.shapes[0].point);
+    const endPoint = clone(endLtif.shapes[0].point);
 
     const point = {
-      x: currentPoint.x + (endPoint.x - currentPoint.x) / step,
-      y: currentPoint.y + (endPoint.y - currentPoint.y) / step,
+      x: currentPoint.x + (endPoint.x - currentPoint.x) * delta,
+      y: currentPoint.y + (endPoint.y - currentPoint.y) * delta,
     };
-    currentShape.point = point;
 
-    this._transformGhostToLabeledThing(labeledThingInFrame);
-    this._saveLabeledThingInFrame(labeledThingInFrame);
+    ltifGhost.shapes[0].point = point;
+
+    const transformedGhost = this._transformGhostToLabeledThing(ltifGhost);
+    return this._saveLabeledThingInFrame(transformedGhost);
   }
 
-  _interpolateCuboid3d(labeledThingInFrame, currentShape, endShape, step) {
+  _interpolateCuboid3d(ltifGhost, endLtif, delta) {
     const newCuboid3d = [];
-    const currentCuboid = this._getCuboidFromRect(currentShape, endShape);
-    const endCuboid = this._getCuboidFromRect(endShape, currentShape);
+    const currentCuboid = this._getCuboidFromRect(clone(ltifGhost.shapes[0]), clone(endLtif.shapes[0]));
+    const endCuboid = this._getCuboidFromRect(clone(endLtif.shapes[0]), clone(ltifGhost.shapes[0]));
 
     for (let index = 0; index <= 7; index++) {
       const newCoordinates = this._cuboid3dCalculateNewVertex(
           currentCuboid.vehicleCoordinates[index],
           endCuboid.vehicleCoordinates[index],
-          step
+          delta
       );
       newCuboid3d.push(newCoordinates);
     }
     currentCuboid.vehicleCoordinates = newCuboid3d;
-
-    this._transformGhostToLabeledThing(labeledThingInFrame);
-    this._saveLabeledThingInFrame(labeledThingInFrame);
+  
+    const transformedGhost = this._transformGhostToLabeledThing(ltifGhost);
+    return this._saveLabeledThingInFrame(transformedGhost);
   }
 
   _getCuboidFromRect(currentCuboid, endCuboid) {
-    // debugger;
     const numberOfCurrentInvisibleVertices = currentCuboid.vehicleCoordinates.filter(vertex => {
-      return vertex === null;
+      return vertex !== null;
     });
     const numberOfEndInvisibleVertices = endCuboid.vehicleCoordinates.filter(vertex => {
-      return vertex === null;
+      return vertex !== null;
     });
     console.log(numberOfCurrentInvisibleVertices);
     console.log(numberOfEndInvisibleVertices);
@@ -239,39 +246,138 @@ class FrontendInterpolation {
     }
 
     let oppositeVertex;
-    console.log(Object.keys(invisibleVerticesIndex));
-    switch (Object.keys(invisibleVerticesIndex)) {
-      case new Array('0', '1', '2', '3'):
-        console.log('0, 1, 2, 3');
+    switch (Object.keys(invisibleVerticesIndex).toString()) {
+      case '0,1,2,3':
+        oppositeVertex = {
+          0:4,
+          1:5,
+          2:6,
+          3:7,
+          'normal': [[6, 5], [6, 7]]
+        };
         break;
-      case new Array('1', '2', '5', '6'):
-        console.log('1, 2, 5, 6');
+      case '1,2,5,6':
+        oppositeVertex = {
+          1:0,
+          2:3,
+          5:4,
+          6:7,
+          'normal': [[7, 4], [7, 3]]
+        };
         break;
-      case new Array('4', '5', '6', '7'):
-        console.log('4, 5, 6, 7');
+      case '4,5,6,7':
+        oppositeVertex = {
+          4:0,
+          5:1,
+          6:2,
+          7:3,
+          'normal': [[3, 0], [3, 2]]
+        };
         break;
-      case new Array('0', '3', '4', '7'):
-        console.log('0, 3, 4, 7');
+      case '0,3,4,7':
+        oppositeVertex = {
+          0:1,
+          3:2,
+          4:5,
+          7:6,
+          'normal': [[2, 1], [2, 6]]
+        };
         break;
-      case new Array('0', '1', '4', '5'):
-        console.log('0, 1, 4, 5');
+      case '0,1,4,5':
+        oppositeVertex = {
+          0:3,
+          1:2,
+          4:7,
+          5:6,
+          'normal': [[3, 7], [3, 2]]
+        };
         break;
-      case new Array('2', '3', '6', '7'):
-        console.log('2, 3, 6, 7');
+      case '2,3,6,7':
+        oppositeVertex = {
+          2:1,
+          3:0,
+          6:5,
+          7:4,
+          'normal': [[1, 0], [1, 5]]
+        };
         break;
       default:
-        oppositeVertex = new Array();
+        throw new Error('Something went wrong with 3D Cuboid that seems to be a 2D object');
     }
+    
+    //return this.filter(function(i) {return a.indexOf(i) < 0;});
+    console.log(currentCuboid.vehicleCoordinates);
+    console.log(oppositeVertex.normal[0][0]);
+   
+    const plainVector1 = currentCuboid.vehicleCoordinates[oppositeVertex.normal[0][0]].filter(index => {
+      return currentCuboid.vehicleCoordinates[oppositeVertex.normal[0][0]].indexOf(index) < 0;
+    });
+    
+    const plainVector2 = currentCuboid.vehicleCoordinates[oppositeVertex.normal[1][0]].filter(index => {
+      return currentCuboid.vehicleCoordinates[oppositeVertex.normal[1][1]].indexOf(index) < 0;
+    });
+    /**
+    console.log(plainVector1);
+    console.log(plainVector2);
+    // need to get cross product
+    // const v1 = new Vector3(plainVector1);
+    // const v2 = new Vector3(plainVector2);
+     const normalVector = v1.cross(v2);
+    
+    // get distance
+    
+    // const endDistanceKey = endCuboid.vehicleCoordinates[Object.keys(oppositeVertex)[0]];
+    // const endDistanceValues = endCuboid.vehicleCoordinates[Object.values(oppositeVertex)[0]];
+    
+    // const distance = new Vector3()
+    
+    //get distance vector
+    
+    // const distanceVector = normalVector.divide(normalVector.length()).multiply(distance);
+    
+    // console.log(endDistanceKey);
+    // console.log(endDistanceValues);
+    
+    const newVertices = {
+      'id': currentCuboid.id,
+      'type': currentCuboid.type,
+      'vehicleCoordinates': []
+    };
+    
+    oppositeVertex.forEach(sourceVertexIndex, targetVertexIndex => {
+      if (targetVertexIndex === 'normal'){
+      
+      }
+      let sourceVertex = currentCuboid.vehicleCoordinates[sourceVertexIndex];
+      //newVertices.vehicleCoordinates[sourceVertex] = sourceVertex.add(distanceVector);
+    });
+    new Array(7).forEach(index => {
+      if (typeof newVertices.vehicleCoordinates[index] === 'undefined'){
+        newVertices.vehicleCoordinates[index] = currentCuboid.vehicleCoordinates[index];
+      }
+    });
+    */
+    //return newVertices;
+    
+    
+    //console.log(normalVector);
   }
-
-  _cuboid3dCalculateNewVertex(currentVertex, endVertex, steps) {
+  
+  /**
+   * @param currentVertex
+   * @param endVertex
+   * @param delta
+   * @returns {*}
+   * @private
+   */
+  _cuboid3dCalculateNewVertex(currentVertex, endVertex, delta) {
     if (currentVertex === null && endVertex === null) {
       return null;
     }
     return [
-      currentVertex[0] + (endVertex[0] - currentVertex[0]) / steps,
-      currentVertex[1] + (endVertex[1] - currentVertex[1]) / steps,
-      currentVertex[2] + (endVertex[2] - currentVertex[2]) / steps,
+      currentVertex[0] + (endVertex[0] - currentVertex[0]) * delta,
+      currentVertex[1] + (endVertex[1] - currentVertex[1]) * delta,
+      currentVertex[2] + (endVertex[2] - currentVertex[2]) * delta,
     ];
   }
 
@@ -282,10 +388,15 @@ class FrontendInterpolation {
   _transformGhostToLabeledThing(labeledThingInFrame) {
     if (labeledThingInFrame.id === null) {
       labeledThingInFrame.id = uuid.v4();
+    } else {
+      throw new Error('labeledThingInFrame.id should be null')
     }
     if (labeledThingInFrame.ghost === true) {
       labeledThingInFrame.ghost = false;
+    } else {
+      throw new Error('labeledThingInFrame.ghost should be true')
     }
+    return labeledThingInFrame;
   }
 
   /**
@@ -293,7 +404,7 @@ class FrontendInterpolation {
    * @private
    */
   _saveLabeledThingInFrame(labeledThingInFrame) {
-    this._labeledThingInFrameGateway.saveLabeledThingInFrame(labeledThingInFrame)
+    return this._labeledThingInFrameGateway.saveLabeledThingInFrame(labeledThingInFrame)
       .then(() => {
         return labeledThingInFrame;
       })
@@ -305,6 +416,7 @@ class FrontendInterpolation {
 
 FrontendInterpolation.$inject = [
   'labeledThingInFrameGateway',
+  '$q',
 ];
 
 export default FrontendInterpolation;
