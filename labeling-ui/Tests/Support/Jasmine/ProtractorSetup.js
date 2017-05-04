dd0b37960fb85f759ebe3033af3bc118ade4d603
Dominik Ehrenberg <doe@crosscan.com>
2017-05-04 17:14:08 +0200

    Add Todo for tomorrow

--- labeling-ui/Tests/Support/Jasmine/ProtractorSetup.js
+++ labeling-ui/Tests/Support/Jasmine/ProtractorSetup.js
@@ -1,14 +1,12 @@
 let protractorPromise;
 
 beforeEach(() => {
-  // browser.wait(() => {
-  //   return browser.executeScript('return !!window.angular');
-  // });
-
   protractorPromise = protractor.promise.defer();
 });
 
 afterEach(() => {
   protractorPromise.fulfill('ok');
   expect(protractorPromise).toBe('ok');
+
+  // @TODO: Add pouch.destroy() to empty database after each test case
 });
