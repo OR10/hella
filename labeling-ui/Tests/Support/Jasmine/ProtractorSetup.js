1b1eb2a6b8dd013b708d6a958ea229ea86504a02
Dominik Ehrenberg <doe@crosscan.com>
2017-05-05 16:22:11 +0200

    Cleanup Pouch after every testcase

--- labeling-ui/Tests/Support/Jasmine/ProtractorSetup.js
+++ labeling-ui/Tests/Support/Jasmine/ProtractorSetup.js
@@ -1,12 +1,26 @@
 let protractorPromise;
+var features = require('../../../Application/features.json');
+var configuration = require('../../../Application/Common/config.json');
 
 beforeEach(() => {
   protractorPromise = protractor.promise.defer();
 });
 
-afterEach(() => {
+afterEach(done => {
   protractorPromise.fulfill('ok');
   expect(protractorPromise).toBe('ok');
 
-  // @TODO: Add pouch.destroy() to empty database after each test case
+  if (features.pouchdb) {
+    const pouchDocument = browser.executeAsyncScript((configuration, callback) => {
+      const db = new PouchDB(`TASKID-TASKID-${configuration.storage.local.databaseName}`);
+
+      return db.destroy().then((result) => {
+        callback(result);
+      });
+    }, configuration);
+
+    pouchDocument.then(() => {
+      done();
+    });
+  }
 });
