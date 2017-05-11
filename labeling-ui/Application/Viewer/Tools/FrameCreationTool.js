8335a038fbeaaab63166123d90d4d2aaa0a8fa32
Felix Knischewski <fek@crosscan.com>
2017-05-11 16:59:35 +0200

    Add new frame creation tool

---
+++ labeling-ui/Application/Viewer/Tools/FrameCreationTool.js
@@ -0,0 +1,102 @@
+import CreationTool from './CreationTool';
+import NotModifiedError from './Errors/NotModifiedError';
+
+/**
+ * A tool for drawing rectangle shapes with the mouse cursor
+ *
+ * @extends DrawingTool
+ * @implements ToolEvents
+ */
+class FrameCreationTool extends CreationTool {
+  /**
+   * @param {DrawingContext} drawingContext
+   * @param {$rootScope.Scope} $rootScope
+   * @param {$q} $q
+   * @param {LoggerService} loggerService
+   * @param {HierarchyCreationService} hierarchyCreationService
+   */
+  constructor(drawingContext, $rootScope, $q, loggerService, hierarchyCreationService) {
+    super(drawingContext, $rootScope, $q, loggerService, hierarchyCreationService);
+  }
+
+  onMouseUp() {
+    this._reject(new NotModifiedError('Can not create frame shape with this FrameCreationTool.'));
+  }
+
+  /**
+   * @param {CreationToolActionStruct} toolActionStruct
+   * @return {Promise.<PaperShape>}
+   */
+  invokeShapeCreation(toolActionStruct) {
+    return super.invokeShapeCreation(toolActionStruct);
+  }
+
+  /**
+   * @return {boolean}
+   */
+  get supportsDefaultShapeCreation() {
+    return false;
+  }
+
+}
+
+/**
+ * Return the name of the tool. The name needs to be unique within the application.
+ * Therefore something like a prefix followed by the className is advisable.
+ *
+ * @return {string}
+ * @public
+ * @abstract
+ * @static
+ */
+FrameCreationTool.getToolName = () => {
+  return 'FrameCreationTool';
+};
+
+/**
+ * Check if the given ShapeClass ({@link PaperShape#getClass}) is supported by this Tool.
+ *
+ * It specifies mostly which shape is affected by the given tool (eg. `rectangle`, `cuboid`, `multi`, ...)
+ *
+ * There maybe multiple Tools with the same name, but different action identifiers. (`rectangle` and ´move`,
+ * `rectangle` and `scale`, ...)
+ *
+ * @return {bool}
+ * @public
+ * @abstract
+ * @static
+ */
+FrameCreationTool.isShapeClassSupported = shapeClass => {
+  return [
+    'frame',
+  ].includes(shapeClass);
+};
+
+/**
+ * Check if the given actionIdentifer is supported by this tool.
+ *
+ * Currently supported actions are:
+ * - `creating`
+ * - `scale`
+ * - `move`
+ *
+ * @return {bool}
+ * @public
+ * @abstract
+ * @static
+ */
+FrameCreationTool.isActionIdentifierSupported = actionIdentifier => {
+  return [
+    'creation',
+  ].includes(actionIdentifier);
+};
+
+FrameCreationTool.$inject = [
+  'drawingContext',
+  '$rootScope',
+  '$q',
+  'loggerService',
+  'hierarchyCreationService',
+];
+
+export default FrameCreationTool;
