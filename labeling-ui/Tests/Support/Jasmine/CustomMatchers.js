4302e73cce3db80e63e5a7cfd60471b9aec073f0
Dominik Ehrenberg <doe@crosscan.com>
2017-05-11 12:22:33 +0200

    Next custom matcher for pouch

--- labeling-ui/Tests/Support/Jasmine/CustomMatchers.js
+++ labeling-ui/Tests/Support/Jasmine/CustomMatchers.js
@@ -4,17 +4,18 @@ beforeEach(function() {
   jasmine.addMatchers({
     toEqualRenderedDrawingStack: require('./Matchers/EqualRenderedDrawingStack'),
     toMatchBelowThreshold: require('./Matchers/MatchBelowThreshold'),
-    toContainNamedParamsRequestOnce: require('./Matchers/ContainNamedParamsRequestOnce'),
   });
 
   if (features.pouchdb) {
     jasmine.addMatchers({
       toContainNamedParamsRequest: require('./Matchers/Pouch/ContainNamedParamsRequest'),
+      toContainNamedParamsRequestOnce: require('./Matchers/Pouch/ContainNamedParamsRequest'),
       toContainRequest: require('./Matchers/Pouch/ContainNamedParamsRequest'),
     });
   } else {
     jasmine.addMatchers({
       toContainNamedParamsRequest: require('./Matchers/ContainNamedParamsRequest'),
+      toContainNamedParamsRequestOnce: require('./Matchers/ContainNamedParamsRequestOnce'),
       toContainRequest: require('./Matchers/ContainRequest'),
     });
   }
