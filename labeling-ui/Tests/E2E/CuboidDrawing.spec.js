8aabff5f0eded243c483b41bb501ae904a67faff
Dominik Ehrenberg <doe@crosscan.com>
2017-05-11 12:13:41 +0200

    Do not focus the DepthBuffer anymore

--- labeling-ui/Tests/E2E/CuboidDrawing.spec.js
+++ labeling-ui/Tests/E2E/CuboidDrawing.spec.js
@@ -9,7 +9,7 @@ import AssetHelper from '../Support/Protractor/AssetHelper';
 
 const canvasInstructionLogManager = new CanvasInstructionLogManager(browser);
 
-describe('Cuboid Drawing', () => {
+fdescribe('Cuboid Drawing', () => {
   let assets;
   let sharedMocks;
   let viewer;
@@ -193,7 +193,7 @@ describe('Cuboid Drawing', () => {
     });
   });
 
-  fdescribe('DepthBuffer', () => {
+  describe('DepthBuffer', () => {
     it('should properly render a cuboid with low height from all sides', done => {
       mock(sharedMocks, [
         assets.mocks.CuboidDrawing.DepthBufferVeryLow.LabeledThingInFrame.frameIndex0,
