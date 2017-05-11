1cd5357947ee92704618dee55f41916a6cddeb38
Felix Knischewski <fek@crosscan.com>
2017-05-11 17:08:26 +0200

    Add paper frame

---
+++ labeling-ui/Application/Viewer/Shapes/PaperFrame.js
@@ -0,0 +1,54 @@
+import PaperShape from './PaperShape';
+
+class PaperFrame extends PaperShape {
+  /**
+   * @param {LabeledFrame} labeledFrame
+   */
+  constructor(labeledFrame) {
+    // Constant values are passed to partent constructor since the shape will never be rendered
+    super(`frame-shape-for-frame-${labeledFrame.frameIndex}`, {primary: 1, secondary: 1});
+
+    /**
+     * @type {LabeledFrame}
+     * @private
+     */
+    this._labeledFrame = labeledFrame;
+  }
+
+  /**
+   * @return {LabeledFrame}
+   */
+  get labeledFrame() {
+    return this._labeledFrame;
+  }
+
+  /**
+   * @abstract
+   * @method PaperShape#select
+   */
+  select() {
+  }
+
+  /**
+   * @abstract
+   * @method PaperShape#deselect
+   */
+  deselect() {
+  }
+
+  /**
+   * @return {Object}
+   */
+  toJSON() {
+    return {};
+  }
+}
+
+/**
+ * @return {string}
+ */
+PaperFrame.getClass = () => {
+  return 'frame';
+};
+
+export default PaperFrame;
