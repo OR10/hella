43efeb4f5d02b9278ca960e068af7b4addfaa5f7
Dominik Ehrenberg <doe@crosscan.com>
2017-05-12 14:09:42 +0200

    Check that document with id is not present anymore when checking for delete requests

--- labeling-ui/Tests/Support/Jasmine/Matchers/Pouch/ContainNamedParamsRequest.js
+++ labeling-ui/Tests/Support/Jasmine/Matchers/Pouch/ContainNamedParamsRequest.js
@@ -1,6 +1,32 @@
 var configuration = require('../../../../../Application/Common/config.json');
 import {matchDocuments, lastMatchChecked} from './MatchDocuments';
 
+function checkLabeledThingAndLabeledThingInFrame(namedParamsRequestData, allPouchDocuments, overallResult) {
+  if (namedParamsRequestData.labeledThing && namedParamsRequestData.labeledThingInFrame) {
+    namedParamsRequestData = namedParamsRequestData.labeledThingInFrame;
+  }
+  const matchingDocuments = allPouchDocuments.rows.filter(row => matchDocuments(namedParamsRequestData, row.doc));
+  const result = matchingDocuments.length > 0;
+  const lastMatchMade = lastMatchChecked();
+  overallResult.message = `0 matching documents found. Last Check made: Expected key "${lastMatchMade.key} to be "${lastMatchMade.expected}". Got "${lastMatchMade.actual}"`;
+
+  return result;
+}
+
+function isDocumentDeleted(namedParamsRequestData, allPouchDocuments, overallResult) {
+  const isDeleted = !checkLabeledThingAndLabeledThingInFrame(namedParamsRequestData, allPouchDocuments, overallResult);
+  if (!isDeleted) {
+    overallResult.message = `Expected document with id ${namedParamsRequestData.id} to be deleted.`;
+  }
+  return isDeleted;
+}
+
+function getIdFromUrl(url) {
+  const pattern = new RegExp(/.*\/(.*)$/);
+  const matches = pattern.exec(url);
+  return matches[1];
+}
+
 module.exports = function toContainNamedParamsRequest() {
   return {
     compare: function (mockedRequests, namedParamsMock) {
@@ -18,15 +44,19 @@ module.exports = function toContainNamedParamsRequest() {
       }, configuration);
 
       overallResult.pass = pouchQuery.then((allPouchDocuments) => {
-        let namedParamsRequestData = namedParamsMock.request.data;
-        if (namedParamsRequestData.labeledThing && namedParamsRequestData.labeledThingInFrame) {
-          namedParamsRequestData = namedParamsRequestData.labeledThingInFrame;
+        let namedParamsRequestData;
+        const requestMethod = namedParamsMock.request.method.toUpperCase();
+
+        if (requestMethod === 'DELETE') {
+          const id = getIdFromUrl(namedParamsMock.request.path);
+          namedParamsRequestData = {
+            id: id,
+          };
+          return isDocumentDeleted(namedParamsRequestData, allPouchDocuments, overallResult);
+        } else {
+          namedParamsRequestData = namedParamsMock.request.data;
+          return checkLabeledThingAndLabeledThingInFrame(namedParamsRequestData, allPouchDocuments, overallResult);
         }
-        const matchingDocuments = allPouchDocuments.rows.filter(row => matchDocuments(namedParamsRequestData, row.doc));
-        const result = matchingDocuments.length > 0;
-        const lastMatchMade = lastMatchChecked();
-        overallResult.message = `0 matching documents found. Last Check made: Expected key "${lastMatchMade.key} to be "${lastMatchMade.expected}". Got "${lastMatchMade.actual}"`;
-        return result;
       });
 
       return overallResult;
