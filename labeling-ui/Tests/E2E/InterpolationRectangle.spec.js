3ddb2eff0fb1c74561d8f5c0012102709559a572
Dominik Ehrenberg <doe@crosscan.com>
2017-05-15 13:52:06 +0200

    First interpolation test for Rectangle

---
+++ labeling-ui/Tests/E2E/InterpolationRectangle.spec.js
@@ -0,0 +1,112 @@
+import CanvasInstructionLogManager from '../Support/CanvasInstructionLogManager';
+import { expectAllModalsToBeClosed, getMockRequestsMade, initApplication, mock} from '../Support/Protractor/Helpers';
+import AssetHelper from '../Support/Protractor/AssetHelper';
+import featureFlags from '../../Application/features.json';
+
+const canvasInstructionLogManager = new CanvasInstructionLogManager(browser);
+
+fdescribe('Interpolation Rectangle Tests', () => {
+  if (!featureFlags.pouchdb) {
+    pending('These tests only work with activated Pouch');
+  }
+
+  let assets;
+  let sharedMocks;
+  let viewer;
+
+  beforeEach(() => {
+    assets = new AssetHelper(`${__dirname}/../Fixtures`, `${__dirname}/../ProtractorMocks`);
+    sharedMocks = [
+      assets.mocks.Shared.TaskDb,
+      assets.mocks.Shared.UserProfile,
+      assets.mocks.Shared.UserPermissions,
+      assets.mocks.Shared.UserOrganisations,
+      assets.mocks.Shared.Video,
+      assets.mocks.Shared.LabelStructure,
+      assets.mocks.Shared.GetTimer,
+      assets.mocks.Shared.PutTimer,
+      assets.mocks.Shared.LabeledThingIncompleteCount,
+      assets.mocks.Shared.FrameLocations.SourceJpg.frameIndex0,
+      assets.mocks.Shared.FrameLocations.SourceJpg.frameIndex0to4,
+      assets.mocks.Shared.FrameLocations.Thumbnail.frameIndex0,
+      assets.mocks.Shared.FrameLocations.Thumbnail.frameIndex0to4,
+      assets.mocks.Shared.Thumbnails.rectangleLabeledThingsInFrame0to3,
+      assets.mocks.Shared.Thumbnails.rectangleLabeledThingsInFrame0to4,
+      assets.mocks.Shared.EmptyLabeledThingGroupInFrame,
+      assets.mocks.Interpolation.Shared.Task,
+      assets.mocks.Interpolation.Rectangle.LabeledThingInFrame.frameIndex0and4,
+    ];
+
+    viewer = element(by.css('.layer-container'));
+  });
+
+  it('should interpolate a Rectangle', done => {
+    let nextFrameButton;
+
+    mock(sharedMocks);
+
+    initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
+      .then(() => {
+        browser.actions()
+          .mouseMove(viewer, {x: 150, y: 150}) // Rectangle in first frame
+          .click()
+          .perform();
+      })
+      .then(() => {
+        const interpolateButton = element(by.css('#interpolate-shape-button'));
+        interpolateButton.click();
+      })
+      .then(() => {
+        nextFrameButton = element(by.css('.next-frame-button'));
+      })
+      .then(
+        // () => canvasInstructionLogManager.getAnnotationCanvasLogs('InterpolationRectangle', 'Frame0')
+        () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+      )
+      .then(drawingStack => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.InterpolationRectangle.Frame0);
+      })
+      .then(() => nextFrameButton.click())
+      .then(() => browser.sleep(500))
+      .then(
+        // () => canvasInstructionLogManager.getAnnotationCanvasLogs('InterpolationRectangle', 'Frame1')
+        () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+      )
+      .then(drawingStack => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.InterpolationRectangle.Frame1);
+      })
+      .then(() => nextFrameButton.click())
+      .then(() => browser.sleep(500))
+      .then(
+        // () => canvasInstructionLogManager.getAnnotationCanvasLogs('InterpolationRectangle', 'Frame2')
+        () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+      )
+      .then(drawingStack => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.InterpolationRectangle.Frame2);
+      })
+      .then(() => nextFrameButton.click())
+      .then(() => browser.sleep(500))
+      .then(
+        // () => canvasInstructionLogManager.getAnnotationCanvasLogs('InterpolationRectangle', 'Frame3')
+        () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+      )
+      .then(drawingStack => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.InterpolationRectangle.Frame3);
+      })
+      .then(() => nextFrameButton.click())
+      .then(() => browser.sleep(500))
+      .then(
+        // () => canvasInstructionLogManager.getAnnotationCanvasLogs('InterpolationRectangle', 'Frame4')
+        () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+      )
+      .then(drawingStack => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.InterpolationRectangle.Frame4);
+      })
+      .then(done);
+  });
+
+  afterEach(() => {
+    expectAllModalsToBeClosed();
+    mock.teardown();
+  });
+});
