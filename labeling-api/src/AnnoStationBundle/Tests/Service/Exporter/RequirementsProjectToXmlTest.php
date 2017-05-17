64410e8743412fd04fb7a011ed2b94b603cdcc74
Christian Tischer <cht@crosscan.com>
2017-05-17 09:36:29 +0200

    Fixed MetaLabeling Export FrameRange

--- labeling-api/src/AnnoStationBundle/Tests/Service/Exporter/RequirementsProjectToXmlTest.php
+++ labeling-api/src/AnnoStationBundle/Tests/Service/Exporter/RequirementsProjectToXmlTest.php
@@ -127,7 +127,7 @@ class RequirementsProjectToXmlTest extends Tests\CouchDbTestCase
             $databaseDocumentManager = $this->getDocumentManagerForTask($task);
             $this->labeledFrameFacade = new Facade\LabeledFrame($databaseDocumentManager);
         }
-        $labeledFrame = Tests\Helper\LabeledFrameBuilder::create($task, 1)
+        $labeledFrame = Tests\Helper\LabeledFrameBuilder::create($task, 0)
             ->withClasses(['sun', 'summer'])
             ->build();
         $this->labeledFrameFacade->save($labeledFrame);
