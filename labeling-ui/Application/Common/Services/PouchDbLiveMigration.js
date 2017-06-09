6f0e09c7b496ad434b42f3e938f68e019ca08bba
Jakob Westhoff <jakob@westhoffswelt.de>
2017-06-09 12:33:14 +0200

    Implemented service to execute pouchdb live migrations

--- labeling-ui/Application/Common/Services/PouchDbLiveMigration.js
+++ labeling-ui/Application/Common/Services/PouchDbLiveMigration.js
@@ -1,12 +1,19 @@
 class PouchDbLiveMigration {
   constructor() {
+    this._migrations = [
+    ];
+
+    this._migrationsCount = this._migrations.length;
   }
 
   /**
    * Install available migrations with the `transform-pouch` plugin for processing on the given pouchdb instance
+   * @param {PouchDB} db
    */
   install(db) {
-
+    db.transform({
+      outgoing: doc => this._transformOutgoing(doc)
+    });
   }
 
   /**
@@ -15,7 +22,21 @@ class PouchDbLiveMigration {
    * @private
    */
   _transformOutgoing(doc) {
+    // Skip design documents
+    if (doc._id && doc._id.indexOf('_design/') === 0) {
+      return doc;
+    }
 
+    for(let index = 0; index < this._migrationsCount; ++index) {
+      const migration = this._migrations[index];
+      if (!migration.supportsDocument(doc)) {
+        continue;
+      }
+
+      migration.migrateDocument(doc);
+    }
+
+    return doc;
   }
 }
 
