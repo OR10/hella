e415a8e43afeb10ce4d5deb0451000a0b6ddd958
Felix Knischewski <fek@crosscan.com>
2017-06-23 10:24:10 +0200

    Set the correct labeledThingInFrameId in the shapes during ghost bust

--- labeling-ui/Application/LabelingData/Models/LabeledThingInFrame.js
+++ labeling-ui/Application/LabelingData/Models/LabeledThingInFrame.js
@@ -151,6 +151,7 @@ class LabeledThingInFrame extends LabeledObject {
       throw new Error('Can\'t realize ghosted LabeledThingInFrame, as it is no ghost');
     }
 
+    this.shapes.forEach(shape => shape.labeledThingInFrameId = id);
     this.ghost = false;
     this.id = id;
     this.frameIndex = frameIndex;
