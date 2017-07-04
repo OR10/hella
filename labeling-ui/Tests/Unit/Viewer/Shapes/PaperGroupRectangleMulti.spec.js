dde1814907971902dcfda243acbf32ac1d748a34
Dominik Ehrenberg <doe@crosscan.com>
2017-07-04 16:06:53 +0200

    subgroup creation test

--- labeling-ui/Tests/Unit/Viewer/Shapes/PaperGroupRectangleMulti.spec.js
+++ labeling-ui/Tests/Unit/Viewer/Shapes/PaperGroupRectangleMulti.spec.js
@@ -14,4 +14,25 @@ fdescribe('PaperGroupRectangleMulti Test Suite', () => {
     const group = new PaperGroupRectangleMulti(null, null, shapes, null);
     expect(group).toEqual(jasmine.any(PaperGroupRectangleMulti));
   });
+
+  it('creates a group for every shape upon instantation', () => {
+    const firstShape = {
+      bounds: {x: 1, y: 1, width: 10, height: 10},
+    };
+    const secondShape = {
+      bounds: {x: 2, y: 2, width: 20, height: 20},
+    };
+    const labeledThingGroupInFrame = {
+      id: 'foobar-heinz'
+    };
+    const color = {
+      primary: 'first-color',
+      secondary: 'second-color'
+    };
+
+    const shapes = [firstShape, secondShape];
+    const group = new PaperGroupRectangleMulti(labeledThingGroupInFrame, null, shapes, color);
+    const groupRectangles = group.children;
+    expect(groupRectangles.length).toEqual(2);
+  });
 });
