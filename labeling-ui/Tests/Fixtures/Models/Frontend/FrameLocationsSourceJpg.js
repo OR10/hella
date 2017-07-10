123e4440a4ba7abc984394d5e1bcc87cda61fc76
Jakob Westhoff <jakob@westhoffswelt.de>
2017-07-10 16:53:10 +0200

    Added FrameLocations fixtures

---
+++ labeling-ui/Tests/Fixtures/Models/Frontend/FrameLocationsSourceJpg.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    'frameIndex': 0,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/22.jpg'
+  },
+  {
+    'frameIndex': 1,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/44.jpg'
+  },
+  {
+    'frameIndex': 2,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/66.jpg'
+  },
+  {
+    'frameIndex': 3,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/88.jpg'
+  },
+  {
+    'frameIndex': 4,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/110.jpg'
+  },
+  {
+    'frameIndex': 5,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/132.jpg'
+  },
+  {
+    'frameIndex': 6,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/154.jpg'
+  },
+  {
+    'frameIndex': 7,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/176.jpg'
+  },
+  {
+    'frameIndex': 8,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/198.jpg'
+  },
+  {
+    'frameIndex': 9,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/220.jpg'
+  },
+  {
+    'frameIndex': 10,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/242.jpg'
+  },
+  {
+    'frameIndex': 11,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/264.jpg'
+  },
+  {
+    'frameIndex': 12,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/286.jpg'
+  },
+  {
+    'frameIndex': 13,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/308.jpg'
+  },
+  {
+    'frameIndex': 14,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/330.jpg'
+  },
+  {
+    'frameIndex': 15,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/352.jpg'
+  },
+  {
+    'frameIndex': 16,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/374.jpg'
+  },
+  {
+    'frameIndex': 17,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/396.jpg'
+  },
+  {
+    'frameIndex': 18,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/418.jpg'
+  },
+  {
+    'frameIndex': 19,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/440.jpg'
+  },
+  {
+    'frameIndex': 20,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/462.jpg'
+  },
+  {
+    'frameIndex': 21,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/484.jpg'
+  },
+  {
+    'frameIndex': 22,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/506.jpg'
+  },
+  {
+    'frameIndex': 23,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/528.jpg'
+  },
+  {
+    'frameIndex': 24,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/550.jpg'
+  },
+  {
+    'frameIndex': 25,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/572.jpg'
+  },
+  {
+    'frameIndex': 26,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/594.jpg'
+  },
+  {
+    'frameIndex': 27,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/616.jpg'
+  },
+  {
+    'frameIndex': 28,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/638.jpg'
+  },
+  {
+    'frameIndex': 29,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/sourceJpg\/660.jpg'
+  }
+];
---
+++ labeling-ui/Tests/Fixtures/Models/Frontend/FrameLocationsThumbnail.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    'frameIndex': 0,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/22.jpg'
+  },
+  {
+    'frameIndex': 1,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/44.jpg'
+  },
+  {
+    'frameIndex': 2,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/66.jpg'
+  },
+  {
+    'frameIndex': 3,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/88.jpg'
+  },
+  {
+    'frameIndex': 4,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/110.jpg'
+  },
+  {
+    'frameIndex': 5,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/132.jpg'
+  },
+  {
+    'frameIndex': 6,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/154.jpg'
+  },
+  {
+    'frameIndex': 7,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/176.jpg'
+  },
+  {
+    'frameIndex': 8,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/198.jpg'
+  },
+  {
+    'frameIndex': 9,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/220.jpg'
+  },
+  {
+    'frameIndex': 10,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/242.jpg'
+  },
+  {
+    'frameIndex': 11,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/264.jpg'
+  },
+  {
+    'frameIndex': 12,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/286.jpg'
+  },
+  {
+    'frameIndex': 13,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/308.jpg'
+  },
+  {
+    'frameIndex': 14,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/330.jpg'
+  },
+  {
+    'frameIndex': 15,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/352.jpg'
+  },
+  {
+    'frameIndex': 16,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/374.jpg'
+  },
+  {
+    'frameIndex': 17,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/396.jpg'
+  },
+  {
+    'frameIndex': 18,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/418.jpg'
+  },
+  {
+    'frameIndex': 19,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/440.jpg'
+  },
+  {
+    'frameIndex': 20,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/462.jpg'
+  },
+  {
+    'frameIndex': 21,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/484.jpg'
+  },
+  {
+    'frameIndex': 22,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/506.jpg'
+  },
+  {
+    'frameIndex': 23,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/528.jpg'
+  },
+  {
+    'frameIndex': 24,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/550.jpg'
+  },
+  {
+    'frameIndex': 25,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/572.jpg'
+  },
+  {
+    'frameIndex': 26,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/594.jpg'
+  },
+  {
+    'frameIndex': 27,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/616.jpg'
+  },
+  {
+    'frameIndex': 28,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/638.jpg'
+  },
+  {
+    'frameIndex': 29,
+    'url': 'https:\/\/192.168.222.20:444\/6c4a1eda0252fea4271a45d08d090ebf\/thumbnail\/660.jpg'
+  }
+];
