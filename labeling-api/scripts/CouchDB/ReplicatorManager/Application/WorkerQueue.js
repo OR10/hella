f0903f687d0794a531e663320e050d7bb5ee0c85
Christian Tischer <cht@crosscan.com>
2017-07-11 11:11:54 +0200

    Improved tests

--- labeling-api/scripts/CouchDB/ReplicatorManager/Application/WorkerQueue.js
+++ labeling-api/scripts/CouchDB/ReplicatorManager/Application/WorkerQueue.js
@@ -1,13 +1,14 @@
 const { compactReplicationDatabase } = require('./Utils');
 
 class WorkerQueue {
-  constructor(nanoAdmin, maxSimultaneousJobs = 50, compactReplicationDbCycle = 500) {
+  constructor(nanoAdmin, logger, maxSimultaneousJobs = 50, compactReplicationDbCycle = 500) {
+    this.nanoAdmin = nanoAdmin;
+    this.logger = logger;
     this.maxSimultaneousJobs = maxSimultaneousJobs;
     this.compactReplicationDbCycle = compactReplicationDbCycle;
     this.compactReplicationCounter = 0;
     this.queue = [];
     this.activeTasks = [];
-    this.nanoAdmin = nanoAdmin;
   }
 
   /**
@@ -76,8 +77,7 @@ class WorkerQueue {
         this.doWork();
       })
       .catch(error => {
-        // eslint-disable-next-line no-console
-        console.log(`Failed adding replication: ${error}`);
+        this.logger.logString(`Failed adding replication: ${error}`);
         const index = this.activeTasks.findIndex(task => task.id === element.id);
         if (index !== -1) {
           this.activeTasks.splice(index, 1);
@@ -92,7 +92,7 @@ class WorkerQueue {
   compactReplicationDatabase() {
     this.compactReplicationCounter += 1;
     if (this.compactReplicationCounter >= this.compactReplicationDbCycle) {
-      compactReplicationDatabase(this.nanoAdmin);
+      compactReplicationDatabase(this.nanoAdmin, this.logger);
       this.compactReplicationCounter = 0;
     }
   }
@@ -117,8 +117,7 @@ class WorkerQueue {
   }
 
   _printQueueStatus() {
-    // eslint-disable-next-line no-console
-    console.log(`Active tasks: ${this.activeTasks.length}/${this.maxSimultaneousJobs} | Queue length: ${this.queue.length}`);
+    this.logger.logString(`Active tasks: ${this.activeTasks.length}/${this.maxSimultaneousJobs} | Queue length: ${this.queue.length}`);
   }
 }
 
