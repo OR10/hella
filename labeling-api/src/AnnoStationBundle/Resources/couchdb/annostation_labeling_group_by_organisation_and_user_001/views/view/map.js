20d2218153fe1ecacec43f594415e077fe994f3f
Christian Tischer <cht@crosscan.com>
2017-07-20 10:16:59 +0200

    Removed deprecated couchdb views/filters

--- labeling-api/src/AnnoStationBundle/Resources/couchdb/annostation_labeling_group_by_organisation_and_user_001/views/view/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-    if (doc.type === 'AppBundle.Model.LabelingGroup') {
-        doc.coordinators.forEach(function (coordinator) {
-            emit([doc.organisationId, coordinator], {id: doc._id, name: doc.name});
-        });
-    }
-}
