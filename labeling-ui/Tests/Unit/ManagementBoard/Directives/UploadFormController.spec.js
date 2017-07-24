12829dc140af849470402cdc40c01cf1631245ff
Dominik Ehrenberg <doe@crosscan.com>
2017-07-24 14:49:32 +0200

    Testcases for uploadComplete, uploadFilesFromSystem and clearFiles

--- labeling-ui/Tests/Unit/ManagementBoard/Directives/UploadFormController.spec.js
+++ labeling-ui/Tests/Unit/ManagementBoard/Directives/UploadFormController.spec.js
@@ -1,4 +1,6 @@
 import UploadFormController from 'Application/ManagementBoard/Directives/UploadFormController';
+import ListDialog from 'Tests/Support/Mocks/ListDialog';
+
 
 fdescribe('UploadFormController test suite', () => {
   /**
@@ -46,14 +48,28 @@ fdescribe('UploadFormController test suite', () => {
    */
   let timeout;
 
+  /**
+   * @type {Object}
+   */
+  let promiseResult;
+
+  /**
+   * @type {Object}
+   */
+  let $flow;
+
   beforeEach(inject(($q, $rootScope, $timeout) => {
     promise = $q;
     rootScope = $rootScope;
     timeout = $timeout;
+
+    promiseResult = {
+      missing3dVideoCalibrationData: [],
+    };
   }));
 
   beforeEach(() => {
-    const ListDialog = () => {};
+
     uploadGateway = jasmine.createSpyObj('uploadGateway', ['getApiUrl', 'markUploadAsFinished']);
     organisationService = jasmine.createSpyObj('organisationService', ['get', 'subscribe']);
     inProgressService = jasmine.createSpyObj('inProgressService', ['start', 'end']);
@@ -72,30 +88,25 @@ fdescribe('UploadFormController test suite', () => {
     );
   });
 
+  beforeEach(() => {
+    $flow = {
+      files: [],
+    };
+    controller.$flow = $flow;
+  });
+
   it('can be created', () => {
     expect(controller).toEqual(jasmine.any(UploadFormController));
-    expect(controller.$flow).toBeNull();
+    expect(controller.$flow).toBe($flow);
     expect(uploadGateway.getApiUrl).toHaveBeenCalledTimes(1);
     expect(organisationService.get).toHaveBeenCalledTimes(1);
     expect(organisationService.subscribe).toHaveBeenCalledWith(jasmine.any(Function));
   });
 
   describe('Removing upload progress bar (TTANNO-1818)', () => {
-    let $flow;
-
-    const result = {
-      missing3dVideoCalibrationData: [],
-    };
-
-    beforeEach(() => {
-      $flow = {
-        files: [],
-      };
-      controller.$flow = $flow;
-    });
 
     it('removes the upload progress bar if upload was fine', () => {
-      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(result));
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(promiseResult));
       controller.uploadComplete();
       rootScope.$apply();
 
@@ -104,7 +115,7 @@ fdescribe('UploadFormController test suite', () => {
     });
 
     it('does not remove the upload progress bar if timeout is not reached', () => {
-      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(result));
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(promiseResult));
       controller.uploadComplete();
       rootScope.$apply();
 
@@ -112,7 +123,7 @@ fdescribe('UploadFormController test suite', () => {
     });
 
     it('does not remove the upload progress bar if a second upload has been started in the meantime', () => {
-      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(result));
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(promiseResult));
 
       controller.uploadComplete();
       rootScope.$apply();
@@ -123,7 +134,7 @@ fdescribe('UploadFormController test suite', () => {
     });
 
     it('removes the upload progress bar after another timeout if a second upload has been started in the meantime and is then completed', () => {
-      uploadGateway.markUploadAsFinished.and.callFake(() => promise.resolve(result));
+      uploadGateway.markUploadAsFinished.and.callFake(() => promise.resolve(promiseResult));
 
       controller.uploadComplete();
       controller.uploadInProgress = true;
@@ -147,18 +158,6 @@ fdescribe('UploadFormController test suite', () => {
       rootScope.$apply();
       timeout.flush();
 
-      const modalParams = {
-        title: 'Upload completed with errors',
-        headline: jasmine.any(String),
-        message: jasmine.any(Array),
-        confirmButtonText: 'Understood'
-      };
-      expect(modalService.info).toHaveBeenCalledWith(
-        modalParams,
-        undefined,
-        undefined,
-        jasmine.any(Object)
-      );
       expect(uploadService.reset).toHaveBeenCalledTimes(1);
     });
 
@@ -187,6 +186,172 @@ fdescribe('UploadFormController test suite', () => {
       timeout.flush();
 
       expect(uploadService.reset).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('uploadComplete()', () => {
+    it('sets the uploadInProgress to false', () => {
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(promiseResult));
+
+      controller.uploadInProgress = true;
+      controller.uploadComplete();
+      rootScope.$apply();
+
+      expect(controller.uploadInProgress).toBe(false);
+    });
+
+    it('calls end on the inProgressService', () => {
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(promiseResult));
+
+      controller.uploadComplete();
+      rootScope.$apply();
+
+      expect(inProgressService.end).toHaveBeenCalled();
+    });
+
+    it('shows a modal if markUploadAsFinished had an error', () => {
+      const promiseResult = {
+        missing3dVideoCalibrationData: [],
+        error: {
+          message: 'PANIK',
+        },
+      };
+
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(promiseResult));
+
+      const modalParams = {
+        title: 'Upload completed with errors',
+        headline: jasmine.any(String),
+        message: jasmine.any(Array),
+        confirmButtonText: 'Understood'
+      };
+
+      controller.uploadComplete();
+      rootScope.$apply();
+
+      expect(modalService.info).toHaveBeenCalledWith(
+        modalParams,
+        undefined,
+        undefined,
+        jasmine.any(Object)
+      );
+    });
+
+    it('shows an error modal with the files if there were incomplete files', () => {
+      const completeFile = { hasUploadError: () => false };
+      const incompleteFile = { hasUploadError: () => true };
+      const files = [completeFile, incompleteFile];
+      $flow.files = files;
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve({
+        missing3dVideoCalibrationData: files
+      }));
+
+      controller.uploadComplete();
+      rootScope.$apply();
+      timeout.flush();
+
+      expect(modalService.show).toHaveBeenCalled();
+      const modalServiceShowArguments = modalService.show.calls.argsFor(0);
+      expect(modalServiceShowArguments[0].config.data).toBe(files);
+    });
+
+    it('shows an error modal if there were files with upload errors, but none incomplete', () => {
+      const completeFile = { hasUploadError: () => false };
+      const incompleteFile = { hasUploadError: () => true };
+      const files = [completeFile, incompleteFile];
+      $flow.files = files;
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve({
+        missing3dVideoCalibrationData: []
+      }));
+
+      controller.uploadComplete();
+      rootScope.$apply();
+      timeout.flush();
+
+      const modalParams = {
+        title: 'Upload completed with errors',
+        headline: jasmine.any(String),
+        message: jasmine.any(Array),
+        confirmButtonText: 'Understood'
+      };
+
+      expect(modalService.info).toHaveBeenCalledWith(
+        modalParams,
+        undefined,
+        undefined,
+        jasmine.any(Object)
+      );
+    });
+
+    it('shows a modal if upload was finished successfully, no incomplete files', () => {
+      const uploadCompleteMessage = 'Bernd das Brot';
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve(promiseResult));
+
+      controller.uploadCompleteMessage = uploadCompleteMessage;
+      controller.uploadComplete();
+      rootScope.$apply();
+
+      const modalParams = {
+        title: 'Upload complete',
+        headline: 'Your upload is complete',
+        message: uploadCompleteMessage,
+        confirmButtonText: jasmine.any(String),
+      };
+      expect(modalService.info).toHaveBeenCalledWith(modalParams, jasmine.any(Function));
+    });
+
+    it('shows a modal if upload was finished successfully, but there were incomplete files', () => {
+      const files = ['one', 'two', 'three'];
+
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.resolve({
+        missing3dVideoCalibrationData: files
+      }));
+
+      controller.uploadComplete();
+      rootScope.$apply();
+
+      expect(modalService.show).toHaveBeenCalled();
+      const modalServiceShowArguments = modalService.show.calls.argsFor(0);
+      expect(modalServiceShowArguments[0].config.data).toBe(files);
+    });
+
+    it('shows an error modal if markUploadAsFinished was rejected', () => {
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.reject());
+
+      controller.uploadComplete();
+      rootScope.$apply();
+
+      const modalParams = {
+        title: 'Upload completed with errors',
+        headline: 'Your upload is complete, but errors occured.',
+        message: jasmine.any(Array),
+        confirmButtonText: jasmine.any(String),
+      };
+      expect(modalService.info).toHaveBeenCalledWith(
+        modalParams,
+        undefined,
+        undefined,
+        jasmine.any(Object)
+      );
+    });
+
+    it('sets uploadInProgress to false if markUploadAsFinished was rejected', () => {
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.reject());
+
+      controller.uploadInProgress = true;
+      controller.uploadComplete();
+      rootScope.$apply();
+
+      expect(controller.uploadInProgress).toBe(false);
+    });
+
+    it('calls end on the inProgressService if markUploadAsFinished was rejected', () => {
+      uploadGateway.markUploadAsFinished.and.returnValue(promise.reject());
+
+      controller.uploadComplete();
+      rootScope.$apply();
+
+      expect(inProgressService.end).toHaveBeenCalled();
     });
   });
 
@@ -319,6 +484,58 @@ fdescribe('UploadFormController test suite', () => {
       const color = controller.fileColorClass(file);
 
       expect(color).toEqual('upload-success');
+    });
+  });
+
+  describe('uploadFilesFromSystem', () => {
+    let getElementById;
+    let element;
+    let original;
+
+    beforeEach(() => {
+      getElementById = jasmine.createSpy('document.getElementById()');
+      element = jasmine.createSpyObj('element', ['click']);
+      getElementById.and.returnValue(element);
+
+      original = document.getElementById;
+      document.getElementById = getElementById;
+    });
+
+    afterEach(() => {
+      document.getElementById = original;
+    });
+
+    it('clicks an element after a timeout', () => {
+      controller.uploadFilesFromSystem();
+      timeout.flush();
+
+      expect(getElementById).toHaveBeenCalledWith('project-upload-input');
+      expect(element.click).toHaveBeenCalled();
+    });
+
+    it('does nothing if the timeout was not reached', () => {
+      controller.uploadFilesFromSystem();
+
+      expect(getElementById).not.toHaveBeenCalled();
+      expect(element.click).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('clearFiles', () => {
+    beforeEach(() => {
+      $flow.cancel = jasmine.createSpy('$flow.cancel()');
+    });
+
+    it('calls cancel on the flow object', () => {
+      controller.clearFiles();
+
+      expect($flow.cancel).toHaveBeenCalled();
+    });
+
+    it('calls reset on the uploadService', () => {
+      controller.clearFiles();
+
+      expect(uploadService.reset).toHaveBeenCalled();
     });
   });
 });
