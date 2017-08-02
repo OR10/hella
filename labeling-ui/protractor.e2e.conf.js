ba87036ac5f3ab746f8cc958de461821a65c86d3
Dominik Ehrenberg <doe@crosscan.com>
2017-08-02 08:12:27 +0200

    Add easy possibility to run e2e tests in non-headless-mode

--- labeling-ui/protractor.e2e.conf.js
+++ labeling-ui/protractor.e2e.conf.js
@@ -81,7 +81,10 @@ if (typeof process.env.PROTRACTOR_SELENIUM_GRID !== 'undefined') {
     'browserName': 'chrome',
     'chromeOptions': {
       'binary': '/Applications/Chromium.app/Contents/MacOS/Chromium',
-      'args': [ "--headless", "--window-size=1920,1080" ],
     },
   };
+
+  if (typeof process.env.NO_HEADLESS === 'undefined') {
+    exports.config.capabilities.chromeOptions.args = [ "--headless", "--window-size=1920,1080" ];
+  }
 }
