f0b3735f9dc506c6db38eacad341d5ba4a98bfa9
Dominik Ehrenberg <doe@crosscan.com>
2017-08-04 17:08:36 +0200

    Testcase to only select shapes of same type

--- labeling-ui/Tests/E2E/MultiSelect.spec.js
+++ labeling-ui/Tests/E2E/MultiSelect.spec.js
@@ -17,8 +17,10 @@ fdescribe('MultiSelect', () => {
       assets.mocks.Shared.UserProfile,
       assets.mocks.Shared.UserPermissions,
       assets.mocks.Shared.UserOrganisations,
-      assets.mocks.Shared.Task,
-      assets.mocks.Shared.Video,
+      assets.mocks.MultipleThings.Shared.Task,
+      assets.mocks.MultipleThings.Shared.Video,
+      assets.mocks.MultipleThings.Shared.TaskConfiguration,
+      assets.mocks.MultipleThings.Shared.TaskConfigurationFile,
       assets.mocks.Shared.LabelStructure,
       assets.mocks.Shared.GetTimer,
       assets.mocks.Shared.PutTimer,
@@ -32,8 +34,6 @@ fdescribe('MultiSelect', () => {
       assets.mocks.Shared.EmptyLabeledThingGroupInFrame,
     ];
 
-    mock(sharedMocks);
-
     viewer = element(by.css('.layer-container'));
   });
 
@@ -42,31 +42,45 @@ fdescribe('MultiSelect', () => {
     mock.teardown();
   });
 
-  const firstRectangle = {
+  const firstShape = {
     topLeft: {x: 100, y: 100},
     bottomRight: {x: 200, y: 200},
   };
 
-  const secondRectangle = {
+  const secondShape = {
     topLeft: {x: 250, y: 250},
     bottomRight: {x: 350, y: 450},
   };
 
-  const thirdRectangle = {
+  const thirdShape = {
     topLeft: {x: 150, y: 500},
     bottomRight: {x: 350, y: 570},
   };
 
-  const fourthRectangle = {
+  const fourthShape = {
     topLeft: {x: 400, y: 400},
     bottomRight: {x: 700, y: 500},
   };
 
+  const toolButton0 = element(by.css('button.tool-button.tool-thing.tool-0'));
+  const toolButton1 = element(by.css('button.tool-button.tool-thing.tool-1'));
+
   function drawRectangle(rectangle) {
     return browser.actions()
+      .click(toolButton0) // Rect drawing
       .mouseMove(viewer, rectangle.topLeft) // initial position
       .mouseDown()
       .mouseMove(viewer, rectangle.bottomRight) // initial position
+      .mouseUp()
+      .perform();
+  }
+
+  function drawPedestrian(pedestrian) {
+    return browser.actions()
+      .click(toolButton1) // Rect drawing
+      .mouseMove(viewer, pedestrian.topLeft) // initial position
+      .mouseDown()
+      .mouseMove(viewer, pedestrian.bottomRight) // initial position
       .mouseUp()
       .perform();
   }
@@ -79,19 +93,21 @@ fdescribe('MultiSelect', () => {
   }
 
   it('should draw four and then additionally select the first three rectangles', done => {
+    mock(sharedMocks);
+
     initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
-      .then(() => drawRectangle(firstRectangle))
-      .then(() => drawRectangle(secondRectangle))
-      .then(() => drawRectangle(thirdRectangle))
-      .then(() => drawRectangle(fourthRectangle))
+      .then(() => drawRectangle(firstShape))
+      .then(() => drawRectangle(secondShape))
+      .then(() => drawRectangle(thirdShape))
+      .then(() => drawRectangle(fourthShape))
       .then(() => {
         return browser.actions()
           .sendKeys(protractor.Key.CONTROL)
-          .mouseMove(viewer, firstRectangle.topLeft)
+          .mouseMove(viewer, firstShape.topLeft)
           .click()
-          .mouseMove(viewer, secondRectangle.topLeft)
+          .mouseMove(viewer, secondShape.topLeft)
           .click()
-          .mouseMove(viewer, thirdRectangle.topLeft)
+          .mouseMove(viewer, thirdShape.topLeft)
           .click()
           .sendKeys(protractor.Key.NULL)
           .perform();
@@ -109,18 +125,20 @@ fdescribe('MultiSelect', () => {
   });
 
   it('should draw four, deselect and then select two of the rectangles with ctrl+click', done => {
+    mock(sharedMocks);
+
     initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
-      .then(() => drawRectangle(firstRectangle))
-      .then(() => drawRectangle(secondRectangle))
-      .then(() => drawRectangle(thirdRectangle))
-      .then(() => drawRectangle(fourthRectangle))
+      .then(() => drawRectangle(firstShape))
+      .then(() => drawRectangle(secondShape))
+      .then(() => drawRectangle(thirdShape))
+      .then(() => drawRectangle(fourthShape))
       .then(deselectAfterDrawing)
       .then(() => {
         return browser.actions()
           .sendKeys(protractor.Key.CONTROL)
-          .mouseMove(viewer, thirdRectangle.topLeft)
+          .mouseMove(viewer, thirdShape.topLeft)
           .click()
-          .mouseMove(viewer, secondRectangle.topLeft)
+          .mouseMove(viewer, secondShape.topLeft)
           .click()
           .sendKeys(protractor.Key.NULL)
           .perform();
@@ -138,18 +156,20 @@ fdescribe('MultiSelect', () => {
   });
 
   it('should draw four, deselect and then select one with normal click and a second rectangle with ctrl+click', done => {
+    mock(sharedMocks);
+
     initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
-      .then(() => drawRectangle(firstRectangle))
-      .then(() => drawRectangle(secondRectangle))
-      .then(() => drawRectangle(thirdRectangle))
-      .then(() => drawRectangle(fourthRectangle))
+      .then(() => drawRectangle(firstShape))
+      .then(() => drawRectangle(secondShape))
+      .then(() => drawRectangle(thirdShape))
+      .then(() => drawRectangle(fourthShape))
       .then(deselectAfterDrawing)
       .then(() => {
         return browser.actions()
-          .mouseMove(viewer, thirdRectangle.topLeft)
+          .mouseMove(viewer, thirdShape.topLeft)
           .click()
           .sendKeys(protractor.Key.CONTROL)
-          .mouseMove(viewer, secondRectangle.topLeft)
+          .mouseMove(viewer, secondShape.topLeft)
           .click()
           .sendKeys(protractor.Key.NULL)
           .perform();
@@ -167,20 +187,22 @@ fdescribe('MultiSelect', () => {
   });
 
   it('should should select and deselect with ctrl+click', done => {
+    mock(sharedMocks);
+
     initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
-      .then(() => drawRectangle(firstRectangle))
-      .then(() => drawRectangle(secondRectangle))
-      .then(() => drawRectangle(thirdRectangle))
-      .then(() => drawRectangle(fourthRectangle))
+      .then(() => drawRectangle(firstShape))
+      .then(() => drawRectangle(secondShape))
+      .then(() => drawRectangle(thirdShape))
+      .then(() => drawRectangle(fourthShape))
       .then(deselectAfterDrawing)
       .then(() => {
         return browser.actions()
-          .mouseMove(viewer, thirdRectangle.topLeft)
+          .mouseMove(viewer, thirdShape.topLeft)
           .click()
           .sendKeys(protractor.Key.CONTROL)
-          .mouseMove(viewer, firstRectangle.topLeft)
+          .mouseMove(viewer, firstShape.topLeft)
           .click()
-          .mouseMove(viewer, secondRectangle.topLeft)
+          .mouseMove(viewer, secondShape.topLeft)
           .click()
           .sendKeys(protractor.Key.NULL)
           .perform();
@@ -195,7 +217,7 @@ fdescribe('MultiSelect', () => {
       .then(() => {
         return browser.actions()
           .sendKeys(protractor.Key.CONTROL)
-          .mouseMove(viewer, firstRectangle.topLeft)
+          .mouseMove(viewer, firstShape.topLeft)
           .click()
           .sendKeys(protractor.Key.NULL)
           .perform();
@@ -213,24 +235,26 @@ fdescribe('MultiSelect', () => {
   });
 
   it('should remove the label selector once more than one rectangle is selected', done => {
+    mock(sharedMocks);
+
     const labelSelector = element(by.css('label-selector'));
     const labelSelectorHelper = new LabelSelectorHelper(labelSelector);
 
     initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
-      .then(() => drawRectangle(firstRectangle))
-      .then(() => drawRectangle(secondRectangle))
-      .then(() => drawRectangle(thirdRectangle))
-      .then(() => drawRectangle(fourthRectangle))
+      .then(() => drawRectangle(firstShape))
+      .then(() => drawRectangle(secondShape))
+      .then(() => drawRectangle(thirdShape))
+      .then(() => drawRectangle(fourthShape))
       .then(() => {
         return browser.actions()
-          .mouseMove(viewer, thirdRectangle.topLeft)
+          .mouseMove(viewer, thirdShape.topLeft)
           .click()
       })
-      .then(() => expect(labelSelectorHelper.getNumberOfVisiblePanes()).toBe(4))
+      .then(() => expect(labelSelectorHelper.getNumberOfVisiblePanes()).toBe(1))
       .then(() => {
         return browser.actions()
           .sendKeys(protractor.Key.CONTROL)
-          .mouseMove(viewer, firstRectangle.topLeft)
+          .mouseMove(viewer, firstShape.topLeft)
           .click()
           .sendKeys(protractor.Key.NULL)
           .perform();
@@ -240,15 +264,48 @@ fdescribe('MultiSelect', () => {
       .then(() => {
         return browser.actions()
           .sendKeys(protractor.Key.CONTROL)
-          .mouseMove(viewer, firstRectangle.topLeft)
+          .mouseMove(viewer, firstShape.topLeft)
           .click()
           .sendKeys(protractor.Key.NULL)
           .perform();
       })
       .then(() => browser.sleep(250))
-      .then(() => expect(labelSelectorHelper.getNumberOfVisiblePanes()).toBe(4))
+      .then(() => expect(labelSelectorHelper.getNumberOfVisiblePanes()).toBe(1))
       .then(() => {
         done();
       })
+  });
+
+  it('should only select shapes of the same type', done => {
+    mock(sharedMocks);
+
+    initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
+      .then(() => drawPedestrian(firstShape))
+      .then(() => drawRectangle(secondShape))
+      .then(() => drawPedestrian(thirdShape))
+      .then(() => drawRectangle(fourthShape))
+      .then(deselectAfterDrawing)
+      .then(() => {
+        return browser.actions()
+          .sendKeys(protractor.Key.CONTROL)
+          .mouseMove(viewer, firstShape.topLeft)
+          .click()
+          .mouseMove(viewer, secondShape.topLeft)
+          .click()
+          .mouseMove(viewer, thirdShape.topLeft)
+          .click()
+          .sendKeys(protractor.Key.NULL)
+          .perform();
+      })
+      .then(
+        // () => canvasInstructionLogManager.getAnnotationCanvasLogs('MultiSelect', 'TwoSelectedPedestrians')
+        () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+      )
+      .then(drawingStack => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.MultiSelect.TwoSelectedPedestrians);
+      })
+      .then(() => {
+        done();
+      });
   });
 });
