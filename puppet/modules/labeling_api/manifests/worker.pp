8f57a4aa7911fc7f7f2785e838edf60d140c2d5c
Christian Hammerl <chh@crosscan.com>
2017-08-07 15:29:28 +0200

    fix supervisor definition for workers

--- puppet/modules/labeling_api/manifests/worker.pp
+++ puppet/modules/labeling_api/manifests/worker.pp
@@ -11,31 +11,31 @@ class labeling_api::worker(
   include ::labeling_api::common
 
   supervisord::program { 'worker-pool-starter-low-normal':
-    command => "${app_dir}/app/AnnoStation/console annostation:workerpool:starter low normal",
-    autostart => $autostart,
-    autorestart => true,
-    user => $user,
-    directory => $app_dir,
-    startsecs => 0,
-    numprocs => $numberOfLowNormalWorkers,
-    environment => {
-        'SYMFONY_ENV' => $symfony_environment,
+    command             => "${app_dir}/app/AnnoStation/console annostation:workerpool:starter low normal",
+    autostart           => $autostart,
+    autorestart         => true,
+    user                => $user,
+    directory           => $app_dir,
+    startsecs           => 0,
+    numprocs            => $numberOfLowNormalWorkers,
+    program_environment => {
+        'SYMFONY_ENV'   => $symfony_environment,
     },
-    notify => Exec['restart supervisord'],
+    notify              => Exec['restart supervisord'],
   }
 
   supervisord::program { 'worker-pool-starter-high-normal':
-    command => "${app_dir}/app/AnnoStation/console annostation:workerpool:starter high normal",
-    autostart => $autostart,
-    autorestart => true,
-    user => $user,
-    directory => $app_dir,
-    startsecs => 0,
-    numprocs => $numberOfHighNormalWorkers,
-    environment => {
-        'SYMFONY_ENV' => $symfony_environment,
+    command             => "${app_dir}/app/AnnoStation/console annostation:workerpool:starter high normal",
+    autostart           => $autostart,
+    autorestart         => true,
+    user                => $user,
+    directory           => $app_dir,
+    startsecs           => 0,
+    numprocs            => $numberOfHighNormalWorkers,
+    program_environment => {
+        'SYMFONY_ENV'   => $symfony_environment,
     },
-    notify => Exec['restart supervisord'],
+    notify              => Exec['restart supervisord'],
   }
 
   supervisord::group { 'annostation-worker-pool':
@@ -52,18 +52,18 @@ class labeling_api::worker(
     $replicationCommand = "bash ${app_dir}/scripts/CouchDB/ReplicatorManager/supervisor-run.sh --adminUrl 'http://${labeling_api::params::couchdb_user}:${labeling_api::params::couchdb_password}@${labeling_api::params::couchdb_host}:${labeling_api::params::couchdb_port}/' --sourceBaseUrl 'http://${labeling_api::params::couchdb_user_read_only}:${labeling_api::params::couchdb_password_read_only}@${labeling_api::params::couchdb_host}:${labeling_api::params::couchdb_port}/' --targetBaseUrl 'http://${labeling_api::params::couchdb_user_read_only}:${labeling_api::params::couchdb_password_read_only}@${labeling_api::params::couchdb_host}:${labeling_api::params::couchdb_port}/' --sourceDbRegex '(taskdb-project-)([a-z0-9_-]+)(-task-)([a-z0-9_-]+)' --targetDb '${labeling_api::params::database_name_read_only}'"
   }
   supervisord::program { 'annostation-couchdb-replication-labeling-api-to-task-databases':
-    command => $replicationCommand,
-    autostart => $autostart,
-    autorestart => true,
-    user => $user,
-    directory => "${app_dir}/scripts/CouchDB/ReplicatorManager/",
-    startsecs => 0,
-    numprocs => 1,
-    stopasgroup => true,
-    environment => {
-        'SYMFONY_ENV' => $symfony_environment,
-        'USER' => $user,
+    command             => $replicationCommand,
+    autostart           => $autostart,
+    autorestart         => true,
+    user                => $user,
+    directory           => "${app_dir}/scripts/CouchDB/ReplicatorManager/",
+    startsecs           => 0,
+    numprocs            => 1,
+    stopasgroup         => true,
+    program_environment => {
+        'SYMFONY_ENV'   => $symfony_environment,
+        'USER'          => $user,
     },
-    notify => Exec['restart supervisord'],
+    notify              => Exec['restart supervisord'],
   }
 }
