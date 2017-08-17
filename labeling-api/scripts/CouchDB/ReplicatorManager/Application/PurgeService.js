094a062d98dc9949f43cf0529e8457129ec529e2
Jakob Westhoff <jakob@westhoffswelt.de>
2017-08-17 11:03:36 +0200

    Refactored and cleaned up purge service
    Next step finally testing this thing :D

--- labeling-api/scripts/CouchDB/ReplicatorManager/Application/PurgeService.js
+++ labeling-api/scripts/CouchDB/ReplicatorManager/Application/PurgeService.js
@@ -1,3 +1,5 @@
+const {clone} = require('lodash');
+
 class PurgeService {
   /**
    * @param {Logger} logger
@@ -31,10 +33,48 @@ class PurgeService {
   purgeDocument(databaseName, documentId) {
     const db = this._nanoAdmin.use(databaseName);
 
+    return Promise.resolve()
+      .then(() => this._fetchAllLeafRevisionsOfDocument(db, documentId))
+      .then(leafs => {
+        const toBePurged = leafs.filter(candidate => candidate.deleted);
+        const toBeDeletedAndPurged = leafs.filter(candidate => !candidate.deleted);
+
+        // Sequentially purge the given leafs
+        const purgePromise = toBePurged.reduce(
+          (carry, leaf) => carry.then(() => this._purgeLeaf(leaf)),
+          Promise.resolve()
+        );
+
+        // After purging already deleted leafs sequentially delete and purge all others
+        const deleteAndPurgePromise = toBeDeletedAndPurged.reduce(
+          (carry, leaf) => carry.then(() => {
+            return Promise.resolve()
+              .then(() => this._deleteLeaf(leaf))
+              .then(updatedLeaf => this._purgeLeaf(updatedLeaf));
+          }),
+          purgePromise
+        );
+
+        return deleteAndPurgePromise;
+      })
+      .then(() => this._logger.logString(`Purged document: ${databaseName}/${documentId}`));
+  }
+
+  /**
+   * Fetch all leaf revisions of an arbitrary document (deleted or still in existence)
+   *
+   * Revisions are provided from the most current down to the first one. (5-abcdef, 4-abcdef, ...)
+   *
+   * @param {nano.db} db
+   * @param {string} documentId
+   * @returns {Promise.<Array.<{db: nano.db, id: string, deleted: boolean, revisions: Array.<string>}>>}
+   * @private
+   */
+  _fetchAllLeafRevisionsOfDocument(db, documentId) {
     return new Promise((resolve, reject) => {
-      db.get(documentId, {revs: true, open_revs: 'all'}, (err, results) => {
-        if (err) {
-          return reject(err);
+      db.get(documentId, {revs: true, open_revs: 'all'}, (error, results) => {
+        if (error) {
+          return reject(error);
         }
 
         /*
@@ -44,86 +84,113 @@ class PurgeService {
          */
         const okResults = results.filter(candidate => ('ok' in candidate));
 
-        // Nothing to purge!
+        // Document does not exist in the database
         if (okResults.length === 0) {
-          return resolve();
+          return resolve([]);
         }
 
-        const toBeDeletedAndPurged = okResults.filter(candidate => !candidate.ok._deleted);
-        const toBePurged = okResults.filter(candidate => candidate.ok._deleted);
-
-        toBePurged.forEach(result => {
-          const revisions = result.ok._revisions.ids.map(
-            (hash, index) => `${result.ok._revisions.ids.length - index}-${hash}`
+        const formattedResults = okResults.map(okResult => {
+          // Revisions in the response don't have a revision number
+          // Therefore we are adding it again.
+          const revisions = okResult.ok._revisions.ids.map(
+            (hash, index) => `${okResult.ok._revisions.ids.length - index}-${hash}`
           );
 
-          return this._purgeDocument(db, documentId, revisions)
-            .then(() => resolve())
-            .catch(error => reject(error));
+          return {
+            id: documentId,
+            deleted: !!okResult.ok._deleted,
+            revisions,
+            db,
+          }
         });
 
-        toBeDeletedAndPurged.forEach(result => {
-          return this._destroyAndPurgeDocument(
-            db,
-            documentId,
-            result.ok._rev,
-          )
-            .then(() => resolve())
-            .catch(destroyAndPurgeError => reject(destroyAndPurgeError));
-        });
+        resolve(formattedResults);
       });
     });
   }
 
-  _destroyAndPurgeDocument(db, documentId, revision) {
+  /**
+   * Delete a leaf from the database, returning an updated version of the leaf, including the latest (deleted) revision
+   *
+   * The returned updated leaf can directly used to purge the document completely.
+   *
+   * @param {{id: string, db: nano.db, deleted: boolean, revisions: Array.<string>}} leaf
+   * @returns {Promise}
+   * @private
+   */
+  _deleteLeaf(leaf) {
+    const {db, id, revisions: [latestRevision]} = leaf;
+
     return new Promise((resolve, reject) => {
-      db.get(documentId, {revs_info: true}, (err, body) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+      db.destroy(
+        id,
+        latestRevision,
+        (error, body) => {
 
-        const revisions = body._revs_info.map(revInfo => revInfo.rev).reverse();
-
-        db.destroy(documentId, revision, (destroyError, destroyBody) => {
-          if (destroyError) {
-            reject(destroyError);
-            return;
+          if (error) {
+            return reject(error);
           }
 
-          revisions.push(destroyBody.rev);
+          const updatedLeaf = this._cloneLeaf(leaf);
+          updatedLeaf.revisions.unshift(body.rev);
 
-          this._purgeDocument(db, documentId, revisions)
-            .then(() => resolve())
-            .catch(error => reject(error));
-        });
-      });
+          return resolve(updatedLeaf);
+        }
+      );
     });
   }
 
-  _purgeDocument(db, documentId, revisions) {
-    const purgeBody = {};
-    purgeBody[documentId] = revisions;
-    return new Promise((resolve, reject) => {
-      this._nanoAdmin.request({
-        db: db.config.db,
-        method: 'post',
-        path: '_purge',
-        body: purgeBody,
-      }, (err, body) => {
-        if (err) {
-          reject(err);
-          return err;
-        }
-        if (Object.keys(body.purged).length === 0) {
-          reject('No revisions purged');
-          return false;
-        }
+  /**
+   * @param {{id: string, db: nano.db, deleted: boolean, revisions: Array.<string>}} leaf
+   * @returns {Promise}
+   * @private
+   */
+  _purgeLeaf(leaf) {
+    const {id, revisions, db} = leaf;
 
-        resolve();
-        return true;
-      });
+    const purgeRequestBody = {
+      [id]: revisions
+    };
+
+    return new Promise((resolve, reject) => {
+      this._nanoAdmin.request(
+        {
+          db: db.config.db,
+          method: 'post',
+          path: '_purge',
+          body: purgeRequestBody,
+        },
+        (error, body) => {
+          if (error) {
+            return reject(error);
+          }
+
+          if (Object.keys(body.purged).length === 0) {
+            return reject(
+              'None of the given leaf revisions could be purged. Are you sure the latest revision was included in the list?');
+          }
+
+          return resolve();
+        }
+      );
     });
+  }
+
+
+  /**
+   * Clone a leaf
+   *
+   * @param {{id: string, db: nano.db, deleted: boolean, revisions: Array.<string>}} leaf
+   * @returns {{id: string, db: nano.db, deleted: boolean, revisions: Array.<string>}}
+   * @private
+   */
+  _cloneLeaf(leaf) {
+    return {
+      db: leaf.db,
+      id: leaf.id,
+      revisions: clone(leaf.revisions),
+      deleted: leaf.deleted,
+    };
   }
 }
 
