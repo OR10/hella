f52f6103fafc2f672ed881c65e11be931245eb04
Jakob Westhoff <jakob@westhoffswelt.de>
2017-09-11 13:17:02 +0200

    Added test fixtures for LabeledThingGroupInFrame

---
+++ labeling-ui/Tests/Fixtures/Models/CouchDb/LabeledThingGroupInFrame.js
@@ -0,0 +1,13 @@
+export default {
+  '_id': 'feea83945187409586bee6f30eaea5b2',
+  '_rev': '1-caae434ccc60630c6bbb8a61f1c94c11',
+  'type': 'AnnoStationBundle.Model.LabeledThingGroupInFrame',
+  'labeledThingGroupId': '0415cc4f101de228c86d7d023501381c',
+  'classes': ['ignore-vehicle'],
+  'incomplete': false,
+  'frameIndex': 0,
+  'taskId': '5242f8bff15774fe72586e569a05ce0c',
+  'projectId': '5242f8bff15774fe72586e569a0469e0',
+  'createdAt': '2017-09-05 16:11:56.000000',
+  'lastModifiedAt': '2017-09-05 16:11:56.000000',
+};
