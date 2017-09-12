2d401eb137a173e96e1565615a909b13c6c342fb
Christian Tischer <cht@crosscan.com>
2017-09-12 10:32:45 +0200

    Added a new Capitalize Angular Filter

--- labeling-ui/Application/Common/Common.js
+++ labeling-ui/Application/Common/Common.js
@@ -81,6 +81,8 @@ import 'flowjs/dist/flow.js';
 import 'ng-flow/dist/ng-flow';
 import 'angular-hotkeys';
 
+import CapitalizeFilter from './Filters/CapitalizeFilter';
+
 class Common extends Module {
   /**
    * Register this {@link Module} with the angular service container system
@@ -150,6 +152,8 @@ class Common extends Module {
     this.module.factory('ListDialog', listDialogProvider);
     this.module.factory('SelectionDialog', selectionDialogProvider);
     this.module.service('groupSelectionDialogFactory', groupSelectionDialogFactory);
+
+    this.module.filter('CapitalizeFilter', CapitalizeFilter);
 
     this.registerDirective('loadingMask', LoadingMaskDirective);
     this.registerDirective('ribbon', RibbonDirective);
