8de46dfee395dda294c65804d8536d680eaaff7c
Jakob Westhoff <jakob@westhoffswelt.de>
2017-09-14 14:23:31 +0200

    Added second LabeledThingGroupFixture

--- labeling-ui/Tests/Fixtures/Models/Frontend/LabeledThingGroup2.js
+++ labeling-ui/Tests/Fixtures/Models/Frontend/LabeledThingGroup2.js
@@ -3,8 +3,7 @@ import taskFixture from './Task';
 import LabeledThingGroup from 'Application/LabelingData/Models/LabeledThingGroup';
 
 export default new LabeledThingGroup({
-  "id": "0415cc4f101de228c86d7d023501381c",
-  "rev": "1-caae434ccc60630c6bbb8a61f1c94c11",
+  "id": "9ec75e00ef3846a78451d53bb6d7dc27",
   "groupType": "extension-sign-group-2",
   "lineColor": "9",
   "groupIds": null,
