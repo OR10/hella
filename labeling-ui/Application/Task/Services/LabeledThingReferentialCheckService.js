0778741123a836ef91da0a9db5c1db73802a42ad
Christian Tischer <cht@crosscan.com>
2017-09-20 11:54:22 +0200

    Added Service to check if there are at least one or more LTIFs for a LT

---
+++ labeling-ui/Application/Task/Services/LabeledThingReferentialCheckService.js
@@ -0,0 +1,44 @@
+class LabeledThingReferentialCheckService {
+  /**
+   * @param $q
+   * @param {LabeledThingGateway} labeledThingGateway
+   */
+  constructor($q, labeledThingGateway) {
+    /**
+     * @type {LabeledThingGateway}
+     * @private
+     */
+    this._labeledThingGateway = labeledThingGateway;
+
+    /**
+     * @type {$q}
+     * @private
+     */
+    this._$q = $q;
+  }
+
+  /**
+   * @param {Task} task
+   * @param {LabeledThing} labeledThing
+   */
+  isAtLeastOneLabeledThingInFrameInRange(task, labeledThing, newStartFrameIndex, newEndFrameIndex) {
+    return this._$q.resolve()
+      .then(() => {
+        return this._labeledThingGateway.getAssociatedLabeledThingsInFrames(task, labeledThing);
+      }).then(documents => {
+        return documents.rows.filter(document => {
+          return (document.doc.frameIndex >= newStartFrameIndex &&
+            document.doc.frameIndex <= newEndFrameIndex);
+        });
+      }).then(documentOutsideRange => {
+        return !(documentOutsideRange.length === 0);
+      });
+  }
+}
+
+LabeledThingReferentialCheckService.$inject = [
+  '$q',
+  'labeledThingGateway',
+];
+
+export default LabeledThingReferentialCheckService;
