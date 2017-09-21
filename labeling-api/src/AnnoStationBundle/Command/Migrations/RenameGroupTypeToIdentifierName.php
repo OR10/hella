a2fc351759ca5ce02bc6f8e492d08d70cd09b3b3
Christian Tischer <cht@crosscan.com>
2017-09-21 10:12:57 +0200

    Added script to rename groupType to identifierName

---
+++ labeling-api/src/AnnoStationBundle/Command/Migrations/RenameGroupTypeToIdentifierName.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace AnnoStationBundle\Command\Migrations;
+
+use AnnoStationBundle\Command;
+use AnnoStationBundle\Database\Facade;
+use AnnoStationBundle\Database\Facade\LabeledThingGroup;
+use Symfony\Component\Console;
+use Symfony\Component\Console\Helper;
+use Doctrine\ODM\CouchDB;
+
+class RenameGroupTypeToIdentifierName extends Command\Base
+{
+    /**
+     * @var Facade\LabelingTask
+     */
+    private $labelingTaskFacade;
+
+    /**
+     * @var LabeledThingGroup\FacadeInterface
+     */
+    private $labeledThingGroupFacadeFactory;
+
+    /**
+     * @var CouchDB\DocumentManager
+     */
+    private $documentManager;
+
+    public function __construct(
+        Facade\LabelingTask $labelingTaskFacade,
+        LabeledThingGroup\FacadeInterface $labeledThingGroupFacadeFactory,
+        CouchDB\DocumentManager $documentManager
+    ) {
+        parent::__construct();
+        $this->labelingTaskFacade             = $labelingTaskFacade;
+        $this->labeledThingGroupFacadeFactory = $labeledThingGroupFacadeFactory;
+        $this->documentManager                = $documentManager;
+    }
+
+    protected function configure()
+    {
+        $this->setName('annostation:migrations:rename-group-type-to-identifier-name');
+    }
+
+    protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output)
+    {
+        $tasks    = $this->labelingTaskFacade->findAll();
+        $progress = new Helper\ProgressBar($output, count($tasks));
+        $progress->start();
+        foreach ($tasks as $task) {
+            $labeledThingGroupFacade = $this->labeledThingGroupFacadeFactory->getFacadeByProjectIdAndTaskId(
+                $task->getProjectId(),
+                $task->getId()
+            );
+
+            $labeledThingGroups = $labeledThingGroupFacade->getLabeledThingGroupsByTask($task);
+
+            foreach ($labeledThingGroups as $labeledThingGroup) {
+                $labeledThingGroup->setIdentifierName($labeledThingGroup->getGroupType());
+                $labeledThingGroupFacade->save($labeledThingGroup);
+                $this->documentManager->detach($labeledThingGroup);
+            }
+            $progress->advance();
+            $this->documentManager->detach($task);
+        }
+        $progress->finish();
+    }
+}
