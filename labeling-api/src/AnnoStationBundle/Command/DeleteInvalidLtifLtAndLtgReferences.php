c775fc46ad30d99919988fa221e4905b548355be
Christian Tischer <cht@crosscan.com>
2017-10-09 09:00:10 +0200

    Moved DeleteInvalidLtifLtAndLtgReferences command to its own job

--- labeling-api/src/AnnoStationBundle/Command/DeleteInvalidLtifLtAndLtgReferences.php
+++ labeling-api/src/AnnoStationBundle/Command/DeleteInvalidLtifLtAndLtgReferences.php
@@ -2,17 +2,13 @@
 
 namespace AnnoStationBundle\Command;
 
-use AppBundle\Model;
-use AnnoStationBundle\Model as AnnoStationBundleModel;
 use AnnoStationBundle\Database\Facade;
+use AnnoStationBundle\Worker\Jobs;
 use Symfony\Component\Console\Input;
 use Symfony\Component\Console\Output;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
-use AnnoStationBundle\Database\Facade\LabeledThing;
-use AnnoStationBundle\Database\Facade\LabeledThingInFrame;
-use AnnoStationBundle\Database\Facade\LabeledThingGroup;
-use Doctrine\ODM\CouchDB;
+use crosscan\WorkerPool;
+use crosscan\WorkerPool\AMQP;
 
 class DeleteInvalidLtifLtAndLtgReferences extends Base
 {
@@ -22,371 +18,40 @@ class DeleteInvalidLtifLtAndLtgReferences extends Base
     private $labelingTaskFacade;
 
     /**
-     * @var LabeledThing\FacadeInterface
+     * @var AMQP\FacadeAMQP
      */
-    private $labeledThingFacadeFactory;
-
-    /**
-     * @var LabeledThingInFrame\FacadeInterface
-     */
-    private $labeledThingInFrameFacadeFactory;
-
-    /**
-     * @var LabeledThingGroup\FacadeInterface
-     */
-    private $labeledThingGroupFacadeFactory;
-
-    /**
-     * @var CouchDB\DocumentManager
-     */
-    private $documentManager;
-
-    /**
-     * @var string
-     */
-    private $deletedObjectsDir;
-
-    /**
-     * @var string
-     */
-    private $couchDbUser;
-
-    /**
-     * @var string
-     */
-    private $couchDbPassword;
-
-    /**
-     * @var string
-     */
-    private $couchDbHost;
-
-    /**
-     * @var string
-     */
-    private $couchDbPort;
+    private $amqpFacade;
 
     public function __construct(
         Facade\LabelingTask $labelingTaskFacade,
-        LabeledThing\FacadeInterface $labeledThingFacadeFactory,
-        LabeledThingInFrame\FacadeInterface $labeledThingInFrameFacadeFactory,
-        LabeledThingGroup\FacadeInterface $labeledThingGroupFacadeFactory,
-        CouchDB\DocumentManager $documentManager,
-        $deletedObjectsDir,
-        $couchDbUser,
-        $couchDbPassword,
-        $couchDbHost,
-        $couchDbPort
+        AMQP\FacadeAMQP $amqpFacade
     ) {
         parent::__construct();
-        $this->labelingTaskFacade               = $labelingTaskFacade;
-        $this->labeledThingInFrameFacadeFactory = $labeledThingInFrameFacadeFactory;
-        $this->labeledThingFacadeFactory        = $labeledThingFacadeFactory;
-        $this->labeledThingGroupFacadeFactory   = $labeledThingGroupFacadeFactory;
-        $this->documentManager                  = $documentManager;
-        $this->deletedObjectsDir                = $deletedObjectsDir;
-        $this->couchDbUser                      = $couchDbUser;
-        $this->couchDbPassword                  = $couchDbPassword;
-        $this->couchDbHost                      = $couchDbHost;
-        $this->couchDbPort                      = $couchDbPort;
+        $this->labelingTaskFacade = $labelingTaskFacade;
+        $this->amqpFacade         = $amqpFacade;
     }
 
     protected function configure()
     {
         $this->setName('annostation:delete-invalid-ltif-lt-and-ltg-references')
-            ->setDescription('Delete all LTIF with an invalid LT reference and LT with an invalid LTG OR no assigned LTIF reference')
+            ->setDescription(
+                'Delete all LTIF with an invalid LT reference and LT with an invalid LTG OR no assigned LTIF reference'
+            )
             ->addArgument('taskId', InputArgument::OPTIONAL)
             ->addOption('dryRun');
     }
 
     protected function execute(Input\InputInterface $input, Output\OutputInterface $output)
     {
-        $dryRun               = $input->getOption('dryRun');
-        $deletedLtifDocs      = 0;
-        $deletedLtgReferences = 0;
-        $deletedLtDocs        = 0;
-
         if ($input->getArgument('taskId') !== null) {
             $tasks = [$this->labelingTaskFacade->find($input->getArgument('taskId'))];
         } else {
             $tasks = $this->labelingTaskFacade->findAll();
         }
 
-        $progressBar          = new ProgressBar($output, count($tasks));
-        $progressBar->setFormatDefinition(
-            'custom',
-            ' Scanning Task %current% of %max% Tasks -- [Deleted LTIFs: %deletedDocs%] [Deleted LTs: %deletedLtDocs%] [Deleted LTG References: %deletedLtgReferences%] [TaskId: %currentTask%]'
-        );
-        $progressBar->setFormat('custom');
-        $progressBar->setMessage($deletedLtifDocs, 'deletedDocs');
-        $progressBar->setMessage($deletedLtgReferences, 'deletedLtgReferences');
-        $progressBar->setMessage($deletedLtDocs, 'deletedLtDocs');
         foreach ($tasks as $task) {
-        $progressBar->setMessage($task->getId(), 'currentTask');
-            $labeledThingInFrameFacade = $this->labeledThingInFrameFacadeFactory->getFacadeByProjectIdAndTaskId(
-                $task->getProjectId(),
-                $task->getId()
-            );
-            $labeledThingFacade        = $this->labeledThingFacadeFactory->getFacadeByProjectIdAndTaskId(
-                $task->getProjectId(),
-                $task->getId()
-            );
-
-            $labeledThings        = $labeledThingFacade->findByTaskId($task);
-            $deletedLtgReferences += $this->removeInvalidLabeledThingGroupReferences(
-                $task,
-                $labeledThings,
-                $dryRun
-            );
-
-            $labeledThingsInFrames        = $labeledThingInFrameFacade->getLabeledThingsInFrame($task);
-            $invalidLabeledThingsInFrames = $this->getInvalidLabeledThingReferencesFromLabeledThingInFrames(
-                $labeledThingsInFrames,
-                $task->getProjectId(),
-                $task->getId()
-            );
-
-            $deletedLtDocs += $this->removeLabeledThingWithoutAnyLabeledThingInFrameRelation(
-                $labeledThings,
-                $task,
-                $dryRun
-            );
-
-            $this->deleteLabeledThingInFrames(
-                $invalidLabeledThingsInFrames,
-                $dryRun
-            );
-            $deletedLtifDocs += count($invalidLabeledThingsInFrames);
-            $progressBar->setMessage($deletedLtifDocs, 'deletedLtifDocs');
-            $progressBar->setMessage($deletedLtgReferences, 'deletedLtgReferences');
-            $progressBar->setMessage($deletedLtDocs, 'deletedLtDocs');
-            $progressBar->advance();
-            $this->detachObjects($labeledThings);
-            $this->detachObjects($labeledThingsInFrames);
-            $this->documentManager->detach($task);
-            $this->documentManager->clear();
+            $job = new Jobs\DeleteInvalidLtifLtAndLtgReferences($task->getId());
+            $this->amqpFacade->addJob($job, WorkerPool\Facade::HIGH_PRIO);
         }
-    }
-
-    /**
-     * @param                    $labeledThings
-     * @param Model\LabelingTask $task
-     * @param                    $dryRun
-     * @return int
-     */
-    private function removeLabeledThingWithoutAnyLabeledThingInFrameRelation(
-      $labeledThings,
-      Model\LabelingTask $task,
-      $dryRun
-    ){
-        $labeledThingFacade        = $this->labeledThingFacadeFactory->getFacadeByProjectIdAndTaskId(
-            $task->getProjectId(),
-            $task->getId()
-        );
-
-        $removedLabeledThings = 0;
-        foreach($labeledThings as $labeledThing) {
-            if (empty($labeledThingFacade->getLabeledThingInFrames($labeledThing))) {
-                $removedLabeledThings++;
-                if (!$dryRun) {
-                    $this->backupAndLogDocument($labeledThing);
-                    $labeledThingFacade->delete($labeledThing);
-                }
-            }
-        }
-
-        return $removedLabeledThings;
-    }
-
-    /**
-     * @param Model\LabelingTask   $task
-     * @param Model\LabeledThing[] $labeledThings
-     * @param                      $dryRun
-     * @return int
-     */
-    private function removeInvalidLabeledThingGroupReferences(
-        Model\LabelingTask $task,
-        $labeledThings,
-        $dryRun
-    ) {
-        $labeledThingGroupFacade   = $this->labeledThingGroupFacadeFactory->getFacadeByProjectIdAndTaskId(
-            $task->getProjectId(),
-            $task->getId()
-        );
-        $labeledThingFacade        = $this->labeledThingFacadeFactory->getFacadeByProjectIdAndTaskId(
-            $task->getProjectId(),
-            $task->getId()
-        );
-        $numberOfDeletedReferences = 0;
-        foreach ($labeledThings as $labeledThing) {
-            $newGroup = $labeledThing->getGroupIds();
-            foreach ($labeledThing->getGroupIds() as $groupId) {
-
-                if (!$labeledThingGroupFacade->find($groupId) instanceof AnnoStationBundleModel\LabeledThingGroup) {
-                    $this->backupAndLogDocument($labeledThing);
-                    $key = array_search($groupId, $newGroup);
-                    unset($newGroup[$key]);
-                    $numberOfDeletedReferences++;
-                }
-            }
-            $labeledThing->setGroupIds($newGroup);
-            if (!$dryRun) {
-                $labeledThingFacade->save($labeledThing);
-            }
-        }
-
-        return $numberOfDeletedReferences;
-    }
-
-    /**
-     * @param Model\LabeledThingInFrame[] $labeledThingsInFrames
-     * @param                             $projectId
-     * @param                             $taskId
-     *
-     * @return array
-     */
-    private function getInvalidLabeledThingReferencesFromLabeledThingInFrames(
-        $labeledThingsInFrames,
-        $projectId,
-        $taskId
-    ) {
-        $labeledThingFacade = $this->labeledThingFacadeFactory->getFacadeByProjectIdAndTaskId(
-            $projectId,
-            $taskId
-        );
-
-        $invalidLabeledThingInFrames = [];
-        foreach ($labeledThingsInFrames as $labeledThingsInFrame) {
-            $labeledThing = $labeledThingFacade->find($labeledThingsInFrame->getLabeledThingId());
-            if ($labeledThing === null) {
-                $invalidLabeledThingInFrames[] = $labeledThingsInFrame;
-            } else {
-                $this->documentManager->detach($labeledThing);
-            }
-        }
-
-        return $invalidLabeledThingInFrames;
-    }
-
-    /**
-     * @param Model\LabeledThingInFrame[] $labeledThingInFrames
-     * @param                             $dryRun
-     */
-    private function deleteLabeledThingInFrames($labeledThingInFrames, $dryRun)
-    {
-        foreach ($labeledThingInFrames as $labeledThingInFrame) {
-            $labeledThingInFrameFacade = $this->labeledThingInFrameFacadeFactory->getFacadeByProjectIdAndTaskId(
-                $labeledThingInFrame->getProjectId(),
-                $labeledThingInFrame->getTaskId()
-            );
-            if (!$dryRun) {
-                $this->backupAndLogDocument($labeledThingInFrame);
-                $labeledThingInFrameFacade->delete([$labeledThingInFrame]);
-            }
-        }
-    }
-
-    /**
-     * @param Model\LabeledThing|Model\LabeledThingInFrame|Model\LabelingGroup $object
-     */
-    private function backupAndLogDocument($object)
-    {
-        file_put_contents(
-            sprintf(
-                '%s/%s.json',
-                $this->getDirectoryPath($object->getProjectId(), $object->getTaskId()),
-                $object->getId()
-            ),
-            $this->getRawDocument($object->getProjectId(), $object->getTaskId(), $object->getId())
-        );
-        $this->logId($object->getId(), $object->getProjectId(), $object->getTaskId());
-    }
-
-    /**
-     * @param $projectId
-     * @param $taskId
-     * @param $documentId
-     *
-     * @return bool|string
-     */
-    private function getRawDocument($projectId, $taskId, $documentId)
-    {
-        return file_get_contents(
-            sprintf(
-                'http://%s:%s@%s:%s/taskdb-project-%s-task-%s/%s',
-                $this->couchDbUser,
-                $this->couchDbPassword,
-                $this->couchDbHost,
-                $this->couchDbPort,
-                $projectId,
-                $taskId,
-                $documentId
-            )
-        );
-    }
-
-    /**
-     * @param $projectId
-     * @param $taskId
-     *
-     * @return string
-     */
-    private function getDirectoryPath($projectId, $taskId)
-    {
-        $date = new \DateTime('now', new \DateTimeZone('UTC'));
-
-        $directoryPathName = sprintf(
-            '%s/files/%s-project-%s-task-%s',
-            $this->deletedObjectsDir,
-            $date->format('d-m-Y'),
-            $projectId,
-            $taskId
-        );
-
-        if (!is_dir($directoryPathName)) {
-            mkdir($directoryPathName);
-        }
-
-        return $directoryPathName;
-    }
-
-    /**
-     * @param $id
-     * @param $projectId
-     * @param $taskId
-     */
-    private function logId($id, $projectId, $taskId)
-    {
-        $date = new \DateTime('now', new \DateTimeZone('UTC'));
-        $file = $this->deletedObjectsDir . '/logs/deleted.txt';
-        if (!is_file($file)) {
-            file_put_contents(
-                $file,
-                "date;id;projectId;taskId\n"
-            );
-        }
-        file_put_contents(
-            $file,
-            sprintf(
-                "%s;%s;%s;%s%s",
-                $date->format('c'),
-                $id,
-                $projectId,
-                $taskId,
-                "\n"
-            ),
-            FILE_APPEND
-        );
-    }
-
-    /**
-     * @param $objects
-     */
-    private function detachObjects($objects)
-    {
-        foreach($objects as $object) {
-            $this->documentManager->detach($object);
-        }
-        $this->documentManager->clear();
     }
 }
---
+++ labeling-api/src/AnnoStationBundle/Worker/JobInstruction/DeleteInvalidLtifLtAndLtgReferences.php
@@ -0,0 +1,365 @@
+<?php
+
+namespace AnnoStationBundle\Worker\JobInstruction;
+
+use crosscan\Logger;
+use crosscan\WorkerPool;
+use AnnoStationBundle\Worker\Jobs;
+use Hagl\WorkerPoolBundle;
+use crosscan\WorkerPool\Exception;
+use Doctrine\ODM\CouchDB;
+use AppBundle\Model;
+use AnnoStationBundle\Model as AnnoStationBundleModel;
+use AnnoStationBundle\Database\Facade;
+use AnnoStationBundle\Database\Facade\LabeledThing;
+use AnnoStationBundle\Database\Facade\LabeledThingInFrame;
+use AnnoStationBundle\Database\Facade\LabeledThingGroup;
+
+class DeleteInvalidLtifLtAndLtgReferences extends WorkerPoolBundle\JobInstruction
+{
+    /**
+     * @var Facade\LabelingTask
+     */
+    private $labelingTaskFacade;
+
+    /**
+     * @var LabeledThing\FacadeInterface
+     */
+    private $labeledThingFacadeFactory;
+
+    /**
+     * @var LabeledThingInFrame\FacadeInterface
+     */
+    private $labeledThingInFrameFacadeFactory;
+
+    /**
+     * @var LabeledThingGroup\FacadeInterface
+     */
+    private $labeledThingGroupFacadeFactory;
+
+    /**
+     * @var CouchDB\DocumentManager
+     */
+    private $documentManager;
+
+    /**
+     * @var string
+     */
+    private $deletedObjectsDir;
+
+    /**
+     * @var string
+     */
+    private $couchDbUser;
+
+    /**
+     * @var string
+     */
+    private $couchDbPassword;
+
+    /**
+     * @var string
+     */
+    private $couchDbHost;
+
+    /**
+     * @var string
+     */
+    private $couchDbPort;
+
+    public function __construct(
+        Facade\LabelingTask $labelingTaskFacade,
+        LabeledThing\FacadeInterface $labeledThingFacadeFactory,
+        LabeledThingInFrame\FacadeInterface $labeledThingInFrameFacadeFactory,
+        LabeledThingGroup\FacadeInterface $labeledThingGroupFacadeFactory,
+        CouchDB\DocumentManager $documentManager,
+        $deletedObjectsDir,
+        $couchDbUser,
+        $couchDbPassword,
+        $couchDbHost,
+        $couchDbPort
+    ) {
+        $this->labelingTaskFacade               = $labelingTaskFacade;
+        $this->labeledThingFacadeFactory        = $labeledThingFacadeFactory;
+        $this->labeledThingInFrameFacadeFactory = $labeledThingInFrameFacadeFactory;
+        $this->labeledThingGroupFacadeFactory   = $labeledThingGroupFacadeFactory;
+        $this->documentManager                  = $documentManager;
+        $this->deletedObjectsDir                = $deletedObjectsDir;
+        $this->couchDbUser                      = $couchDbUser;
+        $this->couchDbPassword                  = $couchDbPassword;
+        $this->couchDbHost                      = $couchDbHost;
+        $this->couchDbPort                      = $couchDbPort;
+    }
+
+    /**
+     * @param WorkerPool\Job             $job
+     * @param Logger\Facade\LoggerFacade $loggerFacade
+     *
+     * @throws Exception\Recoverable
+     */
+    protected function runJob(WorkerPool\Job $job, Logger\Facade\LoggerFacade $loggerFacade)
+    {
+        $task = $this->labelingTaskFacade->find($job->getTaskId());
+
+        $labeledThingInFrameFacade = $this->labeledThingInFrameFacadeFactory->getFacadeByProjectIdAndTaskId(
+            $task->getProjectId(),
+            $task->getId()
+        );
+        $labeledThingFacade        = $this->labeledThingFacadeFactory->getFacadeByProjectIdAndTaskId(
+            $task->getProjectId(),
+            $task->getId()
+        );
+
+        $labeledThings        = $labeledThingFacade->findByTaskId($task);
+        $this->removeInvalidLabeledThingGroupReferences(
+            $task,
+            $labeledThings
+        );
+
+        $labeledThingsInFrames        = $labeledThingInFrameFacade->getLabeledThingsInFrame($task);
+        $invalidLabeledThingsInFrames = $this->getInvalidLabeledThingReferencesFromLabeledThingInFrames(
+            $labeledThingsInFrames,
+            $task->getProjectId(),
+            $task->getId()
+        );
+
+        $this->removeLabeledThingWithoutAnyLabeledThingInFrameRelation(
+            $labeledThings,
+            $task
+        );
+
+        $this->deleteLabeledThingInFrames(
+            $invalidLabeledThingsInFrames
+        );
+        $this->detachObjects($labeledThings);
+        $this->detachObjects($labeledThingsInFrames);
+        $this->documentManager->detach($task);
+        $this->documentManager->clear();
+    }
+
+    /**
+     * @param                    $labeledThings
+     * @param Model\LabelingTask $task
+     */
+    private
+    function removeLabeledThingWithoutAnyLabeledThingInFrameRelation(
+        $labeledThings,
+        Model\LabelingTask $task
+    ) {
+        $labeledThingFacade = $this->labeledThingFacadeFactory->getFacadeByProjectIdAndTaskId(
+            $task->getProjectId(),
+            $task->getId()
+        );
+
+        foreach ($labeledThings as $labeledThing) {
+            if (empty($labeledThingFacade->getLabeledThingInFrames($labeledThing))) {
+                $this->backupAndLogDocument($labeledThing);
+                $labeledThingFacade->delete($labeledThing);
+            }
+        }
+    }
+
+    /**
+     * @param Model\LabelingTask   $task
+     * @param Model\LabeledThing[] $labeledThings
+     *
+     * @return int
+     */
+    private function removeInvalidLabeledThingGroupReferences(
+        Model\LabelingTask $task,
+        $labeledThings
+    ) {
+        $labeledThingGroupFacade   = $this->labeledThingGroupFacadeFactory->getFacadeByProjectIdAndTaskId(
+            $task->getProjectId(),
+            $task->getId()
+        );
+        $labeledThingFacade        = $this->labeledThingFacadeFactory->getFacadeByProjectIdAndTaskId(
+            $task->getProjectId(),
+            $task->getId()
+        );
+        foreach ($labeledThings as $labeledThing) {
+            $newGroup = $labeledThing->getGroupIds();
+            foreach ($labeledThing->getGroupIds() as $groupId) {
+
+                if (!$labeledThingGroupFacade->find($groupId) instanceof AnnoStationBundleModel\LabeledThingGroup) {
+                    $this->backupAndLogDocument($labeledThing);
+                    $key = array_search($groupId, $newGroup);
+                    unset($newGroup[$key]);
+                }
+            }
+            $labeledThing->setGroupIds($newGroup);
+            $labeledThingFacade->save($labeledThing);
+        }
+    }
+
+    /**
+     * @param Model\LabeledThingInFrame[] $labeledThingsInFrames
+     * @param                             $projectId
+     * @param                             $taskId
+     *
+     * @return array
+     */
+    private function getInvalidLabeledThingReferencesFromLabeledThingInFrames(
+        $labeledThingsInFrames,
+        $projectId,
+        $taskId
+    ) {
+        $labeledThingFacade = $this->labeledThingFacadeFactory->getFacadeByProjectIdAndTaskId(
+            $projectId,
+            $taskId
+        );
+
+        $invalidLabeledThingInFrames = [];
+        foreach ($labeledThingsInFrames as $labeledThingsInFrame) {
+            $labeledThing = $labeledThingFacade->find($labeledThingsInFrame->getLabeledThingId());
+            if ($labeledThing === null) {
+                $invalidLabeledThingInFrames[] = $labeledThingsInFrame;
+            } else {
+                $this->documentManager->detach($labeledThing);
+            }
+        }
+
+        return $invalidLabeledThingInFrames;
+    }
+
+    /**
+     * @param Model\LabeledThingInFrame[] $labeledThingInFrames
+     */
+    private function deleteLabeledThingInFrames(
+        $labeledThingInFrames
+    ) {
+        foreach ($labeledThingInFrames as $labeledThingInFrame) {
+            $labeledThingInFrameFacade = $this->labeledThingInFrameFacadeFactory->getFacadeByProjectIdAndTaskId(
+                $labeledThingInFrame->getProjectId(),
+                $labeledThingInFrame->getTaskId()
+            );
+            $this->backupAndLogDocument($labeledThingInFrame);
+            $labeledThingInFrameFacade->delete([$labeledThingInFrame]);
+        }
+    }
+
+    /**
+     * @param Model\LabeledThing|Model\LabeledThingInFrame|Model\LabelingGroup $object
+     */
+    private function backupAndLogDocument(
+        $object
+    ) {
+        file_put_contents(
+            sprintf(
+                '%s/%s.json',
+                $this->getDirectoryPath($object->getProjectId(), $object->getTaskId()),
+                $object->getId()
+            ),
+            $this->getRawDocument($object->getProjectId(), $object->getTaskId(), $object->getId())
+        );
+        $this->logId($object->getId(), $object->getProjectId(), $object->getTaskId());
+    }
+
+    /**
+     * @param $projectId
+     * @param $taskId
+     * @param $documentId
+     *
+     * @return bool|string
+     */
+    private function getRawDocument(
+        $projectId,
+        $taskId,
+        $documentId
+    ) {
+        return file_get_contents(
+            sprintf(
+                'http://%s:%s@%s:%s/taskdb-project-%s-task-%s/%s',
+                $this->couchDbUser,
+                $this->couchDbPassword,
+                $this->couchDbHost,
+                $this->couchDbPort,
+                $projectId,
+                $taskId,
+                $documentId
+            )
+        );
+    }
+
+    /**
+     * @param $projectId
+     * @param $taskId
+     *
+     * @return string
+     */
+    private function getDirectoryPath(
+        $projectId,
+        $taskId
+    ) {
+        $date = new \DateTime('now', new \DateTimeZone('UTC'));
+
+        $directoryPathName = sprintf(
+            '%s/files/%s-project-%s-task-%s',
+            $this->deletedObjectsDir,
+            $date->format('d-m-Y'),
+            $projectId,
+            $taskId
+        );
+
+        if (!is_dir($directoryPathName)) {
+            mkdir($directoryPathName);
+        }
+
+        return $directoryPathName;
+    }
+
+    /**
+     * @param $id
+     * @param $projectId
+     * @param $taskId
+     */
+    private function logId(
+        $id,
+        $projectId,
+        $taskId
+    ) {
+        $date = new \DateTime('now', new \DateTimeZone('UTC'));
+        $file = $this->deletedObjectsDir . '/logs/deleted.txt';
+        if (!is_file($file)) {
+            file_put_contents(
+                $file,
+                "date;id;projectId;taskId\n"
+            );
+        }
+        file_put_contents(
+            $file,
+            sprintf(
+                "%s;%s;%s;%s%s",
+                $date->format('c'),
+                $id,
+                $projectId,
+                $taskId,
+                "\n"
+            ),
+            FILE_APPEND
+        );
+    }
+
+    /**
+     * @param $objects
+     */
+    private function detachObjects(
+        $objects
+    ) {
+        foreach ($objects as $object) {
+            $this->documentManager->detach($object);
+        }
+        $this->documentManager->clear();
+    }
+
+    /**
+     * @param WorkerPool\Job $job
+     *
+     * @return bool
+     */
+    public function supports(
+        WorkerPool\Job $job
+    ) {
+        return $job instanceof Jobs\DeleteInvalidLtifLtAndLtgReferences;
+    }
+}
