cb67009d15585907e2f7f96fcaba6f153b14007f
Christian Tischer <cht@crosscan.com>
2017-10-11 07:51:13 +0200

    Added first e2e test

---
+++ labeling-ui/Tests/E2E/CutShapes.spec.js
@@ -0,0 +1,98 @@
+import CanvasInstructionLogManager from '../Support/CanvasInstructionLogManager';
+import {
+  initApplication,
+  expectAllModalsToBeClosed,
+  bootstrapHttp,
+  bootstrapPouch,
+  mediumSleep,
+} from '../Support/Protractor/Helpers';
+import AssetHelper from '../Support/Protractor/AssetHelper';
+
+const canvasInstructionLogManager = new CanvasInstructionLogManager(browser);
+
+fdescribe('Cut shapes', () => {
+  let assets;
+  let viewer;
+  let cutShapeButton;
+
+  beforeEach(() => {
+    assets = new AssetHelper(
+      `${__dirname}/../Fixtures`,
+      `${__dirname}/../ProtractorMocks`,
+      `${__dirname}/../PouchDbDocuments`
+    );
+    bootstrapHttp([
+      assets.mocks.Shared.TaskDb,
+      assets.mocks.Shared.UserProfile,
+      assets.mocks.Shared.UserPermissions,
+      assets.mocks.Shared.UserOrganisations,
+      assets.mocks.Shared.Video,
+      assets.mocks.CutShape.Rectangle.Task,
+      assets.mocks.CutShape.Rectangle.TaskConfiguration,
+      assets.mocks.CutShape.Rectangle.TaskConfigurationFile,
+      assets.mocks.Shared.LabelStructure,
+      assets.mocks.Shared.GetTimer,
+      assets.mocks.Shared.PutTimer,
+      assets.mocks.Shared.LabeledThingIncompleteCount,
+      assets.mocks.Shared.FrameLocations.SourceJpg.frameIndex0,
+      assets.mocks.Shared.FrameLocations.SourceJpg.frameIndex0to4,
+      assets.mocks.Shared.FrameLocations.Thumbnail.frameIndex0,
+      assets.mocks.Shared.FrameLocations.Thumbnail.frameIndex0to4,
+      assets.mocks.Shared.Thumbnails.rectangleLabeledThingsInFrame0to3,
+      assets.mocks.Shared.EmptyLabeledThingGroupInFrame,
+    ]);
+
+    cutShapeButton = element(by.css('#cut-shape-button'));
+
+    viewer = element(by.css('.layer-container'));
+  });
+
+  it('should cut a shape', done => {
+    bootstrapPouch([
+      assets.documents.CutShapes.DrawOneRectangle.LabeledThingInFrame.frameIndex0_3,
+    ]);
+
+    const nextFrameButton = element(by.css('.next-frame-button'));
+
+    initApplication(
+      '/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
+      .then(() => nextFrameButton.click())
+      .then(() => mediumSleep())
+      .then(() => nextFrameButton.click())
+      .then(() => mediumSleep())
+      .then(() => {
+        return browser.actions()
+          .mouseMove(viewer, {x: 260, y: 260})
+          .click()
+          .perform();
+      })
+      .then(() => mediumSleep())
+      .then(() => cutShapeButton.click())
+      .then(() => mediumSleep())
+      .then(() => {
+        const confirmButton = element(by.css('#modal-confirm-button'));
+        return confirmButton.click();
+      })
+      .then(() => mediumSleep())
+      .then(() => {
+        return browser.actions()
+          .mouseMove(viewer, {x: 400, y: 400})
+          .click()
+          .perform();
+      })
+      .then(
+        // () => canvasInstructionLogManager.getAnnotationCanvasLogs('CutShapes', 'DrawOneRectangle')
+         () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+      )
+      .then(drawingStack => {
+        expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.CutShapes.DrawOneRectangle);
+      })
+      .then(() => done());
+  });
+
+  afterEach(() => {
+    expectAllModalsToBeClosed();
+    bootstrapHttp.teardown();
+    bootstrapPouch.teardown();
+  });
+});
