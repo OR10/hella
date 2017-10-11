4c2b68b5d75e5f6299029ea6b08fed346056e126
Dominik Ehrenberg <doe@crosscan.com>
2017-10-11 10:50:38 +0200

    E2E Tests for merging shapes on the same frame

---
+++ labeling-ui/Tests/E2E/ShapeMerging.spec.js
@@ -0,0 +1,137 @@
+import {
+  expectAllModalsToBeClosed,
+  initApplication,
+  bootstrapHttp,
+  bootstrapPouch,
+} from '../Support/Protractor/Helpers';
+import AssetHelper from '../Support/Protractor/AssetHelper';
+import CanvasInstructionLogManager from '../Support/CanvasInstructionLogManager';
+
+fdescribe('ShapeMerging', () => {
+  let assets;
+  let viewer;
+  let sharedMocks;
+  let shapeInboxButton;
+  let headerPlusButton;
+  let mergeButton;
+  let modalConfirmButton;
+
+  const canvasInstructionLogManager = new CanvasInstructionLogManager(browser);
+
+  const firstShape = {
+    topLeft: {x: 100, y: 100},
+    bottomRight: {x: 200, y: 200},
+  };
+
+  const secondShape = {
+    topLeft: {x: 250, y: 250},
+    bottomRight: {x: 350, y: 450},
+  };
+
+  beforeEach(() => {
+    assets = new AssetHelper(`${__dirname}/../Fixtures`, `${__dirname}/../ProtractorMocks`, `${__dirname}/../PouchDbDocuments`);
+    sharedMocks = [
+      assets.mocks.Shared.TaskDb,
+      assets.mocks.Shared.UserProfile,
+      assets.mocks.Shared.UserPermissions,
+      assets.mocks.Shared.UserOrganisations,
+      assets.mocks.Shared.Task,
+      assets.mocks.Shared.Video,
+      assets.mocks.Shared.LabelStructure,
+      assets.mocks.Shared.GetTimer,
+      assets.mocks.Shared.PutTimer,
+      assets.mocks.Shared.LabeledThingIncompleteCount,
+      assets.mocks.Shared.FrameLocations.SourceJpg.frameIndex0,
+      assets.mocks.Shared.FrameLocations.SourceJpg.frameIndex0to4,
+      assets.mocks.Shared.FrameLocations.Thumbnail.frameIndex0,
+      assets.mocks.Shared.FrameLocations.Thumbnail.frameIndex0to4,
+      assets.mocks.Shared.Thumbnails.rectangleLabeledThingsInFrame0to3,
+      assets.mocks.Shared.Thumbnails.rectangleLabeledThingsInFrame0to4,
+      assets.mocks.Shared.EmptyLabeledThingGroupInFrame,
+    ];
+
+    bootstrapHttp(sharedMocks);
+
+    bootstrapPouch([
+      assets.documents.ShapeInbox.DrawTwoRectangles,
+    ]);
+
+    viewer = element(by.css('.layer-container'));
+    shapeInboxButton = element(by.css('.task-bar .icon.fa-inbox'));
+    headerPlusButton = element(by.css('#popup-inbox-selected .shape-list-header .icon'));
+    mergeButton = element(by.css('.popup-inbox-actions i.fa-compress'));
+    modalConfirmButton = element(by.css('.modal-button-confirm'));
+  });
+
+  describe('Modal window', () => {
+    describe('merges two shapes on the same frame', () => {
+      it('using the first clicked shape as root', done => {
+        initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
+          .then(() => {
+            return browser.actions()
+              .sendKeys(protractor.Key.CONTROL)
+              .mouseMove(viewer, firstShape.topLeft) // initial position
+              .click()
+              .mouseMove(viewer, secondShape.topLeft) // initial position
+              .click()
+              .sendKeys(protractor.Key.NULL)
+              .perform();
+          })
+          .then(() => browser.sleep(250))
+          .then(() => shapeInboxButton.click())
+          .then(() => browser.sleep(250))
+          .then(() => headerPlusButton.click())
+          .then(() => browser.sleep(250))
+          .then(() => mergeButton.click())
+          .then(() => browser.sleep(250))
+          .then(() => modalConfirmButton.click())
+          .then(() => browser.sleep(250))
+          .then(() => browser.sleep(250))
+          .then(() => shapeInboxButton.click())
+          .then(
+            // () => canvasInstructionLogManager.getAnnotationCanvasLogs('ShapeMerging', 'MergeTwoShapesOnOneFrame1')
+            () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+          )
+          .then(drawingStack => {
+            expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.ShapeMerging.MergeTwoShapesOnOneFrame1);
+          })
+          .then(() => done());
+      });
+
+      it('using the second shape', done => {
+        initApplication('/labeling/organisation/ORGANISATION-ID-1/projects/PROJECTID-PROJECTID/tasks/TASKID-TASKID/labeling')
+          .then(() => {
+            return browser.actions()
+              .sendKeys(protractor.Key.CONTROL)
+              .mouseMove(viewer, firstShape.topLeft) // initial position
+              .click()
+              .mouseMove(viewer, secondShape.topLeft) // initial position
+              .click()
+              .sendKeys(protractor.Key.NULL)
+              .perform();
+          })
+          .then(() => browser.sleep(250))
+          .then(() => shapeInboxButton.click())
+          .then(() => browser.sleep(250))
+          .then(() => headerPlusButton.click())
+          .then(() => browser.sleep(250))
+          .then(() => mergeButton.click())
+          .then(() => browser.sleep(250))
+          .then(() => element(by.cssContainingText('option', 'rectangle #2')).click())
+          .then(() => browser.sleep(250))
+          .then(() => modalConfirmButton.click())
+          .then(() => browser.sleep(250))
+          .then(() => browser.sleep(250))
+          .then(() => shapeInboxButton.click())
+          .then(
+            // () => canvasInstructionLogManager.getAnnotationCanvasLogs('ShapeMerging', 'MergeTwoShapesOnOneFrame2')
+            () => canvasInstructionLogManager.getAnnotationCanvasLogs()
+          )
+          .then(drawingStack => {
+            expect(drawingStack).toEqualRenderedDrawingStack(assets.fixtures.Canvas.ShapeMerging.MergeTwoShapesOnOneFrame2);
+          })
+          .then(() => done());
+      });
+    });
+  });
+});
