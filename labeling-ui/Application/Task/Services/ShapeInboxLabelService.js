730286dc48a370b933ee6956b6d5e7490f112049
Jakob Westhoff <jakob@westhoffswelt.de>
2017-10-26 14:47:44 +0200

    Created Service to store shape labels

---
+++ labeling-ui/Application/Task/Services/ShapeInboxLabelService.js
@@ -0,0 +1,82 @@
+class ShapeInboxLabelService {
+  /**
+   * @param {angular.$q} $q
+   */
+  constructor($q) {
+    /**
+     * @type {angular.$q}
+     * @private
+     */
+    this._$q = $q;
+
+    /**
+     * @type {Map.<string, string>}
+     * @private
+     */
+    this._labelMap = new Map();
+
+    /**
+     * @type {Map.<string, number>}
+     * @private
+     */
+    this._uniqueIdCounter = new Map();
+  }
+
+  /**
+   * @param {LabelStructureObject} lso
+   * @param {LabeledThing} labeledThing
+   * @returns {string}
+   */
+  getLabelForLabelStructureObjectAndLabeledThing(lso, labeledThing) {
+    if (!this._labelMap.has(labeledThing.id)) {
+      const newLabel = this._generateName(lso);
+      this._labelMap.set(labeledThing.id, newLabel);
+    }
+
+    return this._labelMap.get(labeledThing.id);
+  }
+
+  /**
+   * @param {LabeledThing} labeledThing
+   * @param {string} newName
+   */
+  setLabelForLabelThing(labeledThing, newName) {
+    this._labelMap.set(labeledThing.id, newName);
+  }
+
+
+  /**
+   * Generate and return a new unique name for the given shape
+   *
+   * @param {LabelStructureObject} lso
+   * @returns {string}
+   *
+   * @private
+   */
+  _generateName(lso) {
+    return `${lso.name} #${this._createNewUniqueIdForLabelStructureObject(lso)}`;
+  }
+
+  /**
+   * @param {LabelStructureObject} lso
+   * @private
+   */
+  _createNewUniqueIdForLabelStructureObject(lso) {
+    const type = lso.name;
+
+    if (!this._uniqueIdCounter.has(type)) {
+      this._uniqueIdCounter.set(type, 0);
+    }
+
+    const previousValue = this._uniqueIdCounter.get(type);
+    this._uniqueIdCounter.set(type, previousValue + 1);
+
+    return this._uniqueIdCounter.get(type);
+  }
+}
+
+ShapeInboxLabelService.$inject = [
+  '$q',
+];
+
+export default ShapeInboxLabelService;
