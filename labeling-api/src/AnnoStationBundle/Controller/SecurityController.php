0b5479995c219f13e926093c4be1ff0e42501e5a
capons <bogdandvinin@rambler.ru>
2018-03-14 17:14:43 +0200

    FE authentication new controller action

---
+++ labeling-api/src/AnnoStationBundle/Controller/SecurityController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AnnoStationBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Annotations\CloseSession;
+use AppBundle\Model;
+use AnnoStationBundle\Database\Facade;
+use AnnoStationBundle\Service;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration;
+use Symfony\Bridge\Twig;
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception;
+
+/**
+ * @Configuration\Route("/", service="annostation.labeling_api.controller.index")
+ *
+ * @CloseSession
+ */
+class SecurityController extends Controller
+{
+
+    /**
+     * @Configuration\Route("/login_check")
+     */
+    public function loginCheckAction()
+    {
+
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logoutAction()
+    {
+
+    }
+}
