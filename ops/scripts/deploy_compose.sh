1b9f2ebb90b6af3023fb66dac62e6998d8a3ae28
Vasily Akulov <vasily.akulov@softeq.com>
2018-03-21 14:56:10 +0300

    syntax fix

--- ops/scripts/deploy_compose.sh
+++ ops/scripts/deploy_compose.sh
@@ -27,7 +27,7 @@ docker-machine ssh $MONITORING_MACHINE ".env"
 #################### COUCHDB DEPLOYMENT BEGIN ####################
 
 #Make yml for docker couchdb
-export COMPOSE_FILE=ops/docker/compose/service/api-couch.ymlops/docker/compose/monitoring/logspout.yml
+export COMPOSE_FILE=ops/docker/compose/service/api-couch.yml:ops/docker/compose/monitoring/logspout.yml
 docker-compose config > couchdb.yml
 
 #Copy docker-compose config
