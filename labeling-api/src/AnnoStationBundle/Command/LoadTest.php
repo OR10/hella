c067843c688d52b363ed2b7c408b9c4e31cb6a92
Maksim Kabanov <maksim.kabanov@softeq.com>
2018-04-02 14:45:30 +0300

    Fix path for videos

--- labeling-api/src/AnnoStationBundle/Command/LoadTest.php
+++ labeling-api/src/AnnoStationBundle/Command/LoadTest.php
@@ -391,10 +391,10 @@ class LoadTest extends Base
             return true;
         }
         
-        $videoFileDir = '/var/www/hella/videos';
+        $videoFileDir = '/code/videos';
         if(!is_dir($videoFileDir)) {
-            exec('wget https://sst.by/videos.zip -O /var/www/hella/videos.zip');
-            exec('cd /var/www/hella/ && unzip /var/www/hella/videos.zip');
+            exec('wget https://sst.by/videos.zip -O /code/videos.zip');
+            exec('cd /code/ && unzip /code/videos.zip');
         }
         $videoFileList = $scanned_directory = array_diff(scandir($videoFileDir), array('..', '.'));
         
