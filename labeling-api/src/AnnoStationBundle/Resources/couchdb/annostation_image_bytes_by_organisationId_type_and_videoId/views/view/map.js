b6f51de1242b0ce0063a4e52440281b952b7e624
Vital Leshchyk <vital.leshchyk@softeq.com>
2018-04-23 12:06:28 +0300

    HELLAAS-195 size calculation fix (accumulatedSizeInBytes)

--- labeling-api/src/AnnoStationBundle/Resources/couchdb/annostation_image_bytes_by_organisationId_type_and_videoId/views/view/map.js
+++ labeling-api/src/AnnoStationBundle/Resources/couchdb/annostation_image_bytes_by_organisationId_type_and_videoId/views/view/map.js
@@ -2,7 +2,7 @@ function(doc) {
   if (doc.type === 'AppBundle.Model.Video') {
     Object.keys(doc.imageTypes).forEach(function(type) {
       if (doc.imageTypes[type]['accumulatedSizeInBytes'] !== undefined) {
-        emit([doc.organisationId, type, doc._id], doc.imageTypes[type]['accumulatedSizeInBytes']);
+        emit([doc.organisationId, type, doc._id], null === doc.imageTypes[type]['accumulatedSizeInBytes'] ? 0 : doc.imageTypes[type]['accumulatedSizeInBytes']);
       }
     });
   }
