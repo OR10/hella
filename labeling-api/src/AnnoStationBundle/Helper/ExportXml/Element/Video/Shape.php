bfecb4617faf1d7116a9fb2a6b3d49ff5ea038dc
Maksim <maksim.kabanov@softeq.com>
2018-05-08 11:04:10 +0300

    Fix switch loop

--- labeling-api/src/AnnoStationBundle/Helper/ExportXml/Element/Video/Shape.php
+++ labeling-api/src/AnnoStationBundle/Helper/ExportXml/Element/Video/Shape.php
@@ -88,6 +88,7 @@ class Shape extends ExportXml\Element
                 break;
             case $this->shape instanceof Model\Shapes\BrightestPixel:
                 $shapeElement = new Shape\BrightestPixel($this->shape, $this->namespace);
+                break;
             case $this->shape instanceof Model\Shapes\Trapezoid:
                 $shapeElement = new Shape\Trapezoid($this->shape, $this->namespace);
                 break;
